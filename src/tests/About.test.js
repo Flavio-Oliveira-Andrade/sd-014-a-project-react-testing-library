aaeb5fcb158c201ae5e4f658f3743a38ea6bad58
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-09-28 18:36:53 -0300

    Requisito 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+test('Teste o componente <About.js />', () => {
+  const { history } = renderWithRouter(<About />);
+
+  const text1 = 'One can filter Pokémons by type, and see more details for each one of them';
+  const text2 = 'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons';
+
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,58 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  const { history } = renderWithRouter(<App />);
+
+  expect(
+    screen.getByText('Home'),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText('About'),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText('Favorite Pokémons'),
+  ).toBeInTheDocument();
+
+  fireEvent.click(
+    screen.getByText('Home'),
+  );
+
+  expect(
+    history.location.pathname,
+  ).toBe('/');
+
+  fireEvent.click(
+    screen.getByText('About'),
+  );
+
+  expect(
+    history.location.pathname,
+  ).toBe('/about');
+
+  expect(
+    screen.getByText('About Pokédex'),
+  ).toBeInTheDocument();
+
+  fireEvent.click(
+    screen.getByText('Favorite Pokémons'),
+  );
+
+  expect(
+    history.location.pathname,
+  ).toBe('/favorites');
+
+  expect(
+    screen.getByText('Favorite pokémons'),
+  ).toBeInTheDocument();
+
+  history.push('anythingelse');
+
+  expect(
+    screen.getByText('Page requested not found'),
+  ).toBeInTheDocument();
+});

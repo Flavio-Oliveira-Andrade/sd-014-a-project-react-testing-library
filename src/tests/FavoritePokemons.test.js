b6b8adfe4cdfd79f5e3aa7594c5b894da7dc3af0
MuriloM <muriloommaia@gmail.com>
2021-09-28 19:19:58 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found,'
+  + ' se a pessoa não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textNotFound = screen.getByText('No favorite pokemon found');
+    expect(textNotFound).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkDetails = screen.getByRole('link', {
+      name: 'More details',
+    });
+    userEvent.click(linkDetails);
+    const { pathname } = history.location;
+    expect(pathname).not.toBe('/');
+    const inputFavorite = screen.getByRole('checkbox');
+    userEvent.click(inputFavorite);
+    history.push('/favorites');
+    const pikachuText = screen.getByText('Pikachu');
+    expect(pikachuText).toBeInTheDocument();
+  });
+});

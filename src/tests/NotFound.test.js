a80d4ed8016585cb36cac9b8590f735e8917f7ab
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-09-28 19:27:35 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('Teste o componente <NotFound.js />', () => {
+  renderWithRouter(<NotFound />);
+
+  const imgLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  expect(
+    screen.getByRole('heading', { level: 2 }).textContent,
+  ).toBe('Page requested not found 😭');
+
+  expect(
+    screen.getAllByRole('img')[1].src,
+  ).toBe(imgLink);
+});

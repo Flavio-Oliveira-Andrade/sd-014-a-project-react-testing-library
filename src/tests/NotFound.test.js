48da0958520a0638f1dc4e4e32f8ad94384b1cd8
MuriloM <muriloommaia@gmail.com>
2021-09-28 19:34:41 -0300

    finaliza 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
-import { FavoritePokemons } from '../components';
 
 const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
 
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { Pokedex } from '../components';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<Pokedex />);
+    const headingH2 = screen.getByRole('heading', { 
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+  });
+});

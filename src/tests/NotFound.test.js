4c3f20110d99f09427f8892bb3d99ad6ef297b6b
MuriloM <muriloommaia@gmail.com>
2021-09-28 19:29:12 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-nao-existente');
+    const headingH2 = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    const { src } = screen.getAllByRole('img')[1];
+    expect(headingH2).toBeInTheDocument();
+    expect(src).toBe(URL);
+  });
+});

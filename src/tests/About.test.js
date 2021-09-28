851530fa5e38db41b4c6176125af5d055f4572b9
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-09-28 18:48:45 -0300

    Requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -6,7 +6,30 @@ import About from '../components/About';
 test('Teste o componente <About.js />', () => {
   const { history } = renderWithRouter(<About />);
 
-  const text1 = 'One can filter Pokémons by type, and see more details for each one of them';
-  const text2 = 'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons';
+  const text1 = (
+    'One can filter Pokémons by type, and see more details for each one of them'
+  );
+  const text2 = (
+    'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons'
+  );
 
+  const imgLink = (
+    'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+  );
+
+  expect(
+    screen.getByText(text1),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText(text2),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByRole('heading', { level: 2 }).textContent,
+  ).toBe('About Pokédex');
+
+  expect(
+    screen.getByRole('img').src,
+  ).toBe(imgLink);
 });

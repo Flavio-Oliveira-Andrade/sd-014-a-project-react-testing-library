43bd75078678f85b7fb86f176b4c748cb49b8b00
BlyNTC <isaacase@hotmail.com>
2021-09-28 20:41:41 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Testando o componente <App.js />', () => {
+  it('Teste se o topo da aplicação contém'
+  + ' um conjunto fixo de links de navegação.', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+  it('a aplicação é redirecionada para a página inicial,'
+  + ' na URL / ao clicar no link Home da barra de navegação', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('a aplicação é redirecionada para a página de About, na URL'
+  + ' /about, ao clicar no link About da barra de navegação.', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it('a aplicação é redirecionada para a página de Pokémons Favoritados, na URL'
+  + ' /favorites, ao clicar no link Favorite Pokémons da barra de navegação.', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+  it('a aplicação é redirecionada para a página'
+  + ' Not Found ao entrar em uma URL desconhecida.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const error = screen.getByText(/Page requested not found/i);
+    expect(error).toBeInTheDocument();
+  });
+});

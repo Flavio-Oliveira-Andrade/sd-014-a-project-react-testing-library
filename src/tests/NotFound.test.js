3e82b87fc72a3504239cb29e8116a3e70b993ba1
Robert Nascimento <robert1a7x@gmail.com>
2021-09-29 16:24:27 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Testes About.js', () => {
+  test('Teste se página tem um heading h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra uma imagem de notFound', () => {
+    renderWithRouter(<NotFound />);
+
+    const IMG_ALT = 'Pikachu crying because the page requested was not found';
+
+    const notFoundImage = screen.getByAltText(IMG_ALT);
+
+    expect(notFoundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

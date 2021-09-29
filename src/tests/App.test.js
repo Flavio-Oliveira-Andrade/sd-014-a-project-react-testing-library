55c23d79335053b836d0d56eac1e63b2be18074f
Anna Hamann <annasoldih@gmail.com>
2021-09-29 14:23:22 -0300

    1. Teste o componente <App.js />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../support/renderWithRouter';
+import App from '../App';
+
+describe('Tests the APP component', () => {
+  beforeEach(() => { renderWithRouter(<App />); });
+
+  it('should have the text Home', () => {
+    expect(screen.getByText(/Home/)).toBeInTheDocument();
+  });
+
+  it('should have the text About', () => {
+    expect(screen.getByText(/About/)).toBeInTheDocument();
+  });
+
+  it('should have the text Favorite Pokémons', () => {
+    expect(screen.getByText(/Favorite Pokémon/)).toBeInTheDocument();
+  });
+});
+

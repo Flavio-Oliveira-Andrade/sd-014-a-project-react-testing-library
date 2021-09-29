4c6105300286c67becf6bd155693efa7c240a371
Douglas Souza <douglassouzabjj@hotmail.com>
2021-09-29 17:08:01 -0300

    c1 - Requisito 1 - Adiciona testes do component App e arquivo util renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../utils/renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente App', () => {
+  it('deve conter um conjunto fixo de links', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favoritePokemonsLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+
+  it('deve ser redirecionada para a página inicial ao clicar no link Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+
+    userEvent.click(homeLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('deve ser rediricionada para a página About ao clicar no link About', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('deve ser redirecionada para a página FavoritePokemons ao clicar no link', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    userEvent.click(favoritesLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('deve ser redirecionada para a página NotFound ao receber uma URL diferente', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/path-qualquer');
+
+    const notFound = screen.getByRole('heading', {
+      name: /Page requested not found/,
+      level: 2,
+    });
+
+    expect(notFound).toBeInTheDocument();
+  });
+});

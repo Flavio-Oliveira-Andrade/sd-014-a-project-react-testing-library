d48526709640b20d7731239a23981e6af5a4dd2a
Lucas Duarte <lucs.rkt@gmail.com>
2021-09-29 19:00:10 -0300

    feat(): added NotFound tests

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('se página contém um heading h2 com o texto Page requested not found 😭', () => {
+    render(<NotFound />);
+    expect(screen.getByRole('heading', {
+      name: /page requested not found/i,
+    })).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem requerida', () => {
+    render(<NotFound />);
+    expect(screen.getByRole('img', {
+      name: /pikachu crying because/i,
+    })).toHaveAttribute('src', URL);
+  });
+});

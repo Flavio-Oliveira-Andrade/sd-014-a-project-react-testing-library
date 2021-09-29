55e5e233b2bff064e365a756a866fb5a4002e3e3
delpapajoao <joaodelpapa1@gmail.com>
2021-09-29 16:43:40 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../uteis/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('testando o NotFound', () => {
+  it('testando se tem um h2 no heading', () => {
+    renderWithRouter(<NotFound />);
+    const found = screen
+      .getByRole('heading', { level: 2, name: 'Page requested not found Crying emoji' });
+    expect(found).toBeInTheDocument();
+  });
+
+  it('teste que a pagina mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

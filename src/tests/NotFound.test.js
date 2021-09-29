b8784af67d59d8458ffd07ed77d2337b5e6be659
Exodin1 <exodin1@outlook.com>
2021-09-29 18:55:25 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('there a h2 heading with text "Page requested not found"', () => {
+  renderWithRouter(<NotFound />);
+  const headingPageNotFound = screen.getByRole('heading', {
+    name: /requested not found/i,
+    level: 2,
+  });
+  expect(headingPageNotFound).toBeInTheDocument();
+});
+
+test('there are pikachu crying img', () => {
+  renderWithRouter(<NotFound />);
+  const pikachuCrying = screen.getByRole('img', { name: /Pikachu crying/i });
+  expect(pikachuCrying.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});

8e5ba932deaccf0b7cd09c32a39866c339a73a35
Écio Ferraz <ecioferraz@hotmail.com>
2021-09-29 18:19:56 -0300

    req 5 incompleto

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import { Pokedex } from '../components';
+
+const renderWithRouter = (component) => {
+  const customHistory = createMemoryHistory();
+  const utils = render(
+    <Router history={ customHistory }>
+      { component }
+    </Router>,
+  );
+
+  return {
+    ...utils,
+    history: customHistory,
+  };
+};
+
+describe('tests Pokedex.js component', () => {
+  it('renders the "Encountered pokémons" heading text', () => {
+    renderWithRouter(<Pokedex />);
+
+    const headingText = screen.getByRole('heading', {
+      level: 2,
+      name: /encountered pokémons/i,
+    });
+    expect(headingText).toBeInTheDocument();
+  });
+});

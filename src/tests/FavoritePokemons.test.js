cb4dc89637ac33c2b55a533e5bd84bd1a7eced3d
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-29 19:16:18 -0300

    retirado o import do App que nao esta sendo usado no FavotitePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components';
 

38ed63ebaf36d4d925cb508d8e401016cbf44ea7
Anna Hamann <annasoldih@gmail.com>
2021-09-29 15:10:11 -0300

    Teste o componente <Pokedex.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -11,22 +11,21 @@ describe('Tests the About component', () => {
   });
 
   it('should have the Pokedex heading', () => {
-    const renderH2 = screen.getByRole('heading', {
+    expect(screen.getByRole('heading', {
       name: /about pokédex/i,
       level: 2,
-    });
-    expect(renderH2).toBeInTheDocument();
+    })).toBeInTheDocument();
   });
 
   it('should have two paragraphs with info about Pokedex', () => {
-    const renderParagraph1 = screen.getByText(/This application simulates a Pokédex/i);
-    const renderParagraph2 = screen.getByText(/One can filter Pokémons by type/i);
-    expect(renderParagraph1).toBeInTheDocument();
-    expect(renderParagraph2).toBeInTheDocument();
+    expect((screen.getByText(/This application simulates a Pokédex/i)))
+      .toBeInTheDocument();
+    expect((screen.getByText(/One can filter Pokémons by type/i)))
+      .toBeInTheDocument();
   });
 
   it('should have a specific Pokedex image', () => {
-    const renderImg = screen.getByRole('img');
-    expect(renderImg.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect((screen.getByRole('img')).src)
+      .toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../support/renderWithRouter';
+import { DATA_TESTID, DATA_TESTID_TYPE, POKE_TYPES } from '../support/noMagicStuff';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Tests the POKEDEX component', () => {
+  beforeEach(() => { renderWithRouter(<App />); });
+  it('should have the Heading with the text Encountered pokémons.', () => {
+    expect(screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+      level: 2,
+    })).toBeInTheDocument();
+  });
+  it('should go to the next pokemon when the button is clicked', () => {
+    const nextPokemonButton = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    const pokemonId = screen.getByTestId(DATA_TESTID);
+    expect(nextPokemonButton).toBeInTheDocument();
+    pokemons.map((pokemon, index) => {
+      expect(pokemonId).toHaveTextContent(pokemon.name);
+      fireEvent.click(nextPokemonButton);
+      return expect(pokemonId).not.toHaveTextContent(pokemons[index].name);
+    });
+    pokemons.forEach((_, index) => {
+      if (index < pokemons.length - 1) fireEvent.click(nextPokemonButton);
+    });
+    expect(pokemonId).toHaveTextContent(pokemons[pokemons.length - 1].name);
+    fireEvent.click(nextPokemonButton);
+    expect(pokemonId).toHaveTextContent(pokemons[0].name);
+  });
+
+  it('should render only one pokemon at a time', () => {
+    expect((screen.getAllByTestId(DATA_TESTID)).length).toBe(1);
+  });
+
+  it('should verify if Pokedex has filter buttons', () => {
+    expect(screen.getAllByTestId(DATA_TESTID_TYPE).length).toBe(POKE_TYPES.length);
+    screen.getAllByTestId(DATA_TESTID_TYPE).map((buttonType, index) => expect(buttonType)
+      .toHaveTextContent(POKE_TYPES[index]));
+    expect(screen.getByRole('button', {
+      name: /All/i,
+    })).toBeInTheDocument();
+  });
+
+  it('should have a button to clean filters', () => {
+    const buttonAllPokemons = screen.getByRole('button', {
+      name: /All/i,
+    });
+    expect(buttonAllPokemons).toBeInTheDocument();
+    fireEvent.click(buttonAllPokemons);
+    pokemons.forEach((_, index) => {
+      expect(screen.getByTestId(DATA_TESTID)).toHaveTextContent(pokemons[index].name);
+      fireEvent.click(screen.getByRole('button', {
+        name: /Próximo pokémon/i,
+      }));
+      expect(buttonAllPokemons).toBeInTheDocument();
+    });
+  });
+});

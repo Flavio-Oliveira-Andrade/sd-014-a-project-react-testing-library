415ed209e12ad902600b6e3f79a2607f276eb2a0
Écio Ferraz <ecioferraz@hotmail.com>
2021-09-29 17:49:17 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+const renderWithRouter = (component) => {
+  const customHistory = createMemoryHistory();
+  const utils = render(
+    <Router history={ customHistory }>
+      { component }
+    </Router>,
+  );
+
+  return {
+    ...utils,
+    history: customHistory,
+  };
+};
+
+describe('tests NotFound.js component', () => {
+  it('renders a "Page requested not found" text', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+
+    const pkmImage = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(pkmImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

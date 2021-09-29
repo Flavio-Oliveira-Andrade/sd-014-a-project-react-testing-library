29bf54808ef17b421502a8fdbf4fef6de924e1ba
Matheus Alves <malves224@gmail.com>
2021-09-29 18:31:53 -0300

    cria teste para quando o componente FavoritePokemons recebe uma lista de favoritos, requisito 3 ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -20,13 +20,30 @@ describe('ao receber lista de pokemnos o component FavoritePokemons renderiza os
       image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
       name: 'Pikachu',
       type: 'Electric',
+    }, {
+      averageWeight: {
+        measurementUnit: 'kg',
+        value: '2.9',
+      },
+      id: 10,
+      image: 'https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png',
+      name: 'Caterpie',
+      type: 'Bug',
     }];
     renderWithRouter(<FavoritePokemons pokemons={ pokemonsFavorited } />);
     const namesFavorited = screen.getAllByTestId('pokemon-name');
-    expect(namesFavorited)
     const typeFavorited = screen.getAllByTestId('pokemon-type');
     const weightFavorited = screen.getAllByTestId('pokemon-weight');
 
-    expect(namesFavorited[0]).toHaveTextContent(/pikachu/i);
+    pokemonsFavorited.forEach((pokemon, index) => {
+      const { name, type, averageWeight: { measurementUnit, value } } = pokemon;
+      expect(namesFavorited[index]).toBeInTheDocument();
+      expect(namesFavorited[index]).toHaveTextContent(name);
+      expect(typeFavorited[index]).toBeInTheDocument();
+      expect(typeFavorited[index]).toHaveTextContent(type);
+      expect(weightFavorited[index]).toBeInTheDocument();
+      expect(weightFavorited[index])
+        .toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
+    });
   });
 });

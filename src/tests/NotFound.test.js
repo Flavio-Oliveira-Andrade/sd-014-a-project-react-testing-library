e9b6b373fbf738a815e4538fde71dcd30ed68cd2
BrianCaetano <brian-caetano@hotmail.com>
2021-09-28 21:57:33 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente NotFound.js', () => {
+  test('página contém um heading h2 com o texto Page requested not found 😭; ', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <NotFound />
+      </Router>,
+    );
+
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+
+  test('página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif ', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <NotFound />
+      </Router>,
+    );
+
+    const img = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(img.src).toBe(
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});

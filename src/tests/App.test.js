8f478f15a179f28df1d96534e3efff4159028326
Filipe <andradefilipe2002@gmail.com>
2021-09-29 08:30:53 -0300

    Requisito 1, testa o componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import RenderWithRouter from '../RenderWithRouter';
+
+describe('Testes para o app.js', () => {
+  test('Primeiro link deve possuir texto Home', () => {
+    RenderWithRouter(<App />);
+    const homeLink = screen.getByText('Home');
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  test('Segundo link deve possuir texto About', () => {
+    RenderWithRouter(<App />);
+    const aboutLink = screen.getByText('About');
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  test('Terceiro link deve possuir texto Favorite Pokémons', () => {
+    RenderWithRouter(<App />);
+    const favPokeLink = screen.getByText('Favorite Pokémons');
+    expect(favPokeLink).toBeInTheDocument();
+  });
+});

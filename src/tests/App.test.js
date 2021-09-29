03e1d0d4bd6db3804638bcd6e4eb487bbfaf19ad
brunofeu <brunofeu89@gmail.com>
2021-09-29 15:09:28 -0300

    fix requirement 01 lint error

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 import renderWithRouter from './util/renderWithRouter';
 

f2c512df5e2a2afdd68b5112926b64a56ccd5a81
Écio Ferraz <ecioferraz@hotmail.com>
2021-09-29 15:35:02 -0300

    req 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,42 @@
 import React from 'react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
 
-test('', () => {});
+const renderWithRouter = (component) => {
+  const customHistory = createMemoryHistory();
+  const utils = render(
+    <Router history={ customHistory }>
+      { component }
+    </Router>,
+  );
+
+  return {
+    ...utils,
+    history: customHistory,
+  };
+};
+
+describe('tests App.js component', () => {
+  it('renders "Home" text on the first link', () => {
+    renderWithRouter(<App />);
+
+    const homeText = screen.getByText(/home/i);
+    expect(homeText).toBeInTheDocument();
+  });
+
+  it('renders "About" text on the first link', () => {
+    renderWithRouter(<App />);
+
+    const aboutText = screen.getByText(/about/i);
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  it('renders "Favorite Pokémons" text on the first link', () => {
+    renderWithRouter(<App />);
+
+    const favoritePkmText = screen.getByText(/favorite pokémons/i);
+    expect(favoritePkmText).toBeInTheDocument();
+  });
+});

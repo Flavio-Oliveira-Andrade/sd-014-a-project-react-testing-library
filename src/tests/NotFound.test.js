99ad451c6e16b7e9cfff5b96e57f34fcc3e2b488
VSSSP <victorssspaula@live.com>
2021-09-29 16:44:52 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testa o componente NotFound.js', () => {
+  it('Deveria conter o texto "Page requested not found" em um h2', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+  it('Deveria mostrar a imagem do Pikachu chorando 😭😭😭', () => {
+    renderWithRouter(<NotFound />);
+    const gif = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    const gifSource = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(gif.src).toBe(gifSource);
+  });
+});

0486f72c002f2f124b478ffc947673fc528712a2
leodiber <leonardodiber@gmail.com>
2021-09-29 16:54:26 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa se página NotFound: ', () => {
+  it('renderiza um h2 com o texto "Page requested not found 😭"', () => {
+    render(<NotFound />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renderiza uma mensagem com a url providenciada', () => {
+    render(<NotFound />);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const img = screen.getByRole('img', { name: /pikachu crying/i });
+
+    expect(img).toHaveAttribute('src', url);
+  });
+});

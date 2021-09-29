0007318aaf26c6551e6d36959b60107810d40d10
rodPinheiro <rod_bma@hotmail.com>
2021-09-29 17:14:11 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Testa o component FavoritePokemons', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[0]] } />);
+    const pokemon = screen.getByTestId('pokemon-name');
+    expect(pokemon).toBeInTheDocument();
+  });
+});

61ed020810a6ee29a3b82b1602f12968b57fa906
Pally <francisca.bezerr@gmail.com>
2021-09-29 18:25:07 -0300

    questao 1 ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../services/RenderWithRouter';
 import App from '../App';
 
@@ -14,17 +15,35 @@ describe('Testa o componente <App.js/>', () => {
     expect(favorite).toBeInTheDocument();
   });
   test('Testa se há redirecionamento para a página inicial  ao clicar em Home', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    const { pathname } = history.location; //  para verificar se estamos na página correta
+    // interagir com elementos
+    fireEvent.click(home);
+    // teste
+    expect(pathname).toBe('/');
   });
   test('Testa se há redirecionamento para a página About, ao clicar em About.', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /About/i });
+    fireEvent.click(about);
+    const { pathname } = history.location; //  para verificar se estamos na página correta
+    // teste
+    expect(pathname).toBe('/about');
   });
   test('Testa se há redirecionamento para Favoritados, ao clicar em Favorite', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    fireEvent.click(favorite);
+    const { pathname } = history.location; //  para verificar se estamos na página correta
+    // teste
+    expect(pathname).toBe('/favorites');
   });
   test('Testa se é redirecionado para Not Found ao entrar em URL desconhecida.', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    history.push('/inexistente/'); //  função history.push() e passamos como argumento algum link que não existe dentro de nossa aplicação
+    const not = screen.getByText(/Page requested not found/i);
+    expect(not).toBeInTheDocument();
+    // Depois disso, testamos se o texto que aparece no navegador, ao digitar um caminho para uma página que não existe, é encontrado. (fonte: course)
   });
 });
-
-// alou mundo

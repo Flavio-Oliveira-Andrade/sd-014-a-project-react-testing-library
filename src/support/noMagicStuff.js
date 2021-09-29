f4b8d9826f77e7bc83c562331918646ee27ebf16
Anna Hamann <annasoldih@gmail.com>
2021-09-29 14:19:04 -0300

    creates file for no magic stuff

---
+++ src/support/noMagicStuff.js
@@ -0,0 +1,11 @@
+export const MORE_DETAILS = 'More details';
+export const NEXT_POKEMON = 'Próximo pokémon';
+export const FAVORITE = 'Pokémon favoritado?';
+export const PAGE_FAVORITES = 'Favorite Pokémons';
+export const PAGE_HOME = 'Home';
+export const DATA_TESTID = 'pokemon-name';
+export const DATA_TYPE = 'pokemon-type';
+export const DATA_WEIGHT = 'pokemon-weight';
+export const DATA_TESTID_TYPE = 'pokemon-type-button';
+export const POKE_TYPES = ['Electric', 'Fire', 'Bug', 'Poison',
+  'Psychic', 'Normal', 'Dragon'];

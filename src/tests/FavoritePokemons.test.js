d2f41782a8e36962a123862d81c909619f181e54
Williamns <williamnstolb@gmail.com>
2021-09-29 18:59:33 -0300

    Requisito 2 completo e 3 testando

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,103 @@
-test('', () => {});
+import React from 'react';
+import {
+  screen,
+} from '@testing-library/react';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+const objectFavoritesPokemon = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard'
+    + 'berries with electricity to make them tender enough to eat.',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+  },
+  {
+    id: 23,
+    name: 'Ekans',
+    type: 'Poison',
+    averageWeight: {
+      value: '6.9',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Goldenrod Game Corner',
+        map: 'https://cdn2.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png',
+      },
+    ],
+    summary: 'It can freely detach its jaw to swallow large prey whole. It can become too heavy to move, however.',
+  },
+];
+
+test('se é exibido na tela a mensagem "No favorite pokemon found".', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/favorites');
+
+  const noFavoriteFound = screen.getByText(/No favorite pokemon found/i);
+  expect(noFavoriteFound).toBeInTheDocument();
+});
+
+test('se é exibido todos os cards de pokémons favoritados.', () => {
+  const { history } = renderWithRouter(
+    <FavoritePokemons pokemons={ objectFavoritesPokemon } />,
+  );
+
+  history.push('/favorites');
+
+  const cardsFavoritesPokemon = screen.queryAllByText('More details').length;
+  const lengthArray = 3;
+  expect(cardsFavoritesPokemon).toEqual(lengthArray);
+});

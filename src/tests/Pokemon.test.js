ae8fb808a27c72ef6eb921520252f338407cddd9
paulodetasso <xcloudgameemail@gmail.com>
2021-09-29 16:57:36 -0300

    Corrige a 5

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import Pokemon from '../components/Pokemon';
+// import renderWithRouter from './renderWithRouter';
+
+// describe('test Pokemon', () => {
+//   it('', () => {
+//     renderWithRouter(<Pokemon />);
+//   });
+//   it('', () => {
+//     renderWithRouter(<Pokemon />);
+//   });
+//   it('', () => {
+//     renderWithRouter(<Pokemon />);
+//   });
+// });

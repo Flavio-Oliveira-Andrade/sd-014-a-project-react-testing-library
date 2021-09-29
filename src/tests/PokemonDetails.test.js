5f09525328347e74d73253d897317dbfa93512fe
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-09-29 14:14:10 -0300

    Ops :P

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import data from '../data';
+
+const pokemon = data[0];
+
+test('Teste o componente <PokemonDetails.js />', () => {
+  renderWithRouter(<App />);
+
+  fireEvent.click(
+    screen.getByText('More details'),
+  );
+
+  expect(
+    screen.getByText(`${pokemon.name}`),
+  ).toBeInTheDocument();
+
+  expect(
+    screen.queryByText('More details'),
+  ).not.toBeInTheDocument();
+
+  expect(
+    screen.getAllByRole('heading', { level: 2 })[1].textContent,
+  ).toBe('Summary');
+
+  expect(
+    screen.getByText(`${pokemon.summary}`),
+  ).toBeInTheDocument();
+
+
+});

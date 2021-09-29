1397a357576b569bfd9b7eff2ad579f4f1e34f47
BlyNTC <isaacase@hotmail.com>
2021-09-28 22:02:42 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
+
+describe('Testando o componente FavoritePokemons', () => {
+  it('é exibido na tela a mensagem No favorite pokemon found,'
+  + ' se a pessoa não tiver pokémons favoritos.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText(/no favorite/i);
+    expect(message).toBeInTheDocument();
+  });
+  it('é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[0]] } />);
+    const pokemon = screen.getByText(/Pikachu/i);
+    expect(pokemon).toBeInTheDocument();
+  });
+  // baseado no codigo desse repositorio abaixo;
+  // https://github.com/tryber/sd-013-a-project-react-testing-library/tree/pedroalles-react-testing
+});

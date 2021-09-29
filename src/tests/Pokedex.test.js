85cc5c28e2baf15f11203351ff8f79780cce6ab3
brunofeu <brunofeu89@gmail.com>
2021-09-29 19:45:07 -0300

    debug lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,6 +4,8 @@ import React from 'react';
 import App from '../App';
 import renderWithRouter from './util/renderWithRouter';
 
+const pokemonName = 'pokemon-name';
+const pokemonType = 'pokemon-type';
 describe('Teste do componente <Pokedex.js />', () => {
   it('Verifica se página contém um heading h2 com o texto "Encountered pokémons"', () => {
     renderWithRouter(<App />);
@@ -13,17 +15,18 @@ describe('Teste do componente <Pokedex.js />', () => {
     });
     expect(encounteredText).toBeInTheDocument();
   });
-  it('Verifica se é exibido o próximo Pokémon da lista quando o botão "Próximo pokémon" é clicado', () => {
+  it('Verifica se é exibido o próximo Pokémon da lista quando o'
+  + ' botão "Próximo pokémon" é clicado', () => {
     renderWithRouter(<App />);
-    const prevPokemonName = screen.getByTestId('pokemon-name');
+    const prevPokemonName = screen.getByTestId(pokemonName);
     const nextButton = screen.getByRole('button', { name: /próximo pokémon/i });
     userEvent.click(nextButton);
-    const nextPokemonName = screen.getAllByTestId('pokemon-name');
+    const nextPokemonName = screen.getAllByTestId(pokemonName);
     expect(prevPokemonName).not.toEqual(nextPokemonName);
   });
   it('Verifica se é mostrado apenas um Pokémon por vez.', () => {
     renderWithRouter(<App />);
-    const pokemonRendered = screen.getAllByTestId('pokemon-name');
+    const pokemonRendered = screen.getAllByTestId(pokemonName);
     expect(pokemonRendered).toHaveLength(1);
   });
 
@@ -31,17 +34,17 @@ describe('Teste do componente <Pokedex.js />', () => {
     renderWithRouter(<App />);
     const filterButton = screen.getAllByTestId('pokemon-type-button');
     userEvent.click(filterButton[0]);
-    const filter = screen.getByTestId('pokemon-type');
+    const filter = screen.getByTestId(pokemonType);
     expect(filter.innerHTML).toBe(filterButton[0].innerHTML);
   });
   it('Verifica se a Pokédex contém um botão para resetar o filtro', () => {
     renderWithRouter(<App />);
     const resetButton = screen.getByRole('button', { name: /all/i });
     userEvent.click(resetButton);
-    const prevType = screen.getByTestId('pokemon-type').innerHTML;
+    const prevType = screen.getByTestId(pokemonType).innerHTML;
     const nextButton = screen.getByRole('button', { name: /próximo pokémon/i });
     userEvent.click(nextButton);
-    const newType = screen.getByTestId('pokemon-type').innerHTML;
+    const newType = screen.getByTestId(pokemonType).innerHTML;
     expect(newType).not.toEqual(prevType);
   });
 });

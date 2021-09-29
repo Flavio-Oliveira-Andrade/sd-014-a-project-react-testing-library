229156e2f8d7941ddbd06663bdbaf24fa35bb4d8
paulodetasso <xcloudgameemail@gmail.com>
2021-09-29 13:47:30 -0300

    requisito 4 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+
+
+describe('test NotFound', () => {
+  
+  it('should contain a h2 with text', () => {
+    const { getByRole } = renderWithRouter(<NotFound />)
+    const heading = getByRole('heading', { 
+      level: 2,
+      name: /Page requested not found/i
+    })
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should have an image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif')
+  });
+});
+

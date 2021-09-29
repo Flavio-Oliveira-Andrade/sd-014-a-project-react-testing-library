5a6307974578a065eb3da55c052e5b47b964e197
Saulo Lima <saulolima.c@protonmail.com>
2021-09-29 18:18:06 -0300

    Requisito 2 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testa a página About', () => {
+  beforeEach(() => render(<About />));
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    const h2 = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const paragrafos = screen.getAllByText(/Pokémons/i);
+    expect(paragrafos).toHaveLength(2);
+    paragrafos.forEach((p) => expect(p).toBeInTheDocument());
+  });
+
+  it('Testa se a página contém uma imagem de uma Pokédex', () => {
+    const pokedexImg = screen.getByRole('img');
+    expect(pokedexImg).toBeInTheDocument();
+    expect(pokedexImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(pokedexImg).toHaveAttribute('alt', 'Pokédex');
+  });
+});

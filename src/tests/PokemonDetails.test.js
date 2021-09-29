b2cb874a895ffe8b100d6c3dbe4fa3eca99e1bc7
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-09-29 14:41:21 -0300

    Requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -5,6 +5,7 @@ import App from '../App';
 import data from '../data';
 
 const pokemon = data[0];
+const NUM_OF_IMG = 3;
 
 test('Teste o componente <PokemonDetails.js />', () => {
   renderWithRouter(<App />);
@@ -18,6 +19,10 @@ test('Teste o componente <PokemonDetails.js />', () => {
   ).toBeInTheDocument();
 
   expect(
+    screen.getAllByRole('heading', { level: 2 })[0].textContent,
+  ).toBe(`${pokemon.name} Details`);
+
+  expect(
     screen.queryByText('More details'),
   ).not.toBeInTheDocument();
 
@@ -28,4 +33,38 @@ test('Teste o componente <PokemonDetails.js />', () => {
   expect(
     screen.getByText(`${pokemon.summary}`),
   ).toBeInTheDocument();
+
+  expect(
+    screen.getAllByRole('heading', { level: 2 })[2].textContent,
+  ).toBe(`Game Locations of ${pokemon.name}`);
+
+  pokemon.foundAt.forEach((place, i) => {
+    expect(
+      screen.getByText(place.location),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getAllByAltText(`${pokemon.name} location`)[i].src,
+    ).toBe(place.map);
+  });
+
+  expect(
+    screen.getAllByAltText(`${pokemon.name} location`),
+  ).toHaveLength(pokemon.foundAt.length);
+
+  fireEvent.click(
+    screen.getByText('Pokémon favoritado?'),
+  );
+
+  expect(
+    screen.getAllByRole('img'),
+  ).toHaveLength(NUM_OF_IMG + 1);
+
+  fireEvent.click(
+    screen.getByText('Pokémon favoritado?'),
+  );
+
+  expect(
+    screen.getAllByRole('img'),
+  ).toHaveLength(NUM_OF_IMG);
 });

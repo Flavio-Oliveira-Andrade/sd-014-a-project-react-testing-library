d817cefe9855ce23d9b6b545ca60f708756c37cd
jagrunewald <je_grunewald@hotmail.com>
2021-09-29 19:03:45 -0300

    Requisito 1 - App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,66 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import NotFound from '../components/NotFound';
+
+// Requisito 1
+test('testa se a aplicação é redirecionada para a página inicial, '
++ 'na URL / ao clicar no link Home da barra de navegação', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const homeLink = screen.getByRole('link', {
+    name: /home/i,
+  });
+  expect(homeLink).toBeInTheDocument();
+  userEvent.click(homeLink);
+});
+
+// Requisito 1
+test('testa se a aplicação é redirecionada para a página de About, '
++ 'na URL /about, ao clicar no link About da barra de navegação', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const aboutLink = screen.getByRole('link', {
+    name: /about/i,
+  });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+});
+
+// Requisito 1
+test('testa se a aplicação é redirecionada para a página de Pokémons Favoritados '
++ 'na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const favoriteLink = screen.getByRole('link', {
+    name: /favorite/i,
+  });
+  expect(favoriteLink).toBeInTheDocument();
+  userEvent.click(favoriteLink);
+});
+
+// Requisito 1
+test('testa se a aplicação é redirecionada para a página Not Found '
++ 'ao entrar em uma URL desconhecida', () => {
+  render(<NotFound />);
+
+  const notFoundLink = screen.getByRole('heading', {
+    level: 2,
+    name: /not found/i,
+  });
+  expect(notFoundLink).toBeInTheDocument();
+});

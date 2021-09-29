15bfd2edc69b991c15f7a1227ae9b6b03269318f
WalmirLucena <walmirlucena3@gmail.com>
2021-09-29 16:03:53 -0400

    Requsito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './utils/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testa o componente NotFound', () => {
+  test('Testa se página contém um h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+
+    const pageNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(pageNotFound).toBeInTheDocument();
+  });
+  test('Testa se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imgNotFound).toBeInTheDocument();
+  });
+});

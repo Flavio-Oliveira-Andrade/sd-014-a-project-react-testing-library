a6b18aaaf11301a1b09e30f0b1de7e2a2a3a77e9
Hugoe-Emannuel <85767023+Hugoe-Emannuel@users.noreply.github.com>
2021-09-29 15:47:53 -0300

    requisito 2-ajustes2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -15,19 +15,18 @@ describe('Teste o componente About.js.', () => {
   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
     const { history } = historyFunction(<App />);
     history.push('/about');
-    const paragraph1 = screen.getByText('This application simulates a Pokédex,'
-    + ' a digital encyclopedia containing all Pokémons');
-
-    const paragraph2 = screen.getByText('One can filter Pokémons by type,'
-    + ' and see more details for each one of them');
+    const paragraph1 = screen.getByText(/This application simulates a Pokédex/);
+    const paragraph2 = screen.getByText(/One can filter Pokémons by type/);
 
     expect(paragraph1).toBeInTheDocument();
     expect(paragraph2).toBeInTheDocument();
   });
+
   test('Teste se a página contém a imagem de uma Pokédex', () => {
     const { history } = historyFunction(<App />);
     history.push('/about');
     const imgAbout = screen.getByAltText('Pokédex');
+    expect(imgAbout).toBeInTheDocument();
     expect(imgAbout).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });

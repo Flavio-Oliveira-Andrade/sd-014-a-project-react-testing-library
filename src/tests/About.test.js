6c7a597b1b44e9d8a7b64faef0f7df7017a04d25
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-09-29 18:04:37 -0300

    Requisito 2: teste <About />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { About } from '../components';
+
+// https://testing-library.com/docs/example-react-router/#reducing-boilerplate
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+  return render(ui, { wrapper: BrowserRouter });
+};
+
+describe('Testa o componente <About.js />', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /about pokédex/i,
+    });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragrafo1 = screen.getByText('This application simulates a Pokédex,'
+      + ' a digital encyclopedia containing all Pokémons');
+    expect(paragrafo1).toBeInTheDocument();
+    const paragrafo2 = screen.getByText('One can filter Pokémons by type,'
+      + ' and see more details for each one of them');
+    expect(paragrafo2).toBeInTheDocument();
+  });
+
+  // https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src
+  test('Testa se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokImg = screen.getByAltText('Pokédex');
+    expect(pokImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

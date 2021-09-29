16a59b0a7cd3e2fb7a55f354eb65c925d045c8b5
leodiber <leonardodiber@gmail.com>
2021-09-29 16:14:13 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('About page', () => {
+  it('should render an h2 heading with "About Pokedex" text', () => {
+    render(<About />);
+    const title = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('should render information about Pokédex', () => {
+    render(<About />);
+
+    const firstParagraph = screen.getByText(/This application simulates a Pokédex/i);
+    const secondParagraph = screen.getByText(/One can filter Pokémons by type/i);
+
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  it('should render the information in two paragraphs', () => {
+    render(<About />);
+    // https://www.ocpsoft.org/tutorials/regular-expressions/or-in-regex/
+    const paragraphs = screen.getAllByText(/this application simulates|one can filter/i);
+
+    expect(paragraphs).toHaveLength(2);
+  });
+
+  it('should render an img with provided source', () => {
+    render(<About />);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', url);
+  });
+});

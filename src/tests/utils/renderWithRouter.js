c7bbbe6f17f743df5f21f7950a011562df96ae67
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-29 15:24:39 -0300

    Create renderWithRouter function

---
+++ src/tests/utils/renderWithRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { render } from '@testing-library/react';
+
+export default function renderWithRouter(component) {
+  const history = createBrowserHistory();
+  return {
+    ...render(<Router history={ history }>{component}</Router>),
+    history,
+  };
+}

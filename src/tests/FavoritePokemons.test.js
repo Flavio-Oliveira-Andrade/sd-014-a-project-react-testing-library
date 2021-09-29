129c0c545aa2342491a4a225b31eccba9a9b503a
Écio Ferraz <ecioferraz@hotmail.com>
2021-09-29 16:58:37 -0300

    req 3 50/50

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+
+const renderWithRouter = (component) => {
+  const customHistory = createMemoryHistory();
+  const utils = render(
+    <Router history={ customHistory }>
+      { component }
+    </Router>,
+  );
+
+  return {
+    ...utils,
+    history: customHistory,
+  };
+};
+
+describe('tests FavoritePokemons.js component', () => {
+  it('renders the "No favorite pokemon found" text', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavFound = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavFound).toBeInTheDocument();
+  });
+});

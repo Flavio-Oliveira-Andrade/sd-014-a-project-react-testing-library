eacd020390165d5bddaea25cda9d8e90c51d3fd7
Welton Lopes <weltonlopesmetal@gmail.com>
2021-09-29 14:15:36 -0300

    Primeiro

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+
+import App from '../App';
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={ history }>{ui}</Router>),
+    history,
+  };
+}
+
+describe('Teste se o topo da aplicaçãocontémumconjunto fixo de links denavegação', () => {
+  test('O primeiro link deve possuir o texto Home', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+
+    expect(home).toBeInTheDocument();
+
+    fireEvent.click(getByText(/Projetos/i));
+
+    const project = getByText(/Projeto 1/i);
+    expect(project).toBeInTheDocument();
+  });
+});

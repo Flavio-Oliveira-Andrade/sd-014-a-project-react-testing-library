354e9ffe062de1be329fa1ae2a7fde7495426ee9
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-09-29 14:45:12 -0300

    Requisito 5 finalizado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -6,6 +6,8 @@ import getPokemonsTypes from '../utils/getPokemonsTypes';
 import Pokedex from '../components/Pokedex';
 import pokemons from '../data';
 
+const nextPokemonButton = 'Próximo pokémon';
+
 describe('Testa o componente <Pokedex.js />', () => {
   test('Teste se página contém um heading h2 com o texto Encountered pokémons',
     () => {
@@ -18,7 +20,7 @@ describe('Testa o componente <Pokedex.js />', () => {
     () => {
       const firstPokemon = pokemons[0].name;
       renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ [] } />);
-      const nextButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+      const nextButton = screen.getByRole('button', { name: nextPokemonButton });
       expect(nextButton).toBeInTheDocument();
 
       pokemons.forEach(({ name }) => {
@@ -31,7 +33,7 @@ describe('Testa o componente <Pokedex.js />', () => {
 
   test('Teste se é mostrado apenas um Pokémon por vez.', () => {
     renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ [] } />);
-    const nextButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const nextButton = screen.getByRole('button', { name: nextPokemonButton });
     let onScreenPokemons;
 
     pokemons.forEach(({ name }) => {
@@ -45,25 +47,25 @@ describe('Testa o componente <Pokedex.js />', () => {
   test('Teste se a Pokédex tem os botões de filtro.', () => {
     renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ [] } />);
     const allButton = screen.getByRole('button', { name: 'All' });
-    const nextButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const nextButton = screen.getByRole('button', { name: nextPokemonButton });
     const types = getPokemonsTypes(pokemons);
 
     const typesButtons = screen.getAllByTestId('pokemon-type-button');
-    expect(typesButtons.length).toBe(7);
+    expect(typesButtons.length).toBe(Object.keys(types).length);
 
     expect(allButton).toBeInTheDocument();
 
-    // Object.keys(types).forEach((type) => {
-    //   const typeButton = screen.getAllByRole('button', { name: type });
-    //   expect(typeButton.length).toBe(1);
-
-    //   userEvent.click(typeButton[0]);
-    //   for (let index = 1; index <= types[type]; index += 1) {
-    //     const typeOnScreen = screen.getByTestId('pokemon-type');
-    //     expect(typeOnScreen).toHaveTextContent(type);
-    //     expect(allButton).toBeInTheDocument();
-    //     userEvent.click(nextButton);
-    //   }
-    // });
+    Object.keys(types).forEach((type) => {
+      const typeButton = screen.getByRole('button', { name: type });
+      userEvent.click(typeButton);
+      for (let index = 1; index <= types[type]; index += 1) {
+        const typeOnScreen = screen.getByTestId('pokemon-type');
+        expect(typeOnScreen).toHaveTextContent(type);
+        expect(allButton).toBeInTheDocument();
+        userEvent.click(nextButton);
+      }
+    });
+    expect(allButton).toBeInTheDocument();
+    userEvent.click(allButton);
   });
 });

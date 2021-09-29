454f5028f88ae64c743cf8f9c5e09945e12dca94
leodiber <leonardodiber@gmail.com>
2021-09-29 16:45:45 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../utils/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Testa se a página de favoritos: ', () => {
+  it('Exibe "No favorite pokemon found" caso não haja pokemon favorito', () => {
+    render(<FavoritePokemons />);
+
+    const message = screen.getByText(/no favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+
+  it('Exibe o pokemon favorito caso haja algum', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const details = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(details);
+
+    const checkbox = screen.getByRole('checkbox', { name: /favoritado/i });
+    userEvent.click(checkbox);
+
+    history.push('/favorites');
+    const pokemon = screen.getByText(/pikachu/i);
+    expect(pokemon).toBeInTheDocument();
+  });
+});

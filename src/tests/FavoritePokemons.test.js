84e9aa456522a892e5a43c7e731ea7270a3165e8
Robert Nascimento <robert1a7x@gmail.com>
2021-09-29 16:09:24 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../utils/renderWithRouter';
+import pokemons from '../data';
+
+describe('Testes FavoritePokemons.js', () => {
+  test('Teste se a mensagem No favorite pokemon found aparece na tela', () => {
+    renderWithRouter(<App />);
+
+    const favoritesLink = screen.getByText('Favorite Pokémons');
+    userEvent.click(favoritesLink);
+
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+    expect(screen.getByText('Charmander')).toBeInTheDocument();
+  });
+});

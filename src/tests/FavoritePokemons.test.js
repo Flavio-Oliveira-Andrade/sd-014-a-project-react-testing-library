2243f631d921a69049d569dda76e96412dc00147
VSSSP <victorssspaula@live.com>
2021-09-29 16:27:16 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../services/renderWithRouter';
+import data from '../data';
+
+describe('Testa o component FavoritePokemons.js', () => {
+  it('Deveria exibir "No favorite pokemon found" se não existirem pokémons favoritos',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+      const noFavorites = screen.getByText('No favorite pokemon found');
+      expect(noFavorites).toBeInTheDocument();
+    });
+  it('Deveria exibir os pokémons favoritos', () => {
+    const snorlaxID = 143;
+    const mewID = 151;
+    const pokemons = data.filter((pokemon) => pokemon.id === snorlaxID
+      || pokemon.id === mewID);
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    expect(screen.getByText('Snorlax')).toBeInTheDocument();
+    expect(screen.getByText('Mew')).toBeInTheDocument();
+  });
+});

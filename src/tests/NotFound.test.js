3def14a0741279619e19c6290d61fc8f4c0c9758
Eduardo Rocha <eduardorocha-dev@protonmail.ch>
2021-09-29 20:02:34 -0300

    Satisfaz os casos de teste do componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('3 - Testes do componente NotFound.js', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  it('Deve haver o texto "Page requestednot found"', () => {
+    const pageNotFoundText = screen.getByText(/Page requested not found/i);
+
+    expect(pageNotFoundText).toBeInTheDocument();
+  });
+
+  it('Deve haver uma imagem personalizada', () => {
+    const pageNotFoundImage = screen.getByAltText(/Pikachu crying/i);
+
+    expect(pageNotFoundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(pageNotFoundImage).toBeInTheDocument();
+  });
+});

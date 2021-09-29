98a261787577e5cf0e174fdcd82aab0f4349235f
Dayane <dayanebmartins@gmail.com>
2021-09-29 19:11:49 -0300

    desenvolvendo requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { fireEvent } from '@testing-library/dom';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '/App';
+
+describe ('testando componente App', () => {
+  test('deve renderizar o componente App', () => {
+    const { getByText, history} = renderWithRouter(<App/>)
+    fireEvent.click(getByText(/Home/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/Home');
+    const home = getByText(/Home/);
+    expect(home).toBeInTheDocument();
+  });
+  
+})
+

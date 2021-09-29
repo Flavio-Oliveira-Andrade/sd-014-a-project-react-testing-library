fa18efb771d56c8e8052d46544de95b1d02f074e
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-29 15:21:36 -0300

    Requisito 1 - testando o App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,62 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
 
-test('', () => {});
+describe('Testando o componete App', () => {
+  test('Verificando se possui o link'
+  + '"Home" e ao clicar aparece o texto "Encountered pokémons"', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+
+    expect(linkHome).toBeInTheDocument();
+
+    userEvent.click(linkHome);
+    const elementH2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+    expect(elementH2).toBeInTheDocument();
+  });
+
+  test('Link "About" e ao clicar aparece o texto "About Pokédex"', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    userEvent.click(linkAbout);
+    const aboutH2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutH2).toBeInTheDocument();
+  });
+
+  test('Verificando "Favorito" e ao clicar aparece o texto "Favorite Pokémons"', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavorite).toBeInTheDocument();
+
+    userEvent.click(linkFavorite);
+    const aboutH2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'Favorite pokémons',
+    });
+    expect(aboutH2).toBeInTheDocument();
+  });
+});

86bf3ba24ec027ce2b079616951e0c1c5c5c0265
rodPinheiro <rod_bma@hotmail.com>
2021-09-29 16:23:43 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Teste o componente App', () => {
+  beforeEach(() => renderWithRouter(<App />));
+  it('se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const links = screen.getAllByRole('link');
+    expect(links[0].href).toBe('http://localhost/');
+    expect(links[0].innerHTML).toBe('Home');
+    expect(links[1].href).toBe('http://localhost/about');
+    expect(links[1].innerHTML).toBe('About');
+    expect(links[2].href).toBe('http://localhost/favorites');
+    expect(links[2].innerHTML).toBe('Favorite Pokémons');
+  });
+});

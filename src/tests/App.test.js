d885f5665f6b8530847b81ecd8fd4f12000a969c
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-09-29 14:35:51 -0300

    verificar se o component app esta sendo renderizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+test('teste para verificar se o component App está sendo renderizado', () => {
+  render(
+    <Router history={ createMemoryHistory('/') }>
+      <App />
+    </Router>,
+  );
+  const titulo = screen.getByRole('heading', { level: 1, name: 'Pokédex' });
+  expect(titulo).toBeInTheDocument();
+});

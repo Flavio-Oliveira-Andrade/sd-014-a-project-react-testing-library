2df313c31b1f641d4faeb756f0a9ab5f5b406832
Williamns <williamnstolb@gmail.com>
2021-09-29 19:46:09 -0300

    Requisito 3 completo e 4 testando

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import {
+  screen,
+} from '@testing-library/react';
+
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+test('se página contém um heading h2 com o texto "Page requested not found"', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/pageNotFound');
+
+  const pageNotFound = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+  expect(pageNotFound).toBeInTheDocument();
+});
+
+test('se página mostra a imagem', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/pageNotFound');
+
+  const imgPokedex = screen
+    .getByAltText('Pikachu crying because the page requested was not found');
+  expect(imgPokedex).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});

ee6d9ce0cbf15c01f79bacf726521f568782cc7b
Pally <francisca.bezerr@gmail.com>
2021-09-29 18:26:19 -0300

    conserta erro lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../services/RenderWithRouter';
 import App from '../App';
 

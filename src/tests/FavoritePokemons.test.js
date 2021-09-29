6e6cba9a0eaa40ae350129a3cab0a8fefd9f560e
Lucas Duarte <lucs.rkt@gmail.com>
2021-09-29 18:23:08 -0300

    feat(): added FavoritePokemons.test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+const POKEMONS_LENGTH = pokemons.length;
+
+describe('Teste o componente <FavoritePokemons />', () => {
+  it('se é exibido "No favorite pokemon found", se não tiver pokémons favoritos.', () => {
+    render(<FavoritePokemons pokemons={ [] } />);
+    expect(screen.getByText(/no favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  it('se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    expect(screen.getAllByTestId('pokemon-name')).toHaveLength(POKEMONS_LENGTH);
+  });
+});

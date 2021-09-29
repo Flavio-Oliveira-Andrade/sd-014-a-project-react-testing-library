c98f3f34f8f29df87c3bc80ef2cfa6894ae3a4d3
thiagoalmeida22 <thiagoguimaraesalmeida@gmail.com>
2021-09-29 18:47:40 -0300

    Req 2 feito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { About } from '../components';
+
+describe('2 - Teste se a página contém as informações sobre a Pokédex', () => {
+  it('Teste a presença de alguns contéudos',
+    () => {
+      render(
+        <MemoryRouter>
+          <About />
+        </MemoryRouter>,
+      );
+      const header = screen.getByRole('heading', {
+        level: 2,
+        name: /About Pokédex/i,
+      });
+      const image = screen.getByRole('img');
+      expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+      expect(header).toBeInTheDocument();
+
+      expect(screen.getByText(/This application simulates/i)).toBeInTheDocument();
+      expect(screen.getByText(/One can filter Pokémons/i)).toBeInTheDocument();
+
+
+    });
+});

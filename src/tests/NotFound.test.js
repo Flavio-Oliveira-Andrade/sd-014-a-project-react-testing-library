70b625fc0b06484b9c0d1d0e7a535a8ebc1dfa0d
rodPinheiro <rod_bma@hotmail.com>
2021-09-29 17:27:19 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente NotFound', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+  });
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    const noMatch = screen.getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const img = screen.getByRole('img', { name: /Pikachu crying because/i });
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(img).toBeInTheDocument();
+  });
+});

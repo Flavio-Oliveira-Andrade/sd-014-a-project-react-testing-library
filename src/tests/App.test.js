c999313b17075decd19b05f54a7eb95103925e5b
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-09-29 15:14:45 -0300

    stryker app 100%

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,12 +4,29 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import App from '../App';
 
-test('teste para verificar se o component App está sendo renderizado', () => {
-  render(
-    <Router history={ createMemoryHistory('/') }>
-      <App />
-    </Router>,
-  );
-  const titulo = screen.getByRole('heading', { level: 1, name: 'Pokédex' });
-  expect(titulo).toBeInTheDocument();
+const renderApp = () => render(
+  <Router history={ createMemoryHistory('/') }>
+    <App />
+  </Router>,
+);
+
+describe('Testes do render App', () => {
+  test('teste para verificar se o component App está sendo renderizado', () => {
+    renderApp();
+  });
+  test('verifica se o link "Home" aparece na tela', () => {
+    renderApp();
+    const home = screen.getByRole('link', { name: /home/i });
+    expect(home).toBeInTheDocument();
+  });
+  test('verifica se o link "About" aparece na tela', () => {
+    renderApp();
+    const about = screen.getByRole('link', { name: /about/i });
+    expect(about).toBeInTheDocument();
+  });
+  test('verifica se o link "Favorite Pokémons" aparece na tela', () => {
+    renderApp();
+    const favorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favorite).toBeInTheDocument();
+  });
 });

7cd4877ad5e90f02e2e7a1c94ec2f7b51c2cdd74
BrianCaetano <brian-caetano@hotmail.com>
2021-09-28 21:46:58 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
+
+describe('este o componente FavoritePokemons.js', () => {
+  test('exibe "No favorite pokemon found", se a não tiver pokémons favoritos.', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={ history }>
+        <FavoritePokemons />
+      </Router>,
+    );
+
+    const paragraph = screen.getByText('No favorite pokemon found');
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  // Ideia de mock via props com Isaac monstrão <3
+  test('should ', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={ history }>
+        <FavoritePokemons pokemons={ pokemons } />
+      </Router>,
+    );
+
+    const charmander = screen.getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+  });
+});

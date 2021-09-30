79168d57111ab51b89262da27162e50ce8aa33fc
Jonathan Ferreira <j0n4t@hotmail.com>
2021-09-30 09:16:53 -0300

    Requisito 2 - About - testando informações renderizadas

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+
+import About from '../components/About';
+
+describe('Página About', () => {
+  test('renderiza informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const heading = screen.getByRole('heading',
+      { level: 2, name: 'About Pokédex' });
+    expect(heading).toBeInTheDocument();
+
+    const paragraphs = screen.getAllByText(/poké/i)
+      .filter((element) => element !== heading);
+    expect(paragraphs).toHaveLength(2);
+
+    const image = screen.getByRole('img');
+    const expectedImageURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(expectedImageURL);
+  });
+});

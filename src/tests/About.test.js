41571742c1cffd8ca4b6df9ea2c587002e472907
hector <hectoreddbenitez@gmail.com>
2021-09-30 18:28:31 -0300

    Adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,32 @@
-test('', () => {});
-// iniciando
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/RenderWithRoute';
+
+import { About } from '../components';
+
+const pokedexPath = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+describe('About.js test', () => {
+  test('Test se a página contém um heading h2 com o texto "About Pokédex" ', () => {
+    renderWithRouter(<About />);
+    const aboutHeading = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    expect(aboutHeading).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragrafos = screen.getAllByText(/poké/i, { selector: 'p' });
+    expect(paragrafos.length).toBe(2);
+  });
+
+  test('Testa se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexImg = screen.getByRole('img', { name: /pokédex/i });
+
+    expect(pokedexImg.src).toBe(pokedexPath);
+    expect(pokedexImg).toBeInTheDocument();
+  });
+});

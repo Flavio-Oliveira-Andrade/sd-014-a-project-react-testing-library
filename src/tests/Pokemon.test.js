454eee6179c6247bc1f0dae9e902188a99b14e39
Matheus Alves <malves224@gmail.com>
2021-09-30 18:22:02 -0300

    adiciona teste que verifica todos conteudos do card pokemon, cumprindo requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -6,11 +6,10 @@ import pokemons from '../data';
 import Pokemon from '../components/Pokemon';
 
 describe('6º verifica o funcionamento do componente Pokemon', () => {
+  const boolFavorite = true;
+  const { name, type, averageWeight, image, id } = pokemons[0];
+  const { value, measurementUnit } = averageWeight;
   it('Verifica se é renderizado um card com as informações do pokemon', () => {
-    const boolFavorite = true;
-    const { name, type, averageWeight, image } = pokemons[0];
-    const { value, measurementUnit } = averageWeight;
-
     renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite={ boolFavorite } />);
 
     const nameElement = screen.getByTestId('pokemon-name');
@@ -23,5 +22,26 @@ describe('6º verifica o funcionamento do componente Pokemon', () => {
     const imgElement = screen.getByRole('img', { name: `${name} sprite` });
     expect(imgElement).toBeInTheDocument();
     expect(imgElement).toHaveAttribute('src', image);
+  });
+  it('testa se o card possui um link que acessa a pagina de detalhes do pokemon', () => {
+    const { history } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite={ boolFavorite }
+    />);
+    const linkDetails = screen.getByRole('link', { name: 'More details' });
+    expect(linkDetails).toBeInTheDocument();
+    expect(linkDetails).toHaveAttribute('href', `/pokemons/${id}`);
+
+    userEvent.click(linkDetails);
+    const pathLocation = history.location.pathname;
+    expect(pathLocation).toBe(`/pokemons/${id}`);
+  });
+  it('verifica se existe um icone de estrela se o pokemon for favoritado', () => {
+    renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite={ boolFavorite } />);
+    const imgFavorite = screen.getByRole('img', { name: /favorite/i });
+    expect(imgFavorite).toBeInTheDocument();
+
+    expect(imgFavorite).toHaveAttribute('src', '/star-icon.svg');
+    expect(imgFavorite).toHaveAttribute('alt', `${name} is marked as favorite`);
   });
 });

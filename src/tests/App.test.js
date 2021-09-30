9b8a7524b11004d8195753f472d37eb7848d6f84
Jonathan Ferreira <j0n4t@hotmail.com>
2021-09-29 21:01:47 -0300

    Requisito 1 - App - testa se links de navegação aparecem na página

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,14 @@
-test(' ', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Componente App', () => {
+  test('renderiza links de navegação', () => {
+    renderWithRouter(<App />);
+    const navLinks = screen.getAllByRole('link');
+    expect(navLinks[0].innerHTML).toBe('Home');
+    expect(navLinks[1].innerHTML).toBe('About');
+    expect(navLinks[2].innerHTML).toBe('Favorite Pokémons');
+  });
+});

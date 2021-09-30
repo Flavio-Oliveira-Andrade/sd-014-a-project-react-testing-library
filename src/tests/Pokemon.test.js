a02bd0e358e49f657469ab650205656a0628c222
Welton Lopes <weltonlopesmetal@gmail.com>
2021-09-30 14:31:26 -0300

    Em busca de resoluçao do pokemon.test

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from 'react-dom';
+// import renderWithRouter from '../components/Rotas';
+
+// import Home from '../App';
+
+// describe('Testando Pokemon', () => {
+//   it('Testand', () => {
+//     renderWithRouter(<Home />);
+//     const btnVer1 = screen.getByRole('button', {
+//       name: 'Próximo pokémon',
+//     });
+
+//     expect(btnVer1).toBeInTheDocument();
+
+//     const cartPokemonName = screen.getByTestId('pokemon-name');
+//     expect(cartPokemonName).toBe('Pikachu');
+//   });
+// });
+it('', () => {});

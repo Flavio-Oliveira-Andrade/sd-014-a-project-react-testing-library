3d24b0950fab89918ec00d074326d67b94910a0c
dandara-dias <dandaradias.contato@gmail.com>
2021-09-30 04:50:13 -0300

    Requisito 1, primeiro teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa se o topo da aplicação contém um conjunto fixo de links de navegação',
+  () => {
+    test('O primeiro link deve possuir o texto "Home"', () => {
+      renderWithRouter(<App />);
+      const linkHome = screen.getByText('Home');
+      expect(linkHome).toBeInTheDocument();
+    });
+  });

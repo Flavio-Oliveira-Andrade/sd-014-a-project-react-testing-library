28f28abe3190553f27ef2ce13ad8e59412b40c77
Dayane <dayanebmartins@gmail.com>
2021-09-29 22:28:41 -0300

    corrige requisito 1 de acordo com liter e importando screen

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,27 +1,27 @@
-import { fireEvent } from '@testing-library/dom';
+import { fireEvent, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-describe ('O primeiro link deve possuir o texto Home', () => {
+describe('O primeiro link deve possuir o texto Home', () => {
   test('deve renderizar o componente App', () => {
-    const { getByText, history} = renderWithRouter( <App/> )
-    fireEvent.click(getByText(/Home/i));
-    const {pathname} = history.location;
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Home/i));
+    const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
+
   test('deve renderizar o componente About', () => {
-    const { getByText, history} = renderWithRouter( <App/> )
-    fireEvent.click(getByText(/about/i));
-    const {pathname} = history.location;
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/about/i));
+    const { pathname } = history.location;
     expect(pathname).toBe('/about');
   });
+
   test('deve renderizar o componente Favorites', () => {
-    const { getByText, history} = renderWithRouter( <App/> )
-    fireEvent.click(getByText(/favorite/i));
-    const {pathname} = history.location;
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/favorite/i));
+    const { pathname } = history.location;
     expect(pathname).toBe('/favorites');
   });
-  
-})
-
+});

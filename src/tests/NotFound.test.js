270f68f946bd9f686b3d7ab10eeeb27e1c7f8a6d
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-09-30 18:12:31 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { NotFound } from '../components';
+import renderWithRouter from './Util/RenderWithRouter';
+
+describe('Verifica página notFound', () => {
+  it('Verifica se a página contém um heading "h2"', () => {
+    render(<NotFound />);
+
+    const notfoundText = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(notfoundText).toBeInTheDocument();
+  });
+  it('Verifica se a página contém uma imagem específica', () => {
+    renderWithRouter(<NotFound />);
+
+    const imageLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const gifNotfound = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+
+    expect(gifNotfound).toHaveAttribute('src', imageLink);
+  });
+});

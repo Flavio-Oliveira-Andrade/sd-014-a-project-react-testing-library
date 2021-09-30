44427ba76f25fcc52e12718ef7dce896b180dfb3
Heitor SF <heitor.sf5@gmail.com>
2021-09-29 21:31:48 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,101 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './auxiliary-tools/renderWithRouter';
+
+describe('Tests if FavoritePokémons page works properly', () => {
+  it('Displays \'No favorite pokemon found\' if none are favorited', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorites = screen.getByText('No favorite pokemon found');
+
+    expect(noFavorites).toBeInTheDocument();
+  });
+
+  it('Displays all the favorited pokémon', () => {
+    const mockFavorites = [
+      {
+        id: 4,
+        name: 'Charmander',
+        type: 'Fire',
+        averageWeight: {
+          value: '8.5',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+        foundAt: [
+          {
+            location: 'Alola Route 3',
+            map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+          },
+          {
+            location: 'Kanto Route 3',
+            map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+          },
+          {
+            location: 'Kanto Route 4',
+            map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+          },
+          {
+            location: 'Kanto Rock Tunnel',
+            map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+          },
+        ],
+        summary: 'The flame on its tail shows the strength of its life force.'
+         + 'If it is weak, the flame also burns weakly.',
+      },
+      {
+        id: 78,
+        name: 'Rapidash',
+        type: 'Fire',
+        averageWeight: {
+          value: '95.0',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)',
+        foundAt: [
+          {
+            location: 'Kanto Route 28',
+            map: 'https://cdn2.bulbagarden.net/upload/5/5b/Kanto_Route_28_Map.png',
+          },
+          {
+            location: 'Johto Mount Silver',
+            map: 'https://cdn2.bulbagarden.net/upload/9/95/Johto_Mt_Silver_Map.png',
+          },
+        ],
+        summary: 'At full gallop, its four hooves barely touch'
+        + 'the ground because it moves so incredibly fast.',
+      },
+      {
+        id: 23,
+        name: 'Ekans',
+        type: 'Poison',
+        averageWeight: {
+          value: '6.9',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)',
+        foundAt: [
+          {
+            location: 'Goldenrod Game Corner',
+            map: 'https://cdn2.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png',
+          },
+        ],
+        summary: 'It can freely detach its jaw to swallow large prey whole.'
+        + 'It can become too heavy to move, however.',
+      },
+    ];
+    renderWithRouter(<FavoritePokemons pokemons={ mockFavorites } />);
+
+    const charmander = screen.getByText(/charmander/i);
+    const rapidash = screen.getByText(/rapidash/i);
+    const ekans = screen.getByText(/ekans/i);
+    const MOCK_PMN_NUMBER = 3;
+
+    const allFavorites = [charmander, rapidash, ekans];
+    expect(allFavorites).toHaveLength(MOCK_PMN_NUMBER);
+  });
+});

ba7ab8c22780b40b88fb46c6e6a46c532797a350
Matheus Souza <ms.conrado25@hotmail.com>
2021-09-30 02:33:50 -0300

    criando testes para o componente FavoritePokemons.js e modificando About para tentar receber o check do evaluetor

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
+const URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
 describe('Testes do componente About', () => {
+  it('Testando para ver se a página contém as informações sobre a Pokédex', () => {
+    render(<About />);
+    const paragraphOne = screen.getByText(/simulates a Pokédex/);
+    const paragraphTwo = screen.getByText(/Pokémons by type/);
+    expect(paragraphOne).toBeInTheDocument();
+    expect(paragraphTwo).toBeInTheDocument();
+  });
+
   it('Testando se a página contém um heading h2 com o texto About Pokédex', () => {
     render(<About />);
     const headingH2 = screen.getByRole('heading', {
@@ -12,17 +22,15 @@ describe('Testes do componente About', () => {
     expect(headingH2).toBeInTheDocument();
   });
 
-  it('Testando se a página contém parágrafos com texto sobre a Pokédex', () => {
+  it('Testando se a página contém 2 parágrafos com texto sobre a Pokédex', () => {
     render(<About />);
-    const paragraphOne = screen.getByText(/simulates a Pokédex/);
-    const paragraphTwo = screen.getByText(/Pokémons by type/);
-    expect(paragraphOne).toBeInTheDocument();
-    expect(paragraphTwo).toBeInTheDocument();
+    const paragraph = screen.getAllByText(/Pokémons/);
+    expect(paragraph).toHaveLength(2);
   });
 
   it('Testando se a página contém a seguinte imagem de uma Pokédex:', () => {
     render(<About />);
     const img = screen.getByRole('img');
-    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(img.src).toBe(URL);
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,34 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import Favorites from '../components/FavoritePokemons';
+import App from '../App';
+
 describe('Testes do componente FavoritePokemon', () => {
   it(`Testanto se a mensagem exibida na tela é
   "No favorite pokemon found", se a pessoa nao tiver pokémons favoritos`, () => {
+    render(<Favorites />);
+    const msgNoFavorite = screen.getByText('No favorite pokemon found');
+    expect(msgNoFavorite).toBeInTheDocument();
+  });
 
+  it('Testando se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(moreDetails);
+
+    const labelFavorite = screen.getByRole('checkbox', {
+      name: 'Pokémon favoritado?',
+    });
+    userEvent.click(labelFavorite);
+
+    history.push('/favorites');
+
+    const namePokemon = screen.getByText('Pikachu');
+    expect(namePokemon).toBeInTheDocument();
   });
 });

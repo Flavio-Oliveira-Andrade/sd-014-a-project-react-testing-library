8b6ebecc3e6534e133f4aabf91cfff8e07037929
Dayane <dayanebmartins@gmail.com>
2021-09-29 22:02:47 -0300

    adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,26 @@
 import { fireEvent } from '@testing-library/dom';
 import React from 'react';
-import renderWithRouter from './renderWithRouter';
-import App from '/App';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-describe ('testando componente App', () => {
+describe ('O primeiro link deve possuir o texto Home', () => {
   test('deve renderizar o componente App', () => {
-    const { getByText, history} = renderWithRouter(<App/>)
+    const { getByText, history} = renderWithRouter( <App/> )
     fireEvent.click(getByText(/Home/i));
-    const pathname = history.location.pathname;
-    expect(pathname).toBe('/Home');
-    const home = getByText(/Home/);
-    expect(home).toBeInTheDocument();
+    const {pathname} = history.location;
+    expect(pathname).toBe('/');
+  });
+  test('deve renderizar o componente About', () => {
+    const { getByText, history} = renderWithRouter( <App/> )
+    fireEvent.click(getByText(/about/i));
+    const {pathname} = history.location;
+    expect(pathname).toBe('/about');
+  });
+  test('deve renderizar o componente Favorites', () => {
+    const { getByText, history} = renderWithRouter( <App/> )
+    fireEvent.click(getByText(/favorite/i));
+    const {pathname} = history.location;
+    expect(pathname).toBe('/favorites');
   });
   
 })

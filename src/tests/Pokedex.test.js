838fcdcac8929958e789b45c851c66a14cacaef1
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-09-30 15:20:29 -0300

    requsito 5 e iniciando requisito 7

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,117 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Testa o componente Pokedex', () => {
+  test('se página contém um heading h2 com o texto Encountered pokémons', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const headingPokedex = screen.getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+    expect(headingPokedex).toBeInTheDocument();
+  });
+
+  test('se é exibido o próximo Pokémon da lista quando o botão é clicado', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const buttonNextPokemon = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    expect(buttonNextPokemon).toBeInTheDocument();
+  });
+
+  test('se é mostrado apenas um Pokémon por vez', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const onePokemon = screen.getAllByTestId('pokemon-name');
+    expect(onePokemon).toHaveLength(1);
+  });
+
+  describe('se a Pokédex tem os botões de filtro', () => {
+    test('Deve existir um botão de filtragem para cada tipo de Pokémon', () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const buttonFilter = screen.getByRole('button', {
+        name: /Psychic/i,
+      });
+      const typePokemon = screen.getByTestId('pokemon-type');
+      userEvent.click(buttonFilter);
+      expect(typePokemon.innerHTML).toEqual('Psychic');
+    });
+
+    test('Pokédex deve circular somente pelos pokémons daquele tipo', () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+      const typesPokemon = 7;
+      const buttonsType = screen.getAllByTestId('pokemon-type-button');
+
+      expect(buttonsType).toHaveLength(typesPokemon);
+    });
+
+    test('texto do botão deve corresponder ao nome do tipo, ex. Psychic;', () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+      const buttonPsychic = screen.getByRole('button', {
+        name: /psychic/i,
+      });
+      expect(buttonPsychic).toBeInTheDocument();
+
+      userEvent.click(buttonPsychic);
+      const pokemon = screen.getByText(/alakazam/i);
+      expect(pokemon).toBeInTheDocument();
+    });
+
+    test('O botão All precisa estar sempre visível', () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const btnAll = screen.getByRole('button', {
+        name: /all/i,
+      });
+      expect(btnAll).toBeInTheDocument();
+    });
+  });
+  test('se a Pokédex contém um botão para resetar o filtro', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const buttonAll = screen.getByRole('button', {
+      name: /all/i,
+    });
+    expect(buttonAll).toBeInTheDocument();
+
+    userEvent.click(buttonAll);
+    const pokemonName = screen.getByText(/pikachu/i);
+    expect(pokemonName).toBeInTheDocument();
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,69 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+const mockPokemon = {
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Kanto Viridian Forest',
+      map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+    },
+    {
+      location: 'Kanto Power Plant',
+      map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    },
+  ],
+};
+
+const beforeEachDetails = () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+    const detailsButton = screen.getByText(/more details/i);
+    fireEvent.click(detailsButton);
+  });
+};
+
+describe('teste o componente PokemonDetails', () => {
+  describe('Teste se os detalhes do Pokémon selecionado são mostrados na tela', () => {
+    beforeEachDetails();
+
+    test('A página deve conter um texto Pokemon Details', () => {
+      const pokemonName = screen.getByText(`${mockPokemon.name} Details`);
+      expect(pokemonName).toBeInTheDocument();
+    });
+
+    test('Não deve existir o link de nav para os detalhes do Pokémon selecionado', () => {
+      const linkButtons = screen.getAllByRole('link');
+      linkButtons.forEach((linkButton) => {
+        expect(linkButton).not.toHaveAttribute('href', '/pokemon/25');
+      });
+    });
+
+    test('A seção de detalhes deve conter um heading h2 com o texto Summary', () => {
+      const sumary = screen.getByRole('heading', {
+        name: 'Summary',
+        level: 2,
+      });
+      expect(sumary).toBeInTheDocument();
+    });
+
+    test('A seção de detalhes deve conter um parágrafo com o resumo do Pokémon', () => {
+      const paragraph = screen.getByText(/This intelligent Pokémon roasts/i);
+      expect(paragraph).toBeInTheDocument();
+    });
+  });
+  describe('Teste se tem seção com os mapas contendo as localizações do pokémon', () => {
+
+  });
+});

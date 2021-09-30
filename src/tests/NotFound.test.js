91d5d40841733b62c8dacd6471abc50036b3fe51
Guinxard <lucasmfarias3@gmail.com>
2021-09-29 21:25:00 -0300

    requisito 4 ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testa o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2'
+  + ' com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const notFoundMessage = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra o gif do pikachu.', () => {
+    render(<NotFound />);
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const pikachuGif = screen.getByAltText(/Pikachu crying/i);
+    expect(pikachuGif.src).toStrictEqual(imgUrl);
+  });
+});

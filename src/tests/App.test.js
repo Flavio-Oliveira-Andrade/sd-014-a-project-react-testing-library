a1d2c77a3296eb19391cc762ae2a99c82e9816ab
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-09-30 19:28:32 -0300

    Primeiro requisito do exercicio 1 passando no teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import App from '../App';
+
+test(
+  'o primeiro link possui o text "Home", "About" e "Favorite Pokemons", respectivamente',
+  () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const home = screen.getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+
+    const about = screen.getByText(/About/i);
+    expect(about).toBeInTheDocument();
+
+    const favoritePokemons = screen.getByText(/Favorite Pokémons/i);
+    expect(favoritePokemons).toBeInTheDocument();
+  });

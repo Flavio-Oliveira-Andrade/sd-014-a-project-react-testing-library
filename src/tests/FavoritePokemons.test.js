b5a3bd2c46210b72b5d6edc459ecf8894ae16d5d
Flavio Pires <flavioasp77@gmail.com>
2021-09-30 19:59:29 -0300

    Requisito 3 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from '../utilities/renderWithRouter';
+
+describe('', () => {
+  it('testa se não tem pokemons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+  it('testa se renderiza o pokemon favoritado', () => {
+    const { history } = renderWithRouter(<App />);
+    const details = screen.getByRole('link', {
+      name: /More details/i,
+    });
+
+    fireEvent.click(details);
+    const pathName = history.location.pathname;
+    expect(pathName).not.toBe('/');
+    const favorite = screen.getByRole('checkbox');
+    fireEvent.click(favorite);
+    history.push('/favorites');
+    const namePokemon = screen.getByText('Pikachu');
+    expect(namePokemon).toBeInTheDocument();
+  });
+});

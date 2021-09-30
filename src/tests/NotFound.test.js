4ca32b347584aebb9a425f8db87f7f29e9a444e2
Riquelme <riquelmebandeira1@outlook.com>
2021-09-29 23:48:43 -0300

    Requisito 4: concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Verifica o funcionamento do componente "NotFound"', () => {
+  it('verifica se a página contém um "h2" com o texto'
+  + ' "Page requested not found 😭"', () => {
+    render(<NotFound />);
+    const heading = screen.getByRole(
+      'heading', { level: 2, name: 'Page requested not found Crying emoji' },
+    );
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('verifica se a página exibe o gif do Pikachu chorando', () => {
+    render(<NotFound />);
+    const imageSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imageAlt = 'Pikachu crying because the page requested was not found';
+    const imageElement = screen.getByRole('img', { name: imageAlt });
+    expect(imageElement).toHaveAttribute('src', imageSrc);
+    expect(imageElement).toBeInTheDocument();
+  });
+});

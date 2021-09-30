7d681b0505009bc6d8d4b75b0d58616583ade357
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-09-30 17:37:19 -0300

    Adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import pokemons from '../data';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './Util/RenderWithRouter';
+
+describe('Verifica component FavoritePokemons', () => {
+  it('Verifica se exibe a mensagem "No favorite pokemon found"', () => {
+    render(<FavoritePokemons />);
+
+    const notFoundFavoriteText = screen.getByText(
+      /No favorite pokemon found/i,
+    );
+    expect(notFoundFavoriteText).toBeInTheDocument();
+  });
+  it('Verifica se é exibido os pokemons favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[0]] } />);
+
+    const pokemonIdName = screen.getByTestId('pokemon-name');
+    expect(pokemonIdName).toBeInTheDocument();
+  });
+});

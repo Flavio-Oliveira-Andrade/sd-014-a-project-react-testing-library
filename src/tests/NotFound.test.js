f8f3dbc7cfb2c610c32139535e81614b1f0bf18e
Viviane Freitas <satierfviviane@gmail.com>
2021-09-29 21:06:41 -0300

    Teste 4 - Feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter.test';
+
+import NotFound from '../components/NotFound';
+
+describe('04-Teste o componente "NotFound.js"', () => {
+  test('se página contém um heading h2 com o texto Page requested not found ', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTxt = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(notFoundTxt).toBeInTheDocument();
+  });
+
+  test('se a página contém uma imagem', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImg = screen.getAllByRole('img');
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const alt = 'Pikachu crying because the page requested was not found';
+    expect(notFoundImg[1].src).toBe(src, alt);
+  });
+});

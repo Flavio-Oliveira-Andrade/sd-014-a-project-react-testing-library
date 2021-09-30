2f55116835c618e560e94ac07f4a8b87c1f324b6
Matheus Souza <ms.conrado25@hotmail.com>
2021-09-30 10:48:23 -0300

    refatorando About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -17,20 +17,20 @@ describe('Testes do componente About', () => {
     render(<About />);
     const headingH2 = screen.getByRole('heading', {
       level: 2,
-      name: /about/i,
+      name: 'About Pokédex',
     });
     expect(headingH2).toBeInTheDocument();
   });
 
   it('Testando se a página contém 2 parágrafos com texto sobre a Pokédex', () => {
     render(<About />);
-    const paragraph = screen.getAllByText(/Pokémons/);
-    expect(paragraph).toHaveLength(2);
+    const paragraphs = screen.getAllByText(/Pokémons/);
+    expect(paragraphs).toHaveLength(2);
   });
 
   it('Testando se a página contém a seguinte imagem de uma Pokédex:', () => {
     render(<About />);
-    const img = screen.getByRole('img');
+    const img = screen.getByAltText('Pokédex');
     expect(img.src).toBe(URL);
   });
 });

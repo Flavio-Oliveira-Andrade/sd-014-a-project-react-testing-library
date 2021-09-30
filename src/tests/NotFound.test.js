83f8a6021fd31647ab9f0475fe2b66f762d0e59e
tiagosathler <sathler@gmail.com>
2021-09-30 16:37:19 -0300

    Requisito 4: feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('4. Teste o componente <NotFound.js />:', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('4.1. se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      renderWithRouter(<NotFound />);
+      const text = screen.getByText(/Page requested not found/i);
+      expect(text).toBeInTheDocument();
+    });
+
+  it('4.2. se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    () => {
+      const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+      renderWithRouter(<NotFound />);
+      const img = screen.getByAltText(/Pikachu/i);
+      expect(img.src).toBe(URL);
+    });
+});

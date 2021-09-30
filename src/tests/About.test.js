72b5a9624949128b08a451c18e23404e12d56779
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-09-30 11:13:05 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { About } from '../components';
+
+describe('Teste o componente <About.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(
+      <About />,
+    );
+    const aboutText = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutText).toBeInTheDocument();
+  });
+});
+
+it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+  render(
+    <About />,
+  );
+  const firstParagraph = screen.getByText(/This application simulates a Pokédex/i);
+  const secondParagraph = screen.getByText(/One can filter Pokémons by type/i);
+  expect(firstParagraph).toBeInTheDocument();
+  expect(secondParagraph).toBeInTheDocument();
+});
+
+it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+  render(
+    <About />,
+  );
+  const imagePokedex = screen.getByRole('img');
+  expect(imagePokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'); // Referencia : https://medium.com/reactbrasil/exemplo-de-teste-de-alto-n%C3%ADvel-com-react-testing-library-5c001e89d493
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,70 +6,72 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-test('Testa se o topo da aplicação contém um conjunto de links de navegação', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const homeText = screen.getByText('Home');
-  expect(homeText).toBeInTheDocument();
+describe('Teste o componente <App.js />', () => {
+  test('Testa se o topo da aplicação contém um conjunto de links de navegação', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const homeText = screen.getByText('Home');
+    expect(homeText).toBeInTheDocument();
 
-  const aboutText = screen.getByText('About');
-  expect(aboutText).toBeInTheDocument();
+    const aboutText = screen.getByText('About');
+    expect(aboutText).toBeInTheDocument();
 
-  const favoriteText = screen.getByText('Favorite Pokémons');
-  expect(favoriteText).toBeInTheDocument();
-});
+    const favoriteText = screen.getByText('Favorite Pokémons');
+    expect(favoriteText).toBeInTheDocument();
+  });
 
-test('Teste se a aplicação é redirecionada para a página inicial,'
+  test('Teste se a aplicação é redirecionada para a página inicial,'
  + 'na URL / ao clicar no link Home da barra de navegação.', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const homeLink = screen.getByRole('link', {
-    name: 'Home',
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const homeLink = screen.getByRole('link', {
+      name: 'Home',
+    });
+    userEvent.click(homeLink);
+    expect(homeLink).toBeInTheDocument();
   });
-  userEvent.click(homeLink);
-  expect(homeLink).toBeInTheDocument();
-});
 
-test('Teste se a aplicação é redirecionada para a página de About,'
+  test('Teste se a aplicação é redirecionada para a página de About,'
 + 'na URL / ao clicar no link About da barra de navegação.', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const aboutLink = screen.getByRole('link', {
-    name: 'About',
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const aboutLink = screen.getByRole('link', {
+      name: 'About',
+    });
+    userEvent.click(aboutLink);
+    expect(aboutLink).toBeInTheDocument();
   });
-  userEvent.click(aboutLink);
-  expect(aboutLink).toBeInTheDocument();
-});
 
-test('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,'
+  test('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,'
 + 'na URL / ao clicar no link Favorite Pokémons da barra de navegação.', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const favortitePokemonLink = screen.getByRole('link', {
-    name: 'Favorite Pokémons',
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const favortitePokemonLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    userEvent.click(favortitePokemonLink);
+    expect(favortitePokemonLink).toBeInTheDocument();
   });
-  userEvent.click(favortitePokemonLink);
-  expect(favortitePokemonLink).toBeInTheDocument();
-});
 
-test('Teste se a aplicação é redirecionada para a página Not Found'
+  test('Teste se a aplicação é redirecionada para a página Not Found'
 + 'ao entrar em uma URL desconhecida.', () => {
-  const { history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
 
-  history.push('/url-desconhecida');
+    history.push('/url-desconhecida');
 
-  const notFoundtext = screen.getByText('Page requested not found');
-  expect(notFoundtext).toBeInTheDocument();
+    const notFoundtext = screen.getByText('Page requested not found');
+    expect(notFoundtext).toBeInTheDocument();
+  });
 });

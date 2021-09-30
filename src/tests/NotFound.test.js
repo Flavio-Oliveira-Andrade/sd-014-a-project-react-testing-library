7f2f5e95715e583d7eb87c7f820fe12efe7be532
Flavio Pires <flavioasp77@gmail.com>
2021-09-30 20:19:49 -0300

    Requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../utilities/renderWithRouter';
+
+describe('Testa o componente "NotFound"', () => {
+  it('testa se tem um tilulo com a frase "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const isH2 = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(isH2).toBeInTheDocument();
+  });
+  it('testa se tem uma imagem na tela', () => {
+    renderWithRouter(<NotFound />);
+    const IMAGE_URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    // aqui como tem mais de uma imagem, pegamos a segunda.
+    const { src } = screen.getAllByRole('img')[1];
+
+    expect(src).toBe(IMAGE_URL);
+  });
+});

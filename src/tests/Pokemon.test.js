850a2e066d73ed0bf9b7eb8b2f58e97efae8c17c
Matheus Alves <malves224@gmail.com>
2021-09-30 17:45:09 -0300

    adiciona teste que verifica se o componenete pokemon renderiza as informaçoes do pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+import Pokemon from '../components/Pokemon';
+
+describe('6º verifica o funcionamento do componente Pokemon', () => {
+  it('Verifica se é renderizado um card com as informações do pokemon', () => {
+    const boolFavorite = true;
+    const { name, type, averageWeight, image } = pokemons[0];
+    const { value, measurementUnit } = averageWeight;
+
+    renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite={ boolFavorite } />);
+
+    const nameElement = screen.getByTestId('pokemon-name');
+    const typeElement = screen.getByTestId('pokemon-type');
+    const weightElement = screen.getByTestId('pokemon-weight');
+    expect(nameElement).toHaveTextContent(name);
+    expect(typeElement).toHaveTextContent(type);
+    expect(weightElement).toHaveTextContent(`${value} ${measurementUnit}`);
+
+    const imgElement = screen.getByRole('img', { name: `${name} sprite` });
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute('src', image);
+  });
+});

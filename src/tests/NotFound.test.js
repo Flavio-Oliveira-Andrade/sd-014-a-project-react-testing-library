f33af3c2b2f02eaa584a13e392805613eed7147d
Pablo Assunção <pabloassuncao2@gmail.com>
2021-09-30 17:22:49 -0300

    Adiciona o requisito 3

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import NotFound from '../components/NotFound';
+
+describe('Teste da página NotFound', () => {
+  test('Verifica se há o texto NotFound', () => {
+    render(<NotFound />, { wrapper: BrowserRouter });
+
+    const title = screen.getByRole('heading', { name: /Page requested not found/i });
+
+    expect(title).toBeInTheDocument();
+  });
+  test('Verifica se a imagem está correta', () => {
+    render(<NotFound />, { wrapper: BrowserRouter });
+
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i });
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img).toHaveAttribute('src', url);
+  });
+});

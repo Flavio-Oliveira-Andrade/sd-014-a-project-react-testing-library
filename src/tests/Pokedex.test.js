4dd5010cce8977d5ee2c92c8725d4c96ae903f52
Semnome1505 <Gabriel_1505@outlook.com>
2021-10-01 16:05:33 -0300

    Adiciona o teste 1 do Requisito 5.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+import App from '../App';
+
+describe('Requisito 5', () => {
+  test('Página contém um heading h2 com o texto Encountered pokémons', () => {
+    const setIs = App.setIsPokemonFavoriteById();
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ setIs }
+      />);
+    const h2 = screen.getByRole('heading', {
+      name: 'Encountered pokémons',
+    });
+    expect(h2).toBeInTheDocument();
+  });
+})

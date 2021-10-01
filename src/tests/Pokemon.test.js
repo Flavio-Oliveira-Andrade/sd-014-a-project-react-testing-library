b5ba339e64d0a1d4194fb3e6b680157e715dbce3
Welton Lopes <weltonlopesmetal@gmail.com>
2021-10-01 12:49:44 -0300

    Talvez finalizado?,

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,20 +1,56 @@
-// import React from 'react';
-// import { screen } from 'react-dom';
-// import renderWithRouter from '../components/Rotas';
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import Pokemon from '../components/Pokemon';
+// import App from '../App';
+import renderWithRouter from '../components/Rotas';
 
-// import Home from '../App';
+const fakePikachu = {
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Kanto Viridian Forest',
+      map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+    },
+    {
+      location: 'Kanto Power Plant',
+      map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    },
+  ],
+  summary: `This intelligent Pokémon roasts
+   hard berries with electricity to make them tender enough to eat.`,
+};
 
-// describe('Testando Pokemon', () => {
-//   it('Testand', () => {
-//     renderWithRouter(<Home />);
-//     const btnVer1 = screen.getByRole('button', {
-//       name: 'Próximo pokémon',
-//     });
+describe(' renderizado um card com as informações de determinado pokémon', () => {
+  it('Testando renderizado um card', () => {
+    renderWithRouter(<Pokemon pokemon={ fakePikachu } isFavorite />);
 
-//     expect(btnVer1).toBeInTheDocument();
+    const averageWeightC = `Average weight: ${fakePikachu
+      .averageWeight.value} ${fakePikachu.averageWeight.measurementUnit}`;
 
-//     const cartPokemonName = screen.getByTestId('pokemon-name');
-//     expect(cartPokemonName).toBe('Pikachu');
-//   });
-// });
-it('', () => {});
+    const cartPokemonName = screen.getByTestId('pokemon-name');
+    expect(cartPokemonName).toBeInTheDocument();
+    expect(cartPokemonName).toHaveTextContent(fakePikachu.name);
+
+    const cartPokemonType = screen.getByTestId('pokemon-type');
+    expect(cartPokemonType).toBeInTheDocument();
+    expect(cartPokemonType).toHaveTextContent(fakePikachu.type);
+
+    const cartPokemonAverageWeight = screen.getByTestId('pokemon-weight');
+    expect(cartPokemonAverageWeight).toBeInTheDocument();
+    expect(cartPokemonAverageWeight).toHaveTextContent(averageWeightC);
+
+    const cartPokemonImgPokemon = screen.getByAltText(/sprite/i);
+    expect(cartPokemonImgPokemon).toBeInTheDocument();
+    expect(cartPokemonImgPokemon).toHaveAttribute('src', fakePikachu.image);
+    expect(cartPokemonImgPokemon).toHaveAttribute('alt', `${fakePikachu.name} sprite`);
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import pokemons from '../data';
+import App from '../App';
+import renderWithRouter from '../components/Rotas';
+
+describe('Testando Pokemon Deatails', () => {
+  beforeEach(() => { renderWithRouter(<App />); });
+  const { name, summary, foundAt } = pokemons[0];
+
+  it('Mostrando detalhes do seu pokemon', () => {
+    const detalhes = screen.getByText(/More details/i);
+    userEvent.click(detalhes);
+    expect(screen.getByText(`${name} Details`)).toBeInTheDocument();
+    expect(detalhes).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: /Summary/i,
+    })).toBeInTheDocument();
+    expect(screen.getByText(summary)).toBeInTheDocument();
+  });
+  it('pokemon mapas', () => {
+    userEvent.click(screen.getByText(/More details/i));
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: `Game Locations of ${name}`,
+    })).toBeInTheDocument();
+
+    foundAt.forEach((loc, index) => {
+      const imgLocation = screen.getAllByRole('img', {
+        name: `${name} location`,
+      });
+      expect(imgLocation[index]).toHaveAttribute('src', loc.map);
+      expect(imgLocation[index]).toHaveAttribute('alt', `${name} location`);
+    });
+  });
+
+  it('Favoritar Pokemons', () => {
+    userEvent.click(screen.getByText(/More details/i));
+    userEvent.click(screen.getByRole('checkbox', {
+      name: 'Pokémon favoritado?',
+    }));
+    expect(screen.getByRole('img', {
+      name: `${name} is marked as favorite`,
+    })).toHaveAttribute('src', '/star-icon.svg');
+  });
+});

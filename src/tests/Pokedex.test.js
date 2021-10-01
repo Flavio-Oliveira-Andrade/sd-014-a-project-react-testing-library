14b8e36ce770780627a1915aeb542e54ab1a6434
Semnome1505 <Gabriel_1505@outlook.com>
2021-10-01 17:50:14 -0300

    Adiciona o teste 2 do Requisito 5.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import Pokedex from '../components/Pokedex';
 import pokemons from '../data';
@@ -12,10 +12,38 @@ describe('Requisito 5', () => {
       <Pokedex
         pokemons={ pokemons }
         isPokemonFavoriteById={ setIs }
-      />);
+      />,
+    );
     const h2 = screen.getByRole('heading', {
       name: 'Encountered pokémons',
     });
     expect(h2).toBeInTheDocument();
   });
-})
+  test('Próximos Pokémons da lista devem ser mostrados, '
+  + 'um a um, ao clicar sucessivamente no botão', () => {
+    const setIs = App.setIsPokemonFavoriteById();
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ setIs }
+      />,
+    );
+    const btnAll = screen.getByRole('button', {
+      name: 'All',
+    });
+    expect(btnAll).toBeInTheDocument();
+    pokemons.forEach((poke) => {
+      const screenName = screen.getByTestId('pokemon-name');
+      const pokeName = poke.name;
+      expect(screenName).toHaveTextContent(pokeName);
+      const btnNext = screen.getByText('Próximo pokémon');
+      fireEvent.click(btnNext);
+    });
+    const screenName = screen.getByTestId('pokemon-name');
+    const pokeName = pokemons[0].name;
+     expect(screenName).toHaveTextContent(pokeName);
+  });
+  test('É mostrado apenas um Pokémon por vez', () => {
+
+  });
+});

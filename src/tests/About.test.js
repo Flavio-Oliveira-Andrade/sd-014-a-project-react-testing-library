b60a7ed3ef36bd596df893a75ce79229b25f4bc3
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-10-01 19:38:06 -0300

    Exercicio 2 do projeto passando nos testes

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import About from '../components/About';
+
+describe('testa o componente About.js', () => {
+  // test('testa se a página contém as informações sobre a Pokédex.', () => {
+  //   render(<About />, { wrapper: BrowserRouter });
+
+  //   const image = screen.getByRole('img', { name:  });
+  //   expect(image).toBeInTheDocument();
+  // });
+
+  test('testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />, { wrapper: BrowserRouter });
+
+    const heading = screen.getByRole('heading', { name: /About pokédex/i, level: 2 });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    render(<About />, { wrapper: BrowserRouter });
+
+    const paragraphs = screen.getAllByText(/Pokémon/i, { selector: 'p' });
+    expect(paragraphs.length).toBe(2);
+  });
+
+  test('testa se a página contém a respectiva imagem de uma Pokédex', () => {
+    render(<About />, { wrapper: BrowserRouter });
+
+    const image = screen.getByRole('img', { name: 'Pokédex' });
+    expect(image).toBeInTheDocument();
+  });
+});
+
+// Requisito dos parágrafos realizado com a ajuda dos colegas no Slack(https://trybecourse.slack.com/archives/C023LSXLGG7/p1633002970441800)

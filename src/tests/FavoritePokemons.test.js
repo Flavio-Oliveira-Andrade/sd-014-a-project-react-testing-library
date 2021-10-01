da1ea596369ffe19ec7c6b521d4deff24a5a88db
danielm <play.daniel@hotmail.com>
2021-10-01 14:43:30 -0300

    Requisito 3: Concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Elementos do componente <FavoritePokemon/>', () => {
+  it('É exibido na tela a mensagem "No favorite pokemon found"'
+  + ', se a pessoa não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textNotFoundPokemond = screen.getByText(/no favorite pokemon found/i);
+    expect(textNotFoundPokemond).toBeInTheDocument();
+  });
+});

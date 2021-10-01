769e0ee9d1b59adec92bdf70190d8eea1ab51027
Gabriel Franklin <franklingabriel158@gmail.com>
2021-10-01 19:03:19 -0300

    Requisito 4 - Testando NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Testando NotFound.js', () => {
+  test('Verifica se a página contem o titulo "Page requested not found😭"', () => {
+    const { history } = renderWithRouter(<NotFound />);
+
+    history.push('/qualquer-coisa');
+
+    const titleNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+  test('Verifca se a pagina contem o gif', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const gif = screen.getByRole('img', {
+      name: /pikachu crying because/i,
+    });
+    expect(gif).toBeInTheDocument();
+    expect(gif).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

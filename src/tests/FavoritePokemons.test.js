96adaae5504e6d448e5c864e295b7e500db590d0
danielm <play.daniel@hotmail.com>
2021-10-01 15:50:14 -0300

    Requisito 4: Concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -3,11 +3,11 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from './utils/renderWithRouter';
 import { FavoritePokemons } from '../components';
 
-describe('Elementos do componente <FavoritePokemon/>', () => {
+describe('Elemento do componente <FavoritePokemon/>', () => {
   it('É exibido na tela a mensagem "No favorite pokemon found"'
   + ', se a pessoa não tiver pokémons favoritos', () => {
     renderWithRouter(<FavoritePokemons />);
-    const textNotFoundPokemond = screen.getByText(/no favorite pokemon found/i);
-    expect(textNotFoundPokemond).toBeInTheDocument();
+    const textFavoritePokemon = screen.getByText(/no favorite pokemon found/i);
+    expect(textFavoritePokemon).toBeInTheDocument();
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+describe('Elemento do componente <NotFound.js />', () => {
+  it('A página contém 1 heading h2 com  texto "Page requested not found 😭"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/aqui-pode-ser-xablau');
+    const textNotFound = screen.getByRole('heading',
+      { name: /page requested not found/i, level: 2 });
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  it('A página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/aqui-pode-ser-xablau');
+    const imgNotFound = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

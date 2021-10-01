e6c90a0213236661eff255d12ea3679746d823bb
dandara-dias <dandaradias.contato@gmail.com>
2021-10-01 18:34:33 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
@@ -6,6 +7,10 @@ describe('Testa o componente <FavoritePokemons />', () => {
   test('Testa se é exibido na tela a mensagem "No favorite pokemon found", se a pessoa'
   + 'não tiver pokémons favoritos', () => {
     renderWithRouter(<FavoritePokemons />);
+
+    const noFavorite = screen.getByText('No favorite pokemon found');
+
+    expect(noFavorite).toBeInTheDocument();
   });
 });
 

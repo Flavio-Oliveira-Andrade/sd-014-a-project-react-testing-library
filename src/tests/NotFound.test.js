37b5a8d48cff98417733931a1c7a6d640b707f9b
Semnome1505 <Gabriel_1505@outlook.com>
2021-10-01 13:32:17 -0300

    Requisito 4.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Requisito 4', () => {
+  test('página contém um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const phrase = /Page requested not found/i;
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const altText = 'Pikachu crying because the page requested was not found';
+    const heading = screen.getByText(phrase);
+    const image = screen.getByAltText(altText);
+    expect(heading).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(src);
+  });
+});

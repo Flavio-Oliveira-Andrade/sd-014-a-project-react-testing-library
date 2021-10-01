ec98a30390aeb3aeca59e83e0c1b4ab9acf85b8a
Semnome1505 <Gabriel_1505@outlook.com>
2021-10-01 12:57:34 -0300

    Requisito 3.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Requisito 3', () => {
+  test('É exibido "No favorite pokemon found", se não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const text = 'No favorite pokemon found';
+    const phrase = screen.getByText(text);
+    expect(phrase).toBeInTheDocument();
+  });
+});

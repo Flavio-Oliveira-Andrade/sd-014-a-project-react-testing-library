7c13d6fc20d79b0b8f62c7a172cfe68434e1d1c7
Jonathan Ferreira <j0n4t@hotmail.com>
2021-10-01 09:24:02 -0300

    Requisito 5 - Pokedex - Testa heading e botão de Próximo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import pokeProps from './mocks/pokeMocks';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Página Pokedex', () => {
+  test('renderiza o heading com o texto "Encountered pokémons"', () => {
+    renderWithRouter(<Pokedex { ...pokeProps } />);
+
+    const heading = screen.getByRole('heading',
+      { level: 2, name: 'Encountered pokémons' });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('mostra o próximo Pokémon ao clicar no botão "Próximo"', () => {
+    renderWithRouter(<Pokedex { ...pokeProps } />);
+    const TEST_ID = 'pokemon-name';
+    const pkmnName = screen.getByTestId(TEST_ID).innerHTML;
+    expect(pkmnName).toBe(pokeProps.pokemons[0].name);
+
+    const nextPkmnButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+    userEvent.click(nextPkmnButton);
+
+    const pkmnName2 = screen.getByTestId(TEST_ID).innerHTML;
+    expect(pkmnName2).toBe(pokeProps.pokemons[1].name);
+
+    userEvent.click(nextPkmnButton);
+    const pkmnName3 = screen.getByTestId(TEST_ID).innerHTML;
+    expect(pkmnName3).toBe(pokeProps.pokemons[0].name);
+  });
+});

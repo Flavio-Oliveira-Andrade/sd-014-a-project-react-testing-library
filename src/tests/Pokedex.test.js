12e040b6e17570f251dae1353df13f7db8e023aa
Jonathan Ferreira <j0n4t@hotmail.com>
2021-10-01 14:49:04 -0300

    Requisito 5 - Pokedex - Testa botão All

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -7,6 +7,11 @@ import Pokedex from '../components/Pokedex';
 import renderWithRouter from '../utils/renderWithRouter';
 
 describe('Página Pokedex', () => {
+  const TEST_ID_NAME = 'pokemon-name';
+  const TEST_ID_TYPE = 'pokemon-type';
+  const TEST_ID_TYPE_BTN = 'pokemon-type-button';
+  const NEXT_BTN_NAME = 'Próximo pokémon';
+
   test('renderiza o heading com o texto "Encountered pokémons"', () => {
     renderWithRouter(<Pokedex { ...pokeProps } />);
 
@@ -17,8 +22,7 @@ describe('Página Pokedex', () => {
 
   test('mostra o próximo Pokémon ao clicar no botão "Próximo"', () => {
     renderWithRouter(<Pokedex { ...pokeProps } />);
-    const TEST_ID_NAME = 'pokemon-name';
-    const nextPkmnButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const nextPkmnButton = screen.getByRole('button', { name: NEXT_BTN_NAME });
 
     const pkmnName = screen.getByTestId(TEST_ID_NAME).innerHTML;
     expect(pkmnName).toBe(pokeProps.pokemons[0].name);
@@ -39,7 +43,7 @@ describe('Página Pokedex', () => {
   test('renderiza botões de filtro por tipo', () => {
     renderWithRouter(<Pokedex { ...pokeProps } />);
 
-    const typeButtons = screen.getAllByTestId('pokemon-type-button');
+    const typeButtons = screen.getAllByTestId(TEST_ID_TYPE_BTN);
     expect(typeButtons.length).toBe(2);
     expect(typeButtons[0].innerHTML).toBe('Electric');
     expect(typeButtons[1].innerHTML).toBe('Fire');
@@ -47,20 +51,24 @@ describe('Página Pokedex', () => {
 
   test('ao clicar em um botão de tipo, mostra apenas pokémons do tipo', () => {
     renderWithRouter(<Pokedex { ...pokeProps } />);
-    const TEST_ID_TYPE = 'pokemon-type';
+    const allButton = screen.getByRole('button', { name: /all/i });
+    const typeButtons = screen.getAllByTestId(TEST_ID_TYPE_BTN);
+    let pkmnType = screen.getByTestId(TEST_ID_TYPE).innerHTML;
 
-    const typeButtons = screen.getAllByTestId('pokemon-type-button');
+    expect(pkmnType).toBe(typeButtons[0].innerHTML);
     userEvent.click(typeButtons[1]);
 
-    let pkmnType = screen.getByTestId(TEST_ID_TYPE).innerHTML;
+    pkmnType = screen.getByTestId(TEST_ID_TYPE).innerHTML;
     expect(pkmnType).toBe(typeButtons[1].innerHTML);
+    expect(allButton).toBeInTheDocument();
 
-    const nextPkmnButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const nextPkmnButton = screen.getByRole('button', { name: NEXT_BTN_NAME });
 
     // changing some times the pokémon to make sure
     userEvent.click(nextPkmnButton);
     userEvent.click(nextPkmnButton);
     userEvent.click(nextPkmnButton);
+    expect(allButton).toBeInTheDocument();
 
     pkmnType = screen.getByTestId(TEST_ID_TYPE).innerHTML;
     expect(pkmnType).toBe(typeButtons[1].innerHTML);
@@ -68,5 +76,24 @@ describe('Página Pokedex', () => {
     userEvent.click(typeButtons[0]);
     pkmnType = screen.getByTestId(TEST_ID_TYPE).innerHTML;
     expect(pkmnType).toBe(typeButtons[0].innerHTML);
+    expect(allButton).toBeInTheDocument();
+  });
+
+  test('ao clicar no botão all, reseta o filtro selecionado', () => {
+    renderWithRouter(<Pokedex { ...pokeProps } />);
+    const allButton = screen.getByRole('button', { name: /all/i });
+    const typeButtons = screen.getAllByTestId(TEST_ID_TYPE_BTN);
+    const initialType = screen.getByTestId(TEST_ID_TYPE).innerHTML;
+
+    userEvent.click(typeButtons[1]);
+    userEvent.click(allButton);
+
+    let pkmnType = screen.getByTestId(TEST_ID_TYPE).innerHTML;
+    expect(pkmnType).toBe(initialType);
+
+    const nextPkmnButton = screen.getByRole('button', { name: NEXT_BTN_NAME });
+    userEvent.click(nextPkmnButton);
+    pkmnType = screen.getByTestId(TEST_ID_TYPE).innerHTML;
+    expect(pkmnType).not.toBe(initialType);
   });
 });

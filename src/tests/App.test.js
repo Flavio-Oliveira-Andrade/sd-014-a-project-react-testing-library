b9bae84bd4c04bee1dc60ccc9c50499f77861d94
Guilherme Leao <leaob7@gmail.com>
2021-10-01 17:40:50 -0300

    Req 1 App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('verify link elements', () => {
+  renderWithRouter(<App />);
+  const home = screen.getByText('Home');
+  const about = screen.getByText('About');
+  const favPokemons = screen.getByText('Favorite Pokémons');
+
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favPokemons).toBeInTheDocument();
+});
+
+test('verify the correct path to Home', () => {
+  const { history } = renderWithRouter(<App />);
+  fireEvent.click(screen.getByText('Home'));
+  const path = history.location.pathname;
+
+  expect(path).toBe('/');
+});
+
+test('verify the correct path to About', () => {
+  // renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />);
+  fireEvent.click(screen.getByText('About'));
+  const path = history.location.pathname;
+
+  expect(path).toBe('/about');
+});
+
+test('verify the correct path to Favorite Pokemons', () => {
+  const { history } = renderWithRouter(<App />);
+  fireEvent.click(screen.getByText('Favorite Pokémons'));
+  const path = history.location.pathname;
+
+  expect(path).toBe('/favorites');
+});

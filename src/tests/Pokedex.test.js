5719c87008cf57c51eb602982d2554f1bdbe139c
dandara-dias <dandaradias.contato@gmail.com>
2021-10-01 04:37:48 -0300

    Requisito 5, com erro

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+describe('Testa o componente <Pokedex />', () => {
+  test('Testa se a página contém um heading h2 com o texto "Encountered pokémons"',
+    () => {
+      renderWithRouter(<Pokedex />);
+
+      const h2Text = screen.getByRole('heading', {
+        level: 2,
+        name: 'Encountered pokémons',
+      });
+
+      expect(h2Text).toBeInTheDocument();
+    });
+});
+
 test('', () => {});

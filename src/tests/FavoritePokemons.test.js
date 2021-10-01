aa251347b3f3594c95a966426b92692ab159afcf
dandara-dias <dandaradias.contato@gmail.com>
2021-10-01 03:50:34 -0300

    Requisito 3, origens

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente <FavoritePokemons />', () => {
+  test('Testa se é exibido na tela a mensagem "No favorite pokemon found", se a pessoa'
+  + 'não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+});
+
 test('', () => {});

4e05e25fb1750e630c0bc76ac8921973470a4ba6
dandara-dias <dandaradias.contato@gmail.com>
2021-10-01 04:14:58 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente <NotFound />', () => {
+  test('Testa se a página contém um heading h2 com o texto "Page requested not found 😭"',
+    () => {
+      renderWithRouter(<NotFound />);
+
+      const h2Text = screen.getByRole('heading', {
+        level: 2,
+        name: /Page requested not found/,
+      });
+
+      expect(h2Text).toBeInTheDocument();
+    });
+  test('Testa se a página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundImage = screen.getByAltText('Pikachu crying because the page'
+    + ' requested was not found');
+
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

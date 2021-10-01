d4bf7dea79569145b58ae4d9c20d030a5ec8c413
Dayane <dayanebmartins@gmail.com>
2021-09-30 23:31:10 -0300

    completa requisito 3 testando se os cards sao exibidos na tela

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemons } from '../components';
+import App from '../App';
 
 describe('Testar componente Favorite Pokemons', () => {
   test('Verifica se é exibido na pagina a mensagem No favorite pokemon found', () => {
@@ -9,5 +10,10 @@ describe('Testar componente Favorite Pokemons', () => {
     const titleFavorites = screen.getByText('No favorite '
     + 'pokemon found');
     expect(titleFavorites).toBeInTheDocument();
+  });
+  test('Verifica se exibe todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const cards = screen.getByTestId(/pokemon-name/i);
+    expect(cards).toBeInTheDocument();
   });
 });

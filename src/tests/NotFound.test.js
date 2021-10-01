9cb6cc74b3b11478a089f146023627000b1badd6
hector <hectoreddbenitez@gmail.com>
2021-10-01 18:07:02 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/RenderWithRoute';
+
+import { NotFound } from '../components';
+
+const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+describe('NotFound.js test', () => {
+  test('Testa se página contém um heading h2'
+   + 'com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem definida como URL', () => {
+    renderWithRouter(<NotFound />);
+    const pikachuIMG = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(pikachuIMG).toHaveAttribute('src', URL);
+  });
+});

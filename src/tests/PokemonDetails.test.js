814b27a2eeaa3adec2abb762ab4c9493e61927e4
“pedrobarreto” <pbdeoliveira@gmail.com>
2021-10-02 18:54:39 -0300

    requisito 5

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,59 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+function renderWithRouter(component) {
+  const customHistory = createMemoryHistory();
+  return {
+    ...render(
+      <Router history={ customHistory }>
+        {component}
+      </Router>,
+    ),
+    history: customHistory,
+  };
+}
+
+describe('7 Requisito', () => {
+  test('Se as infos detalhadas do Pokémon selecionado são mostradas na tela', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/');
+
+    const detailsLink = screen.getByRole('link', {
+      name: /details/i,
+    });
+
+    userEvent.click(detailsLink);
+
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /summary/i,
+    });
+
+    expect(h2).toBeInTheDocument();
+  });
+
+  test('Teste se existe na página uma seção com os mapas'
+  + 'contendo as localizações do pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/');
+
+    const detailsLink = screen.getByRole('link', {
+      name: /details/i,
+    });
+
+    userEvent.click(detailsLink);
+
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /game locations/i,
+    });
+
+    expect(h2).toBeInTheDocument();
+  });
+});

9eab22b59c310856735fa94d806001e0deefedc0
Guilherme Leao <leaob7@gmail.com>
2021-10-02 15:56:32 -0300

    Req 3 Favorite Pokes ?

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+import renderWithRouter from '../renderWithRouter';
 
 test('verify no favorites message', () => {
   render(<FavoritePokemons />);
+  const message = screen.getByText('No favorite pokemon found');
+
+  expect(message).toBeInTheDocument();
+});
+
+test('verify poke cards exibition', () => {
+  renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+  const pika = screen.getByText('Pikachu');
+  const mew = screen.getByText('Mew');
+
+  expect(pika).toBeInTheDocument();
+  expect(mew).toBeInTheDocument();
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+test('verify correct text for heading', () => {
+  render(<Pokedex isPokemonFavoriteById={ {} } pokemons={ pokemons } />);
+  const h2 = screen.getByRole('heading');
+
+  expect(h2).toHaveTextContent('Encountered pokémons');
+});

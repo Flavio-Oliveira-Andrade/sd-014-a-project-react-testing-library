6a867e902a9664ed47f8c3cd3d7dbb8cdfda4328
Matheus Pozett <maa_souza11@hotmail.com>
2021-10-02 18:52:56 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testes do requisito 4', () => {
+  it('Testa se página contém h2 com o texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

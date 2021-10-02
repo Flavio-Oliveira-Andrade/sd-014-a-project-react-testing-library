c7b35c5b38c276fa2ac009382c82bedc5d8ef378
Guilherme Leao <leaob7@gmail.com>
2021-10-02 15:06:52 -0300

    Req 4 NotFound

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('verify no favorites message', () => {
+  render(<FavoritePokemons />);
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('verify correct text for heading', () => {
+  render(<NotFound />);
+  const h2 = screen.getByRole('heading');
+
+  expect(h2).toHaveTextContent('Page requested not found 😭');
+});
+
+test('verify correct image exibition', () => {
+  render(<NotFound />);
+  const alt = 'Pikachu crying because the page requested was not found';
+  const altImg = screen.getByAltText(alt);
+
+  expect(altImg).toBeVisible();
+});

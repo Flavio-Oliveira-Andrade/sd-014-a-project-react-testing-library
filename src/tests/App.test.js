3bfd1d7402f26c932cddc67cfd0448f7dcfc013b
Elielson Nascimento <nascimento.elielson@gmail.com>
2021-10-02 20:15:12 -0300

    Requisito 01 - OK

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import RenderWithRouter from './new-test/RenderWithRouter';
 import App from '../App';
 
@@ -7,9 +7,6 @@ describe('Verifique se aplicação contém um conjunto fixo de links de navegaç
   it('Existem os respectivos texto "Home" na página:', () => {
     // Acessar elementos da sua tela:
     RenderWithRouter(<App />);
-
-    // Interagir com eles (se houver necessidade):
-
     // Fazer o teste / testes:
     expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
   });
@@ -17,9 +14,6 @@ describe('Verifique se aplicação contém um conjunto fixo de links de navegaç
   it('Existem o texto "About" na página:', () => {
     // Acessar elementos da sua tela:
     RenderWithRouter(<App />);
-
-    // Interagir com eles (se houver necessidade):
-
     // Fazer o teste / testes:
     expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
   });
@@ -27,10 +21,39 @@ describe('Verifique se aplicação contém um conjunto fixo de links de navegaç
   it('Existem os respectivos texto "Favorite Pokémons" na página:', () => {
     // Acessar elementos da sua tela:
     RenderWithRouter(<App />);
-
-    // Interagir com eles (se houver necessidade):
-
     // Fazer o teste / testes:
     expect(screen.getByRole('link', { name: 'Favorite Pokémons' })).toBeInTheDocument();
+  });
+});
+
+describe('Verifique se os links são direcionados conforme o esperado:', () => {
+  it('Teste se a aplicação é redirecionada ao clicar no link "/"', () => {
+    // Acessar elementos da sua tela:
+    const { history } = RenderWithRouter(<App />);
+    // Interagir com eles (se houver necessidade):
+    fireEvent.click(screen.getByText('Home'));
+    // Fazer o teste / testes:
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Teste se a aplicação é redirecionada ao clicar no link "/about"', () => {
+    // Acessar elementos da sua tela:
+    const { history } = RenderWithRouter(<App />);
+    // Interagir com eles (se houver necessidade):
+    fireEvent.click(screen.getByText('About'));
+    // Fazer teste / testes:
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Teste se aplicação é direcionada ao clicar no link "/favorites"', () => {
+    // Acessar elementos da sua tela:
+    const { history } = RenderWithRouter(<App />);
+    // Interagir com eles (se houver necessidade):
+    fireEvent.click(screen.getByText('Favorite Pokémons'));
+    // Fazer teste / testes:
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
   });
 });

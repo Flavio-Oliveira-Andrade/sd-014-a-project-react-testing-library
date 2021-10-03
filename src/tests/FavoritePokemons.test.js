877cb41b72a4c68f89c6552da5b76429913ceaa2
Andrea <andrealh08@gmail.com>
2021-10-03 20:32:24 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test.skip('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Testa o componente "FavoritePokemons"', () => {
+  test('Verifica se é exibido na tela a mensagem `No favorite pokemon found`,'
+  + 'se a pessoa não tiver pokémons favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+
+    const noFavoriteText = screen.getByText('No favorite pokemon found');
+    expect(noFavoriteText).toBeInTheDocument();
+  });
+
+  test('Verifica se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const moreDetailsButton = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetailsButton);
+
+    const favoriteCheckbox = screen.getByRole('checkbox');
+    expect(favoriteCheckbox).toBeInTheDocument();
+    userEvent.click(favoriteCheckbox);
+
+    history.push('/favorites');
+
+    const pokemonName = screen.getByText(/pikachu/i);
+    const pokemonType = screen.getByText(/electric/i);
+    const pokemonWeight = screen.getByText(/average/i);
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonType).toBeInTheDocument();
+    expect(pokemonWeight).toBeInTheDocument();
+  });
+});

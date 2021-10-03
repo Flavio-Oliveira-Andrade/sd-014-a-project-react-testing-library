e3c9b7534833fd71baa6c11f350f2ef6b9e6c3d1
Yasmin <yasminleticia_@hotmail.com>
2021-10-03 08:48:47 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste se renderiza o componente App', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText(/Home/i);
+    const linkAbout = screen.getByText(/About/i);
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    const linkFavoritePokemons = screen.getByText(/Favorite Pokémons/i);
+    expect(linkFavoritePokemons).toBeInTheDocument();
+  });
+
+  it('Teste se renderiza a Home', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Teste se renderiza About', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Teste se renderiza Favorite Pokemons', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Teste se renderiza Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/dfsdfsd');
+    const notFound = screen.getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+});

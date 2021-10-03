d62e5408ba47e1d9a6769a97f45de5ce339f1883
Yasmin <yasminleticia_@hotmail.com>
2021-10-03 11:51:10 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Teste o componente FavoritePokemons', () => {
+  it('Teste se é exibido No favorite pokemon found,'
+  + 'se a pessoa não tiver pokémons favoritos', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[2]] } />);
+    const namePoke = screen.getByText('Caterpie');
+    const testId = screen.getByTestId('pokemon-name');
+    expect(namePoke).toBeInTheDocument();
+    expect(testId).toBeInTheDocument();
+  });
+});

f752e79c7063cd804ace512e792f686f04934a4c
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-04 16:07:50 -0300

    5 - Test if contains a heading with the text "Encountered pokémons"

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test Pokedex component', () => {
+  it('contains a heading with the text "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});

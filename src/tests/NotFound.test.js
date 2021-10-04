7b267a256b68bf269dd1b9f40821934f42ab2a22
vanessabastos <vanessarsb83@gmail.com>
2021-10-04 20:55:12 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando componente NotFound', () => {
+  test('Teste se página contém um heading com o texto'
+    + '`Page requested not found`', () => {
+    render(<NotFound />);
+
+    expect(screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    }))
+      .toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    render(<NotFound />);
+    const imageAlt = 'Pikachu crying because the page requested was not found';
+
+    expect(screen.getByAltText(imageAlt)).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

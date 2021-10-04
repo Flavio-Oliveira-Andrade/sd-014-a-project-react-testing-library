1b0459258dec172240b104b8bdbebe2580b9d5d4
JosueTavaress <josueferreira8.jf@gmail.com>
2021-10-04 17:25:31 -0300

    add requisito 5 test component <Pokedex />

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,90 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { fireEvent } from '@testing-library/dom';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('Teste o componente `<Pokedex.js />`', () => {
+  it('página contém um heading `h2` com o texto `Encountered pokémons`', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const h2Pkm = screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+      level: 2,
+    });
+    expect(h2Pkm).toBeInTheDocument();
+  });
+  it('Teste se é exibido o próximo Pokémon da lista quando o botão'
+    + '`Próximo pokémon` é clicado.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const proximoPkm = screen.getByRole('button', { name: /Próximo pokémon/i });
+
+    fireEvent.click(proximoPkm);
+
+    const PkmCharmander = screen.getByText('Charmander');
+    expect(PkmCharmander).toBeInTheDocument();
+  });
+  it('O botão deve conter o texto `Próximo pokémon`', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const textoBtn = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(textoBtn).toBeInTheDocument();
+  });
+  it('Teste se a Pokédex tem os botões de filtro.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const typeAll = screen.getByRole('button', { name: /All/i });
+    const typeEletric = screen.getByRole('button', { name: /Electric/i });
+    const typeFire = screen.getByRole('button', { name: /Fire/i });
+    const typeBug = screen.getByRole('button', { name: /Bug/i });
+    const typePoison = screen.getByRole('button', { name: /Poison/i });
+    const typePsychic = screen.getByRole('button', { name: /Psychic/i });
+
+    expect(typeAll).toBeInTheDocument();
+    expect(typeEletric).toBeInTheDocument();
+    expect(typeFire).toBeInTheDocument();
+    expect(typeBug).toBeInTheDocument();
+    expect(typePoison).toBeInTheDocument();
+    expect(typePsychic).toBeInTheDocument();
+  });
+  it('A partir da seleção de um botão de tipo, a Pokédex deve'
+    + 'circular somente pelos pokémons daquele tipo', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const cardId = screen.getByTestId('pokemon-type');
+    const buttonId = screen.getAllByTestId('pokemon-type-button');
+    const allSelect = screen.getByRole('button', { name: /all/i });
+
+    expect(cardId).toBeInTheDocument();
+    expect(buttonId[0]).toBeInTheDocument();
+
+    fireEvent.click(buttonId[0]);
+
+    expect(cardId.innerHTML).toBe(buttonId[0].innerHTML);
+
+    fireEvent.click(allSelect);
+    expect(allSelect.innerHTML).not.toBe(cardId.innerHTML);
+  });
+});

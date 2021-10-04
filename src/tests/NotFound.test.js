a0c309bc7593b96ececa8962e0cf946b1aa48bdc
juli0c3sar <jcmuchiutti@gmail.com>
2021-10-04 13:49:29 -0300

    requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,5 +1,21 @@
-describe('', () => {
-  test('', () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import RenderWithRouter from '../util/RenderWithRouter';
 
+describe('testando o <NotFound />', () => {
+  test('testando se existe um h2', () => {
+    RenderWithRouter(<NotFound />);
+    const getTextoNotfound = screen.getByText(/Page requested not found/i);
+    expect(getTextoNotfound).toBeInTheDocument();
+  });
+  test('verificando img', () => {
+    RenderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });

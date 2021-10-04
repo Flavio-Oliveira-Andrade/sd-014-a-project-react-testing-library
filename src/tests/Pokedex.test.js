81bdff669c9bf27c32500d3c50755aa1dfcb2e4f
Semnome1505 <Gabriel_1505@outlook.com>
2021-10-04 00:18:53 -0300

    Adiciona o teste 5 do Requisito 5.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -5,6 +5,10 @@ import Pokedex from '../components/Pokedex';
 import pokemons from '../data';
 import App from '../App';
 
+//  https://stackoverflow.com/questions/62544907/define-a-constant-instead-of-duplicating-this-literal-3-times-sonar
+const POKEMON_NAME = 'pokemon-name';
+const PROXIMO_POKEMON = 'Próximo pokémon';
+
 describe('Requisito 5', () => {
   test('Página contém um heading h2 com o texto Encountered pokémons', () => {
     const setIs = App.setIsPokemonFavoriteById();
@@ -32,11 +36,11 @@ describe('Requisito 5', () => {
       name: 'All',
     });
     expect(btnAll).toBeInTheDocument();
-    const screenName = screen.getByTestId('pokemon-name');
+    const screenName = screen.getByTestId(POKEMON_NAME);
     pokemons.forEach((poke) => {
       const pokeName = poke.name;
       expect(screenName).toHaveTextContent(pokeName);
-      const btnNext = screen.getByText('Próximo pokémon');
+      const btnNext = screen.getByText(PROXIMO_POKEMON);
       fireEvent.click(btnNext);
     });
     const pokeName = pokemons[0].name;
@@ -51,9 +55,9 @@ describe('Requisito 5', () => {
       />,
     );
     pokemons.forEach(() => {
-      const pokemonsById = screen.getAllByTestId('pokemon-name');
+      const pokemonsById = screen.getAllByTestId(POKEMON_NAME);
       expect(pokemonsById.length).toBe(1);
-      const btnNext = screen.getByText('Próximo pokémon');
+      const btnNext = screen.getByText(PROXIMO_POKEMON);
       fireEvent.click(btnNext);
     });
   });
@@ -65,7 +69,15 @@ describe('Requisito 5', () => {
         isPokemonFavoriteById={ setIs }
       />,
     );
-    const pokeTypes = [[[ 'Electric'],[ 1 ]], [[ 'Fire'],[ 2 ]], [[ 'Bug'],[ 1 ]], [[ 'Poison'],[ 1 ]], [[ 'Psychic'],[ 2 ]], [[ 'Normal'],[ 1 ]], [[ 'Dragon'],[ 1 ]]];
+    const pokeTypes = [
+      [['Electric'], [1]],
+      [['Fire'], [2]],
+      [['Bug'], [1]],
+      [['Poison'], [1]],
+      [['Psychic'], [2]],
+      [['Normal'], [1]],
+      [['Dragon'], [1]],
+    ];
 
     const btnTypes = screen.getAllByTestId('pokemon-type-button');
     const btnElectric = screen.getByRole('button', {
@@ -106,7 +118,7 @@ describe('Requisito 5', () => {
       fireEvent.click(btnType);
       const pokeType = screen.getByTestId('pokemon-type');
       expect(pokeType).toHaveTextContent(type);
-      const btnNext = screen.getByText('Próximo pokémon');
+      const btnNext = screen.getByText(PROXIMO_POKEMON);
       for (let i = 1; i < number; i += 1) {
         fireEvent.click(btnNext);
         expect(pokeType).toHaveTextContent(type);
@@ -114,8 +126,31 @@ describe('Requisito 5', () => {
       const btnAll = screen.getByRole('button', {
         name: 'All',
       });
-      expect(btnAll)
       expect(btnAll).toBeVisible();
     });
+  });
+  test('A Pokédex contém um botão para resetar o filtro', () => {
+    const setIs = App.setIsPokemonFavoriteById();
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ setIs }
+      />,
+    );
+    const NUM_POKEMONS = 9;
+    const btnNext = screen.getByText(PROXIMO_POKEMON);
+    for (let i = 0; i < NUM_POKEMONS; i += 1) {
+      const pokeName = screen.getByTestId(POKEMON_NAME);
+      expect(pokeName).toHaveTextContent(pokemons[i].name);
+      fireEvent.click(btnNext);
+    }
+    const btnAll = screen.getByText('All');
+    expect(btnAll).toBeInTheDocument();
+    fireEvent.click(btnAll);
+    for (let i = 0; i < NUM_POKEMONS; i += 1) {
+      const pokeName = screen.getByTestId(POKEMON_NAME);
+      expect(pokeName).toHaveTextContent(pokemons[i].name);
+      fireEvent.click(btnNext);
+    }
   });
 });

f774676362f49fcc56253a9025b71902922d8bb5
Pablo Assunção <pabloassuncao2@gmail.com>
2021-10-04 19:49:03 -0300

    Adiciona o requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,91 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+const isPokemonFavoriteById = {
+  4: true,
+  10: false,
+  23: false,
+  25: true,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: true,
+};
+
+describe('Teste da Pokédex', () => {
+  test('Verifica se há o texto Encountered Pokémons', () => {
+    render(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />, { wrapper: BrowserRouter });
+
+    const title = screen.getByRole('heading', { leve: 2, name: /Encountered pokémons/i });
+
+    expect(title).toBeInTheDocument();
+  });
+  test('Verifica se o botão Próximo Pokémon funciona', () => {
+    render(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />, { wrapper: BrowserRouter });
+
+    const pokemonName = 'pokemon-name';
+
+    let pokemon = screen.getByTestId(pokemonName);
+    expect(pokemon).toHaveTextContent('Pikachu');
+
+    const nextBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(nextBtn);
+
+    pokemon = screen.getByTestId(pokemonName);
+    expect(pokemon).toHaveTextContent('Charmander');
+
+    userEvent.click(nextBtn);
+    userEvent.click(nextBtn);
+    userEvent.click(nextBtn);
+    userEvent.click(nextBtn);
+    userEvent.click(nextBtn);
+    userEvent.click(nextBtn);
+    userEvent.click(nextBtn);
+    userEvent.click(nextBtn);
+
+    pokemon = screen.getByTestId(pokemonName);
+    expect(pokemon).toHaveTextContent('Pikachu');
+  });
+  test('Verifica se renderiza apenas um pokémon por vez', () => {
+    render(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />, { wrapper: BrowserRouter });
+
+    const display = screen.getAllByTestId('pokemon-name');
+
+    expect(display).toHaveLength(1);
+  });
+  test('Verifica se renderiza apenas um pokémon por vez', () => {
+    render(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />, { wrapper: BrowserRouter });
+
+    const button = screen.getAllByTestId('pokemon-type-button');
+    const TYPES_QUANT = 7;
+
+    expect(button).toHaveLength(TYPES_QUANT);
+
+    const repeat = button.reduce((acc, type) => {
+      if (acc) return;
+      let counter = 0;
+      console.log(type);
+      button.forEach((element) => { if (element.text === type.text) counter += 1; });
+      acc = counter >= 2;
+      return acc;
+    }, false);
+    console.log(repeat);
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,68 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+describe('Teste da Pokemon Card', () => {
+  test('Verifica as informações do Pokemon', () => {
+    render(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite
+    />, { wrapper: BrowserRouter });
+
+    const name = screen.getByTestId('pokemon-name');
+    const type = screen.getByTestId('pokemon-type');
+    const weight = screen.getByTestId('pokemon-weight');
+    const img = screen.getByRole('img', { name: /pikachu sprite/i });
+    const url = pokemons[0].image;
+    const { value } = pokemons[0].averageWeight;
+    const { measurementUnit } = pokemons[0].averageWeight;
+
+    expect(name).toHaveTextContent('Pikachu');
+    expect(type).toHaveTextContent('Electric');
+    expect(weight).toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', url);
+  });
+  test('Verifica se existe o link More Details', () => {
+    render(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite
+    />, { wrapper: BrowserRouter });
+
+    const link = screen.getByRole('link', { name: /more details/i });
+
+    expect(link).toBeInTheDocument();
+  });
+  test('Verifica o redirecionamento do link MoreDetails', () => {
+    const isFavorite = true;
+    render(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite={ isFavorite }
+    />, { wrapper: BrowserRouter });
+
+    const { id } = pokemons[0];
+    const link = screen.getByRole('link', { name: /more details/i });
+
+    expect(link).toBeInTheDocument();
+
+    userEvent.click(link);
+
+    expect(window.location.pathname).toStrictEqual(`/pokemons/${id}`);
+  });
+  test('Verifica a img de Favorito', () => {
+    const isFavorite = true;
+    render(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite={ isFavorite }
+    />, { wrapper: BrowserRouter });
+
+    const img = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    const url = '/star-icon.svg';
+
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', url);
+  });
+});

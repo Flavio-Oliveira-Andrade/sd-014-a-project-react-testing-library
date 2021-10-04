5c34e3934dfe9439e00af8e1622a1a8502567c0f
AlineCarolina <eualine2017@gmail.com>
2021-10-04 18:29:26 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Pokemon.js', () => {
+  it('Teste se é rndrzd um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+    expect((screen.getByTestId('pokemon-name').innerHTML)).toBe('Pikachu');
+  });
+});

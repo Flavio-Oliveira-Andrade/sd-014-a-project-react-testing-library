9bfcf25c146ed68d121366ee1b17e9d9f4557857
Carlos Afonso <carlos.flach15@gmail.com>
2021-10-04 18:06:27 -0300

    Testing Not Found Component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testa o se o componente Not Found renderiza', () => {
+  it('Verifica se apresenta o h2 com texto "Page requested not found"', () => {
+    render(<NotFound />);
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verifica se a pagina Not Found mostra a imagem do pikachu', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText(/pikachu crying/i);
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', imgUrl);
+  });
+});

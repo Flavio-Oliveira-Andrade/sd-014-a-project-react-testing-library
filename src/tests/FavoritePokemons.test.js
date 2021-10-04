adf2681a21cb69ac75997de3d375c06e72877ec0
AlineCarolina <eualine2017@gmail.com>
2021-10-04 14:54:46 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('FavoritePokemons.js Testes', () => {
+  it('Teste se é exibido "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    expect((screen.getByText('No favorite pokemon found'))).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[0]] } />);
+    expect((screen.getByTestId('pokemon-name'))).toBeInTheDocument();
+  });
+});

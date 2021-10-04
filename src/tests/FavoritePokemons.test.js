16dfbda463d4fbebc79d332315381b9ac6f290d1
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-04 11:52:37 -0300

    3 - Test if displays all favorite Pokemon cards

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
 
 describe('Test FavoritePokemons component', () => {
   it('should display No favorite pokemon found message', () => {
     render(<FavoritePokemons />);
     const message = screen.getByText('No favorite pokemon found');
     expect(message).toBeInTheDocument();
+  });
+
+  it('should display all favorite Pokemon cards', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const cards = screen.getAllByTestId('pokemon-name');
+    expect(cards.length).toBe(pokemons.length);
   });
 });

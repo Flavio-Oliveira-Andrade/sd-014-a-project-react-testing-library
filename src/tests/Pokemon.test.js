a4679531912fb88c4ce20426d3b7dbbb8b6d5a2f
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-10-04 17:44:46 -0300

    Estruturaçao do exercicio 6 do projeto

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('testa o componente "Pokemon"', () => {
+  test('renderiza um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+  });
+
+  test('link possui URL: /pokemons/<id>, onde <id> é o id do Pokémon exibido', () => {
+    renderWithRouter(<App />);
+  });
+
+  test('redireciona a aplicação para a página de detalhes de Pokémon', () => {
+    renderWithRouter(<App />);
+  });
+
+  test('URL exibida no navegador muda para /pokemon/<id>, <id> é o id do Pokémon', () => {
+    renderWithRouter(<App />);
+  });
+
+  test('existe um ícone de estrela nos Pokémons favoritados', () => {
+    renderWithRouter(<App />);
+  });
+});

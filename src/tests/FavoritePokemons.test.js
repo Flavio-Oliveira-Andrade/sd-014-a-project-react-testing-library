61575872f6ba647c35ad992df28f551c6dd9435b
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-04 10:56:14 -0300

    3 - Test if displays No favorite pokemon found message

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Test FavoritePokemons component', () => {
+  it('should display No favorite pokemon found message', () => {
+    render(<FavoritePokemons />);
+    const message = screen.getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+});

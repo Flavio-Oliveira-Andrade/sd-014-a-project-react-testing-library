a66ba7737997f53499c6cc8220722f8b79b79bc9
AlineCarolina <eualine2017@gmail.com>
2021-10-04 15:28:47 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('NotFound.js Testes', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-inexistente/');
+  });
+  it('Teste se pg contém um heading h2 com o txt Page requested not found 😭', () => {
+    expect((screen.getByText('Page requested not found'))).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem', () => {
+    expect((screen.getByRole('img', { name: /Pikachu crying because/i })).src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

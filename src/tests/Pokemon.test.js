eaa5538ea7e41f57329b94c25ffb89b289b29c80
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-10-04 17:06:50 -0300

    corrige requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import RenderWithRouter from './Util/RenderWithRouter';
+import data from '../data';
+import App from '../App';
+
+describe('Verifica o componente Pokemon.js', () => {
+  it('Verifca se o nome, tipo, peso médio e imagem do pokemon é mostrado na tela', () => {
+    RenderWithRouter(<App />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type').innerHTML;
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    const pokemonImg = screen.getByRole('img', { name: `${data[0].name} sprite` });
+
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonType).toBe('Electric');
+    expect(pokemonWeight).toBeInTheDocument();
+    expect(pokemonImg).toHaveAttribute('src', data[0].image);
+  });
+  it('Verifica link e redirecionamento de "More Info"', () => {
+    RenderWithRouter(<App />);
+
+    const moreInfoLink = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    expect(moreInfoLink).toBeInTheDocument();
+
+    userEvent.click(moreInfoLink);
+    expect(moreInfoLink.href).toEqual('http://localhost/pokemons/25');
+  });
+  it('Verifica se ícone de favoritos é uma estrela', () => {
+    RenderWithRouter(<App />);
+
+    const moreInfoLink = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(moreInfoLink);
+
+    const addFavorite = screen.getByRole('checkbox');
+
+    userEvent.click(addFavorite);
+
+    const favoritesPokemons = screen.getByAltText('Pikachu is marked as favorite');
+    const starImg = '/star-icon.svg';
+    expect(favoritesPokemons).toHaveAttribute('src', starImg);
+  });
+});

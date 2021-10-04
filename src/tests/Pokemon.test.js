8d7836b25baad3a1fd0895c6e9c85c67bd868e43
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-10-04 15:07:10 -0300

    req 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,31 +5,31 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Req 5 pokemon.test', () => {
-  test('Verifica se nome correto do Pokémon é mostrado na tela;', () => {
+  test('checks if the correct pokemon is rendered on screen;', () => {
     renderWithRouter(<App />);
     const pokemon = screen.getByText(/Pikachu/i);
     expect(pokemon).toBeInTheDocument();
   });
 
-  test('Verifica se o tipo correto do Pokémon é mostrado na tela', () => {
+  test('checks if the rendered pokemon element is the correct one', () => {
     renderWithRouter(<App />);
     const pokemonElement = screen.getAllByText(/Electric/i);
     expect(pokemonElement).toHaveLength(2);
   });
 
-  test('Verifica se o peso médio do Pokémon é mostrado na tela', () => {
+  test('checks if the pokemon weight is rendered', () => {
     renderWithRouter(<App />);
     const pokemonWeight = screen.getByText('Average weight: 6.0 kg');
     expect(pokemonWeight).toBeInTheDocument();
   });
 
-  test('Verifica se a imagem do pokemon é mostrada na tela', () => {
+  test('checks if the pokemon image is rendered', () => {
     renderWithRouter(<App />);
     const pokemonThumbnail = screen.getByRole('img', { name: 'Pikachu sprite' });
     expect(pokemonThumbnail.src).toStrictEqual('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
 
-  test('Verifica funcionalidade dos detalhes do card do Pokémon', () => {
+  test('checks the pokemon card', () => {
     const { history } = renderWithRouter(<App />);
     const details = screen.getByRole('link', { name: /More Details/i });
     expect(details).toBeInTheDocument();

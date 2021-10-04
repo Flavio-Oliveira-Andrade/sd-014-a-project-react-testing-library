60d340c43f2e41fa64d7c35604d175b00e60524a
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-04 09:33:10 -0300

    1 - Test if the top of the app contains a fixed set of nav links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Test App component', () => {
+  it('should contain a fixed set of nav links at the top of the app', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritePokemon).toBeInTheDocument();
+  });
+});

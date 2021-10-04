75288cc1ed59a5144b3d5c802fefc14ecb40131b
JosueTavaress <josueferreira8.jf@gmail.com>
2021-10-04 10:45:47 -0300

    add requisito 2 -> component <About/>

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
+import { fireEvent } from '@testing-library/dom';
+import App from '../App';
+
+describe('Teste o componente `<About.js />.`', () => {
+  it('Teste se a página contém as informações sobre a Pokédex.', async () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+
+    fireEvent.click(linkAbout);
+
+    const aboutPokedex = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+
+    const img = screen.getByAltText(/Pokédex/i);
+
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(img).toHaveAttribute('alt', 'Pokédex');
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+});

997c66cf2a891981fe3db50c7b8d111962f4b753
Guilherme Leao <leaob7@gmail.com>
2021-10-04 11:26:16 -0300

    change for lint

---
+++ src/dataP.js
@@ -0,0 +1,3 @@
+const p = 'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons';
+
+export default p
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import About from '../components/About';
+import p from '../dataP';
 
 test('verify informations of Pokedex to be in the document', () => {
   render(<About />);
@@ -17,7 +18,7 @@ test('verify h2 to have correct text', () => {
 
 test('verify paragraphs to have correct text', () => {
   render(<About />);
-  const p = 'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons';
+  // const p = 'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons';
   const primeiroP = screen.getByText(p);
   const p2 = 'One can filter Pokémons by type, and see more details for each one of them';
   const segundoP = screen.getByText(p2);

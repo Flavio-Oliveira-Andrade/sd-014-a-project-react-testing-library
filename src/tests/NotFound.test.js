c2923a2316e983656c176b573e813512ccc8b7f6
Andrea <andrealh08@gmail.com>
2021-10-03 21:32:17 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test.skip('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Teste o componente "NotFound"', () => {
+  test('Verifica que a página contém um heading `h2`'
+  + 'com o texto `Page requested not found 😭`', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notFound');
+
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji' });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('Verifica se a página mostra a imagem `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notFound');
+
+    const notFoundImage = screen.getAllByRole('img');
+    expect(notFoundImage[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(notFoundImage[1]).toBeInTheDocument();
+  });
+});

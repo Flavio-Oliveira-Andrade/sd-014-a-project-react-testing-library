05943d434039777074838ee16b62ae637996a6ab
Thiago Oshiro <oshirothiago@gmail.com>
2021-10-04 18:26:43 -0300

    correcoes lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -6,40 +6,37 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import pokemons from '../data';
 
-const getBtn = (text) => {
-  return screen.getByRole('button', {
-    name: text,
-  });
-}
+const getBtn = (text) => screen.getByRole('button', { name: text });
 
 const pokeFilter = (btnType, pokemonType) => {
   const nextBtn = getBtn('Próximo pokémon');
 
   userEvent.click(btnType);
   const displayedPokemon = screen.getByTestId('pokemon-type');
-   
+
   expect(displayedPokemon.textContent).toMatch(btnType.textContent);
-  
+
   const filteredPokemons = pokemons.filter((pokemon) => pokemon.type === pokemonType);
   filteredPokemons.forEach((pokemon) => {
     const pokemonScreen = screen.getByText(pokemon.name);
     expect(pokemonScreen).toBeInTheDocument();
     userEvent.click(nextBtn);
   });
-}
+};
 
 describe('Testa o componente "Pokedex"', () => {
   test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
     render(<MemoryRouter><App /></MemoryRouter>);
     const pokedexHeading = screen.getByRole('heading', {
-      level:2,
+      level: 2,
       name: 'Encountered pokémons',
     });
 
     expect(pokedexHeading).toBeInTheDocument();
   });
 
-  test('Testa a lista de pokemons e o botão "proximo pokemon" funcionam corretamente', () => {
+  test('Testa a lista de pokemons e o botão'
+  + '"proximo pokemon" funcionam corretamente', () => {
     render(<MemoryRouter><App /></MemoryRouter>);
 
     const allFilterBtn = screen.getByRole('button', {
@@ -47,24 +44,23 @@ describe('Testa o componente "Pokedex"', () => {
     });
 
     userEvent.click(allFilterBtn);
-    
+
     const nextBtn = screen.getByTestId('next-pokemon');
     const pokemonDataTest = screen.getAllByTestId('pokemon-name');
-   
-    const arrayPokemons = pokemons.reduce((acc, pokemon, ) => {
+
+    const arrayPokemons = pokemons.reduce((acc, pokemon) => {
       const pokemonName = screen.getByText(pokemon.name);
       acc.push(pokemon);
       expect(pokemonName).toBeInTheDocument();
       expect(pokemonDataTest.length).toBe(1);
       userEvent.click(nextBtn);
       return acc;
-     }, []);
-    
+    }, []);
+
     const firstPokeName = arrayPokemons[0].name;
     const firstNameScreen = screen.getByText(firstPokeName);
     expect(firstNameScreen).toBeInTheDocument();
     expect(pokemonDataTest.length).toBe(1);
-
   });
 
   test('Testa os botões de filtro de pokemons', () => {
@@ -77,7 +73,7 @@ describe('Testa o componente "Pokedex"', () => {
     const psychicBtn = getBtn('Psychic');
     const normalBtn = getBtn('Normal');
     const dragonBtn = getBtn('Dragon');
-    
+
     expect(allBtn).toBeInTheDocument();
     expect(eletricBtn).toBeInTheDocument();
     expect(fireBtn).toBeInTheDocument();
@@ -85,7 +81,7 @@ describe('Testa o componente "Pokedex"', () => {
     expect(poisonBtn).toBeInTheDocument();
     expect(psychicBtn).toBeInTheDocument();
     expect(normalBtn).toBeInTheDocument();
-    expect(dragonBtn).toBeInTheDocument();    
+    expect(dragonBtn).toBeInTheDocument();
   });
 
   test('Testa cada filtro e seu funcionamento', () => {
@@ -97,11 +93,7 @@ describe('Testa o componente "Pokedex"', () => {
     const psychicBtn = getBtn('Psychic');
     const normalBtn = getBtn('Normal');
     const dragonBtn = getBtn('Dragon');
-        
-    userEvent.click(eletricBtn);
-    const displayedPokemon = screen.getByTestId('pokemon-type');
-    expect(displayedPokemon.textContent).toMatch(eletricBtn.textContent);
-    
+
     pokeFilter(eletricBtn, 'Electric');
     pokeFilter(fireBtn, 'Fire');
     pokeFilter(bugBtn, 'Bug');

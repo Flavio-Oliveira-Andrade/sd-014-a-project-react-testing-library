1ec303523213e95019d770c24ca5aede8bc2ae96
vanessabastos <vanessarsb83@gmail.com>
2021-10-04 19:19:39 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+const noFound = 'No favorite pokemon found';
+
+describe('Testando componente FavortitePokemons', () => {
+  test('Teste se é exibido na tela a mensagem `No favorite pokemon found`', () => {
+    // passo 1 adicionar componente
+    render(<FavoritePokemons />);
+
+    // passo 2 interagir com ele(caso aja necessidade)
+
+    // passo 3 fazer o teste
+    expect(screen.getByText(noFound)).toBeInTheDocument();
+  });
+
+  // test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+  //   // passo 1 adicionar componente
+  //   render(<FavoritePokemons />);
+
+  //   // passo 2 interagir com ele(caso aja necessidade)
+
+  //   // passo 3 fazer o teste
+  //   expect(screen.('section')).toBeInTheDocument();
+  // });
+});

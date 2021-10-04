a6b5e10f098e848846f4168ff0d9d2addb7ea4f8
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-04 16:24:53 -0300

    5 - Test if displays the next Pokémon in the list when the button is clicked

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import App from '../App';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
 
 describe('Test Pokedex component', () => {
   it('contains a heading with the text "Encountered pokémons"', () => {
@@ -11,5 +13,25 @@ describe('Test Pokedex component', () => {
       name: 'Encountered pokémons',
     });
     expect(heading).toBeInTheDocument();
+  });
+
+  it('displays the next Pokémon in the list when the button is clicked', () => {
+    renderWithRouter(<App />);
+    const nextPokemonButton = screen.getByRole('button', {
+      name: 'Próximo pokémon',
+    });
+    expect(nextPokemonButton).toBeInTheDocument();
+
+    pokemons.forEach(({ name }) => {
+      const pokemonName = screen.getByTestId('pokemon-name');
+      expect(pokemonName).toBeInTheDocument();
+      expect(pokemonName).toHaveTextContent(name);
+      userEvent.click(nextPokemonButton);
+    });
+
+    // Displays the first Pokémon again
+    const firstPokemonName = screen.getByTestId('pokemon-name');
+    expect(firstPokemonName).toBeInTheDocument();
+    expect(firstPokemonName).toHaveTextContent(pokemons[0].name);
   });
 });

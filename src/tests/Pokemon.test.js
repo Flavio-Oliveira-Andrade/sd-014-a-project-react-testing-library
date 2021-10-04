7151d3c74082a531ad8ed1818193c159878e92b9
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-10-04 18:29:57 -0300

    Testes passando para o primeiro requisito do exercicio 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import pokemons from '../data';
 
 describe('testa o componente "Pokemon"', () => {
   test('renderiza um card com as informações de determinado pokémon', () => {
     renderWithRouter(<App />);
+
+    const pokemonName = screen.getByTestId('pokemon-name').innerHTML;
+    const pokemonType = screen.getByTestId('pokemon-type').innerHTML;
+    const averageWeigth = screen.getByTestId('pokemon-weight').innerHTML;
+    const spritePokemon = screen.getByRole('img', { name: 'Pikachu sprite' });
+
+    expect(pokemonName).toBe('Pikachu');
+    expect(pokemonType).toBe('Electric');
+    expect(averageWeigth).toBe('Average weight: 6.0 kg');
+    expect(spritePokemon.src).toBe(pokemons[0].image);
   });
 
   test('link possui URL: /pokemons/<id>, onde <id> é o id do Pokémon exibido', () => {

43826e2529d7e8a6fa08a19fce5646a93e7abc37
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-04 10:44:21 -0300

    2 - Test if the page contain a heading

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Test About component', () => {
+  it('should contain a heading with the text About Pokédex', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(heading).toBeInTheDocument();
+  });
+});

eb74e62896e074c08fb92e9bcb3d7445da1cd240
Pablo Assunção <pabloassuncao2@gmail.com>
2021-10-05 12:35:41 -0300

    Corrige o requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste da página FavoritePokemons', () => {
+  test('Verifica se há a mensagem No Favorite Pokemons', () => {
+    render(<FavoritePokemons />, { wrapper: BrowserRouter });
+
+    const notFoundMSG = screen.getByText(/no favorite pokemon found/i);
+
+    expect(notFoundMSG).toBeInTheDocument();
+  });
+});

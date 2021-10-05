7a15d172ad115d222cceecbdc7602f29c53f1f39
Elielson Nascimento <nascimento.elielson@gmail.com>
2021-10-04 21:18:25 -0300

    Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testando componente FavortitePokemons', () => {
+  test('Teste se é exibido na tela a mensagem `No favorite pokemon found`', () => {
+    // passo 1 adicionar componente
+    render(<FavoritePokemons />);
+    // passo 2 interagir com ele(caso aja necessidade)
+    // passo 3 fazer o teste
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});

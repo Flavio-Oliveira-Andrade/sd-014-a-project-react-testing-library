74b0930c05c17ef67e5054141a804d1d2ea5a214
JosueTavaress <josueferreira8.jf@gmail.com>
2021-10-05 14:23:14 -0300

    add function historyRouter test Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { fireEvent } from '@testing-library/dom';
-import { Router } from 'react-router';
-import { createMemoryHistory } from 'history';
 import App from '../App';
+import historyRouter from '../services/historyRouter';
 
 describe('Teste o componente `<Pokedex.js />`', () => {
   it('página contém um heading `h2` com o texto `Encountered pokémons`', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <App />
-      </Router>,
-    );
+    historyRouter(<App />);
     const h2Pkm = screen.getByRole('heading', {
       name: /Encountered pokémons/i,
       level: 2,
@@ -21,12 +15,7 @@ describe('Teste o componente `<Pokedex.js />`', () => {
   });
   it('Teste se é exibido o próximo Pokémon da lista quando o botão'
     + '`Próximo pokémon` é clicado.', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <App />
-      </Router>,
-    );
+    historyRouter(<App />);
     const proximoPkm = screen.getByRole('button', { name: /Próximo pokémon/i });
 
     fireEvent.click(proximoPkm);
@@ -35,22 +24,13 @@ describe('Teste o componente `<Pokedex.js />`', () => {
     expect(PkmCharmander).toBeInTheDocument();
   });
   it('O botão deve conter o texto `Próximo pokémon`', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <App />
-      </Router>,
-    );
+    historyRouter(<App />);
     const textoBtn = screen.getByRole('button', { name: 'Próximo pokémon' });
     expect(textoBtn).toBeInTheDocument();
   });
   it('Teste se a Pokédex tem os botões de filtro.', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <App />
-      </Router>,
-    );
+    historyRouter(<App />);
+
     const typeAll = screen.getByRole('button', { name: /All/i });
     const typeEletric = screen.getByRole('button', { name: /Electric/i });
     const typeFire = screen.getByRole('button', { name: /Fire/i });
@@ -67,12 +47,7 @@ describe('Teste o componente `<Pokedex.js />`', () => {
   });
   it('A partir da seleção de um botão de tipo, a Pokédex deve'
     + 'circular somente pelos pokémons daquele tipo', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <App />
-      </Router>,
-    );
+    historyRouter(<App />);
     const cardId = screen.getByTestId('pokemon-type');
     const buttonId = screen.getAllByTestId('pokemon-type-button');
     const allSelect = screen.getByRole('button', { name: /all/i });

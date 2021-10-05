10ee5604f9f16ac791eb7465ee5a3959f549923d
Saulo Lima <saulolima.c@protonmail.com>
2021-10-04 21:45:10 -0300

    Requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+const imageAddress = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+const AtlText = 'Pikachu crying because the page requested was not found';
+
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+
+  return render(ui, { wrapper: BrowserRouter });
+};
+
+describe('Testa a página NotFound', () => {
+  it('Testa se página tem um heading h2 com o texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByText(/Page requested not found/i);
+    expect(h2).toBeInTheDocument();
+  });
+  it('Testa se o pikachu está chorando', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', { name: AtlText });
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe(imageAddress);
+  });
+});

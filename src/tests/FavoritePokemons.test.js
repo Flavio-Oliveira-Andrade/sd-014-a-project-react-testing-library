1be79501fc62db480a6b42a59965e8e186c93d38
Filipe Brochier <filipe.brochier@gmail.com>
2021-10-05 20:51:06 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Test <FavoritePokemons.js /> component', () => {
+  it('should show a message if there are no favorite pokémons', () => {
+    render(<FavoritePokemons />);
+    const noFavoritePokemonMessage = screen.getByText(/No favorite pokemon found/i);
+
+    expect(noFavoritePokemonMessage).toBeInTheDocument();
+  });
+
+  it('should show every favorite pokémon card', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const pokemonCards = screen.getAllByTestId('pokemon-name');
+
+    expect(pokemonCards.length).toEqual(pokemons.length);
+  });
+});

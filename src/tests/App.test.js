ef58dea05ddb551f8c9d4531d72e596800e6755c
Elielson Nascimento <nascimento.elielson@gmail.com>
2021-10-04 21:12:48 -0300

    Requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,96 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../App';
+
+describe('1. Teste o componente `<App.js />`', () => {
+  it('O primeiro link deve possuir o texto `Home`:', () => {
+    // Passo 1 - Acesse o componente:
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    // Passo 2 - Interagir com ele (caso haja necessidade):
+    // Passo 3 - Faça o teste:
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+
+  it('O primeiro link deve possuir o texto `About`:', () => {
+    // Passo 1 - Acesse o componente:
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    // Passo 2 - Interagir com ele (caso haja necessidade):
+    // Passo 3 - Faça o teste:
+    expect(screen.getByText('About')).toBeInTheDocument();
+  });
+
+  it('O primeiro link deve possuir o texto `Favorite Pokémons`:', () => {
+    // Passo 1 - Acesse o componente:
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    // Passo 2 - Interagir com ele (caso haja necessidade):
+    // Passo 3 - Faça o teste:
+    expect(screen.getByRole('link', { name: 'Favorite Pokémons' })).toBeInTheDocument();
+  });
+
+  it('Teste se é redirecionada para URL `/` ao clicar no link `Home`:', () => {
+    // Passo 1 - Acesse o componente:
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    // Passo 2 - Interagir com ele (caso haja necessidade):
+
+    // Passo 3 - Faça o teste:
+    expect(screen.getByRole('link', { name: 'Home' })).toHaveAttribute('href', '/');
+  });
+
+  it('Teste se é redirecionada para URL `/about` ao clicar no link `About`:', () => {
+    // Passo 1 - Acesse o componente:
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    // Passo 2 - Interagir com ele (caso haja necessidade):
+
+    // Passo 3 - Faça o teste:
+    expect(screen.getByRole('link', { name: 'About' })).toHaveAttribute('href', '/about');
+  });
+
+  it('Teste se é redirecionada para URL `/` ao clicar em `Favorite Pokémons`:', () => {
+    // Passo 1 - Acesse o componente:
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    // Passo 2 - Interagir com ele (caso haja necessidade):
+
+    // Passo 3 - Faça o teste:
+    expect(screen
+      .getByRole('link', { name: 'Favorite Pokémons' }))
+      .toHaveAttribute('href', '/favorites');
+  });
+
+  it.skip('Teste se é direcionada `Not Found` se acessar uma URL desconhecida:', () => {
+    // Passo 1 - Acesse o componente:
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    // Passo 2 - Interagir com ele (caso haja necessidade):
+
+    // Passo 3 - Faça o teste:
+    expect(/* Falta implementar esse requisito. */);
+  });
+});

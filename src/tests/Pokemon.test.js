3ea7b23b98920c2bac9b6df26b68b158d04a3a4b
JosueTavaress <josueferreira8.jf@gmail.com>
2021-10-04 22:24:23 -0300

    add test implementation component <Pokemon />

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { Router } from 'react-router';
 import { createMemoryHistory } from 'history';
 import { fireEvent } from '@testing-library/dom';
+import data from '../data';
 import App from '../App';
 
 describe('Teste o componente `<Pokemon.js />`', () => {
@@ -13,16 +14,25 @@ describe('Teste o componente `<Pokemon.js />`', () => {
         <App />
       </Router>,
     );
-    const details = screen.getByRole('link', { name: /More details/i });
+    const {
+      name,
+      image,
+    } = data[0];
+    const details = screen.getByRole('link', { name: 'More details' });
     const nav = screen.getByRole('navigation');
+    const imgPkm = screen.getByAltText(/Pikachu sprite/i);
 
     const namePkm = screen.getByTestId('pokemon-name');
     const typePkm = screen.getByTestId('pokemon-type');
     const averagePkm = screen.getAllByTestId('pokemon-weight');
 
+    console.log(typePkm.innerHTML);
+
+    expect(imgPkm).toHaveAttribute('src', `${image}`);
     expect(nav).toBeInTheDocument();
     expect(namePkm).toBeInTheDocument();
     expect(typePkm).toBeInTheDocument();
+    expect(typePkm.innerHTML).toBe('Electric');
     expect(averagePkm[0].innerHTML).toBe('Average weight: 6.0 kg');
 
     fireEvent.click(details);
@@ -33,10 +43,11 @@ describe('Teste o componente `<Pokemon.js />`', () => {
     fireEvent.click(labelFavorite);
 
     const imgStar = screen.getByAltText(/Pikachu is marked as favorite/i);
+    const starLink = '/star-icon.svg';
 
     expect(imgStar).toBeInTheDocument();
-    expect(imgStar).toHaveAttribute('src', '/star-icon.svg');
-    expect(imgStar).toHaveAttribute('alt', 'Pikachu is marked as favorite');
+    expect(imgStar).toHaveAttribute('src', `${starLink}`);
+    expect(imgStar).toHaveAttribute('alt', `${name} is marked as favorite`);
     expect(details).toHaveAttribute('href', '/pokemons/25');
   });
 });

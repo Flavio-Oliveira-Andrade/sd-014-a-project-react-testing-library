e087a8ea02be0887472e205098078fbb92e6b9a7
Marcio <marciofonseca.ufrj@gmail.com>
2021-10-06 15:14:53 -0300

    Teste Home Link

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+it('should redirect to initial page', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const home = screen.getByRole('link', {
+    name: 'Home',
+  });
+  expect(home).toBeInTheDocument();
+});

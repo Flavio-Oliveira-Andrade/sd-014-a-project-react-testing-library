ab3ae1636ebe0b7ccc842539357fcf75b3f9c6d7
Marcio <marciofonseca.ufrj@gmail.com>
2021-10-06 15:28:17 -0300

    Requisito 1 completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import NotFound from '../components/NotFound';
 
 it('should redirect to initial page', () => {
   render(
@@ -13,4 +15,28 @@ it('should redirect to initial page', () => {
     name: 'Home',
   });
   expect(home).toBeInTheDocument();
+  userEvent.click(home);
+
+  const about = screen.getByRole('link', {
+    name: 'About',
+  });
+  expect(about).toBeInTheDocument();
+  userEvent.click(about);
+
+  const favoritePokemons = screen.getByRole('link', {
+    name: 'Favorite Pokémons',
+  });
+  expect(favoritePokemons).toBeInTheDocument();
+  userEvent.click(favoritePokemons);
+});
+
+it('Should redirect to page Not Found', () => {
+  render(
+    <NotFound />,
+  );
+  const notFoundLink = screen.getByRole('heading', {
+    level: 2,
+    name: /not found/i,
+  });
+  expect(notFoundLink).toBeInTheDocument();
 });

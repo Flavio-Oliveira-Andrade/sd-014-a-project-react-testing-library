919de3f7cc56adac4afb5f13895c77d03b8eac20
Filipe Brochier <filipe.brochier@gmail.com>
2021-10-05 21:08:31 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Test <NotFound.js /> component', () => {
+  it('should contain a heading (h2) with the following text: '
+    + '"Page requested not found"', () => {
+    render(<NotFound />);
+    const heading = screen.getByRole('heading',
+      { name: /Page requested not found/i,
+        level: 2,
+      });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should have an image (gif)', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText(/Pikachu crying because the page/);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

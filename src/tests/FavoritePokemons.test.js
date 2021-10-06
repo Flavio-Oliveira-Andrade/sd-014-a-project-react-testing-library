dcdd8d277729f593da7f982ec8fa978ba263253d
Marcio <marciofonseca.ufrj@gmail.com>
2021-10-06 19:26:24 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+it('should appear msg "No favorite pokemon found" if user doesnt have fav poke', () => {
+  render(
+    <FavoritePokemons />,
+  );
+  const paragraphFavorite = screen.getByText('No favorite pokemon found');
+  expect(paragraphFavorite).toBeInTheDocument();
+});
+it('should appear cards pokemon', () => {
+  renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+  const pikachu = screen.getByText('Pikachu');
+  expect(pikachu).toBeInTheDocument();
+});

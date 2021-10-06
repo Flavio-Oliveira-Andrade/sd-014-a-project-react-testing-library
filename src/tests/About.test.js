240eb41175b7199072955b1849042b5d014f0b6e
Vincenzo Fedzuirek Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2021-10-06 14:36:15 -0300

    Requisito 02 => 1/3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testes relacionados ao About', () => {
+  it('Informações sobre o pokedex são renderizadas', () => {
+    renderWithRouter(<About />);
+
+    const info01 = screen.getByText(/application simulates a Pokédex/i);
+    const info02 = screen.getByText(/and see more details for each/i);
+
+    expect(info01).toBeInTheDocument();
+    expect(info02).toBeInTheDocument();
+  });
+});

289ec1bf6c781c1be6d5276fcc392d348a1bf005
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-10-07 19:19:20 -0300

    finalizando requisito 2

---
+++ src/tests/About.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Componente About', () => {
+  beforeEach(() => render(<About />));
+  it('Teste se a página contém um heading "h2" com o texto "About Pokédex"', () => {
+    const h2 = screen.getByRole('heading',
+      {
+        name: 'About Pokédex',
+        level: 2,
+      });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const paragraph1 = screen.getByText(/This application simulates a Pokédex/);
+    const paragraph2 = screen.getByText(/One can filter Pokémons by type/);
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex: "https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png."', () => {
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

97606c2cb04a087231a0966a1edba2ca79eb23b8
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-10-07 19:46:58 -0300

    finalizando requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithReactRoute from './renderWithReactRoute';
+
+describe('Componente Not Found', () => {
+  it('Teste se página contém um heading h2 com o texto: Page requested not found', () => {
+    renderWithReactRoute(<NotFound />);
+    const h2 = screen.getByRole('heading',
+      {
+        name: /Page requested not found/,
+        level: 2,
+      });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    renderWithReactRoute(<NotFound />);
+    const img = screen.getByAltText(/pikachu crying/i);
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

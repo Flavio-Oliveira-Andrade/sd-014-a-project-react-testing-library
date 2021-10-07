4462df5edff085653f114e70e07dbe54a368d8c7
WillianAmorim <willian.amorim1@outlook.com>
2021-10-06 23:27:22 -0300

    feito requisito 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+// test('', () => {
+// });
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import { About } from '../components';
+
+describe('Testando componente About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    render(<About/>);
+
+    expect(screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémons')).toBeInTheDocument();
+    expect(screen.getByText('One can filter Pokémons by type, and see more details for each one of them')).toBeInTheDocument();
+  })
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    render(<About/>)
+  })
+})

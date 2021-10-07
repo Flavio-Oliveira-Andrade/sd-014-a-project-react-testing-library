4281e482b0a7404c0a447728ec5d3365385c55c3
jhonathancs <jhonathan_jcs@hotmail.com>
2021-10-06 21:51:36 -0500

    favorite

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from './render/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Testes do requisito 3', () => {
+  it('se é exibido na tela a mensagem No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  it('se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[5]] } />);
+    expect(screen.getByText('Mew')).toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+  });
+});

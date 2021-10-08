f712097ff84d9c38775fab6a3af918a86a459ad8
Fernando F Monteiro <fernandoflmbatera@gmail.com>
2021-10-08 18:08:45 -0300

    Requisito 04 - Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+// test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+
+import { NotFound } from '../components';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFound = screen.getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByAltText(/page requested was not found/i);
+    expect(img).toBeInTheDocument();
+  });
+});

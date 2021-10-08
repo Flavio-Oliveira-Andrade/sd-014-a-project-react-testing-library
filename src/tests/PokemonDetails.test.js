bccbefc3407929c970d82d6b9527bca33da43971
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-08 20:49:56 -0300

    7 - Test if shows detailed information of selected Pokémon

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Test PokemonDetails component', () => {
+  it('shows detailed information of selected Pokémon', () => {
+    pokemons.forEach((pokemon) => {
+      const { history } = renderWithRouter(<App />);
+      const { id, name, summary } = pokemon;
+
+      history.push(`/pokemons/${id}`);
+
+      const pokemonDetails = screen.getByRole('heading', {
+        name: `${name} Details`,
+      });
+      const detailsLink = screen.queryByRole('link', { name: 'More details' });
+      const pokemonSummary = screen.getByRole('heading', {
+        level: 2,
+        name: 'Summary',
+      });
+      const pokemonSummaryParagraph = screen.getByText(summary);
+
+      expect(pokemonDetails).toBeInTheDocument();
+      expect(pokemonSummary).toBeInTheDocument();
+      expect(pokemonSummaryParagraph).toBeInTheDocument();
+      expect(detailsLink).not.toBeInTheDocument();
+
+      // Cleanup is necessary to avoid memory leaks in the test environment
+      cleanup();
+    });
+  });
+});

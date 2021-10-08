1116f69e3a81a6275bf8e20e88bf9a47f94d7d82
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-08 19:16:18 -0300

    6 - Test if a card is rendered

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+describe('Test Pokemon component', () => {
+  it('renders a card with the information of a certain Pokémon', () => {
+    pokemons.forEach((pokemon) => {
+      renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite={ false } />);
+
+      const { averageWeight, image, name, type } = pokemon;
+      const { measurementUnit, value } = averageWeight;
+
+      const pokemonName = screen.getByTestId('pokemon-name');
+      const pokemonType = screen.getByTestId('pokemon-type');
+      const pokemonWeight = screen.getByTestId('pokemon-weight');
+      const pokemonImage = screen.getByRole('img');
+
+      expect(pokemonName).toBeInTheDocument();
+      expect(pokemonName).toHaveTextContent(name);
+      expect(pokemonType).toBeInTheDocument();
+      expect(pokemonType).toHaveTextContent(type);
+      expect(pokemonWeight).toBeInTheDocument();
+      expect(pokemonWeight).toHaveTextContent(
+        `Average weight: ${value} ${measurementUnit}`,
+      );
+      expect(pokemonImage).toBeInTheDocument();
+      expect(pokemonImage).toHaveAttribute('src', image);
+      expect(pokemonImage).toHaveAttribute('alt', `${name} sprite`);
+
+      // Cleanup is necessary to avoid memory leaks in the test environment
+      cleanup();
+    });
+  });
+});

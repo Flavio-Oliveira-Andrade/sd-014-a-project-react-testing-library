f983f3bf7344e47ce8bda0f9b3e39665ce85b808
gsdu-arte <gabi15duarte@hotmail.com>
2021-10-08 20:27:25 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './helper/renderWithRouter';
+import About from '../components/About';
+
+describe('About component test', () => {
+  test('should the page have a heading with a text "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const heading = screen.getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('should the page have two paragraphs with a text about the pokédex', () => {
+    renderWithRouter(<About />);
+
+    const paragraphs = screen.getAllByText(/pokémons/i);
+    expect(paragraphs).toHaveLength(2);
+  });
+
+  test('should have a specific image URL', () => {
+    renderWithRouter(<About />);
+
+    const image = screen.getByRole('img');
+    const URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image.src).toBe(URL);
+  });
+});

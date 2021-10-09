cdfbf6f3d3732b70e59e32e77c1950da9e97aae9
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2021-10-09 12:15:25 -0300

    req 02

--- src/renderWithRouter.js
+++ src/renderWithRouter.js
@@ -3,13 +3,13 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
-const renderWhithRouter = (component) => {
+const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
     ...render(<Router history={history}>{component}</Router>), history,
   });
 };
 
-export default renderWhithRouter;
+export default renderWithRouter;
 
 //criação do arquivo com a ajuda da aula ao vivo do dia 28/09
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+describe('Requisto 02', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title.innerHTML).toBe('About Pokédex');
+  });
+  it('deve conter dois paragrafos com o texto sobre a pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraph = screen.getAllByText(/pokémons/i);
+    expect(paragraph).toHaveLength(2);
+  });
+  it('Deve conter a imagem com a URL', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image.src).toBe(url);
+  });
+});

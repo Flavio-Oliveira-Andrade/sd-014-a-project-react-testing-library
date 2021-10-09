796e5f5573f9fc0a451681fc8a9c05b1586a2d1d
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2021-10-09 12:59:40 -0300

    req 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Requisito 3 Favorite Pokemons', () => {
+  it('Deve ser exibido na tela a mensagem No favorite pokemon foun', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textNotFound = screen.getByText('No favorite pokemon found');
+    expect(textNotFound).toBeInTheDocument();
+  });
+  it('Deve aparecer todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const details = screen.getByRole('link', {
+      name: 'More details',
+    });
+    userEvent.click(details);
+    const { pathname } = history.location;
+    expect(pathname).not.toBe('/');
+    const favorite = screen.getByRole('checkbox');
+    userEvent.click(favorite);
+    history.push('/favorites');
+    const pokemon = screen.getByText('Pikachu');
+    expect(pokemon).toBeInTheDocument();
+  });
+});

4603dfe1d098adc1e2977a28c6cd39e8bead669e
Andrea <andrealh08@gmail.com>
2021-10-08 22:48:41 -0300

    Requisito 4 melhorado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 
-import App from '../App';
-import renderWithRouter from '../components/renderWithRouter';
+// import App from '../App';
+// import renderWithRouter from '../components/renderWithRouter';
+import NotFound from '../components/NotFound';
 
 describe('Teste o componente "NotFound"', () => {
   test('Verifica que a página contém um heading `h2`'
   + 'com o texto `Page requested not found 😭`', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/notFound');
+    // const { history } = renderWithRouter(<App />);
+    // history.push('/notFound');
+    render(<NotFound />);
 
     const notFoundText = screen.getByRole('heading', {
       level: 2,
@@ -17,11 +19,14 @@ describe('Teste o componente "NotFound"', () => {
   });
 
   test('Verifica se a página mostra a imagem `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/notFound');
+    // const { history } = renderWithRouter(<App />);
+    // history.push('/notFound');
+    render(<NotFound />);
 
-    const notFoundImage = screen.getAllByRole('img');
-    expect(notFoundImage[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
-    expect(notFoundImage[1]).toBeInTheDocument();
+    // const notFoundImage = screen.getAllByRole('img');
+    // expect(notFoundImage[1]).toBeInTheDocument();
+    const notFoundImage = screen.getByAltText(/Pikachu crying/i);
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(notFoundImage).toBeInTheDocument();
   });
 });

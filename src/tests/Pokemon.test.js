ef0e3ab59cbe401991248d68d90acd11e0ba956e
Elielson Nascimento <nascimento.elielson@gmail.com>
2021-10-09 01:24:07 -0300

    Requisito 6 finalizado

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,6 +4,8 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../App';
 
+const MoreDetails = 'More details';
+
 describe('6. Teste o componente `<Pokemon.js />`', () => {
   it('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
     render(
@@ -12,14 +14,14 @@ describe('6. Teste o componente `<Pokemon.js />`', () => {
       </Router>,
     );
 
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
     expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
 
     expect(screen.getByTestId('pokemon-type')).toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Electric');
 
-    // - O peso médio do pokémon deve ser exibido com um texto no formato
-    //  `Average weight: <value> <measurementUnit>`; onde `<value>` e
-    //  `<measurementUnit>` são, respectivamente, o peso médio do pokémon
-    //  e sua unidade de medida.
+    expect(screen.getByTestId(('pokemon-weight')
+      .valueOf('Average weight: 6.0 kg'))).toBeInTheDocument();
   });
 
   it('- A imagem do Pokémon deve ser exibida. Ela deve'
@@ -30,7 +32,8 @@ describe('6. Teste o componente `<Pokemon.js />`', () => {
         <App />
       </Router>,
     );
-    expect(screen.getByAltText('Pikachu sprite')).toHaveAttribute('src');
+    expect(screen.getByAltText('Pikachu sprite'))
+      .toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
 
   it('Teste se o card do Pokémon indicado na Pokédex contém'
@@ -57,7 +60,7 @@ describe('6. Teste o componente `<Pokemon.js />`', () => {
       </Router>,
     );
 
-    userEvent.click(screen.getByRole('link', { name: 'More details' }));
+    userEvent.click(screen.getByRole('link', { name: MoreDetails }));
     expect(screen.getByText('Pikachu Details')).toBeInTheDocument();
   });
 
@@ -66,9 +69,13 @@ describe('6. Teste o componente `<Pokemon.js />`', () => {
   // + 'detalhes se deseja ver;', () => {
   //   render(
   //     <Router>
-  //       <App />
+  //       <Pokemon />
   //     </Router>,
   //   );
+
+  //   userEvent.click(screen.getAllByText('More Details'));
+
+  //   expect().toBe('/pokemons/25');
   // });
 
   it('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {

8179c3a214843e7cb64656809ef655d2c6a05e1b
unamednada <dias.gustavo.dias@gmail.com>
2021-10-10 20:33:04 -0300

    Create unit test for nav links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Tests the App component', () => {
+  it('should render the nav links Home, About and Favorite Pokemons', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', {
+      name: /home/i,
+    });
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', {
+      name: /about/i,
+    });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoritePokemonsLink = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+});

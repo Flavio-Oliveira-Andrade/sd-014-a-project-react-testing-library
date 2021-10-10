7a9455556e37ef91eb0fedcf38ca7575d133025c
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2021-10-10 20:43:06 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Testes do componente <App /> - Requisito 1', () => {
+  test('Se renderiza link "Home"', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+
+  test('Se renderiza link "About"', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText('About')).toBeInTheDocument();
+  });
+
+  test('Se renderiza link "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  test('Se a aplicação é redirecionada para a página inicial,'
+  + 'na URL "/" ao clicar no link Home da barra de navegação.', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('Home'));
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('Se a aplicação é redirecionada para a página de About,'
+  + 'na URL "/about" ao clicar no link About da barra de navegação.', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('About'));
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Se a aplicação é redirecionada para a página Pokémons Favoritados,'
+  + 'na URL "/favorites" ao clicar no link Favorite Pokémons da barra de'
+  + ' navegação.', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('Favorite Pokémons'));
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  test('Se a aplicação é redirecionada para a página Not Found ao entrar'
+  + ' em uma URL desconhecida.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('nao-existe');
+    expect(screen.getByText('Page requested not found')).toBeInTheDocument();
+  });
+
+});

b949db7420cde2db5a278ef0e40c48f759ee0142
Andrea <andrealh08@gmail.com>
2021-10-10 13:51:15 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -6,13 +6,15 @@ import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
 import pokemons from '../data';
 
-// const pokemonsType = () => {
-//   pokemons.reduce((acc, pokemon) => {
-//     if (!acc.includes(pokemon.type) {
-
-//     })
-//   })
-// };
+const pokemonsType = () => pokemons.reduce((acc, pokemon) => {
+  if (!acc.find((ac) => pokemon.type === ac.type)) {
+    return [...acc, {
+      type: pokemon.type,
+      filteredPokemons: (pokemons.filter((poke) => poke.type === pokemon.type)),
+    }];
+  }
+  return acc;
+}, []);
 
 describe('Testa o componente "Pokedex', () => {
   test('Verifica se página contém um heading `h2`'
@@ -45,28 +47,32 @@ describe('Testa o componente "Pokedex', () => {
     expect(encounteredPokemon).toBeInTheDocument();
   });
 
-  test('Verifica que seja mostrado apenas um Pokemon por vez', () => {
-    renderWithRouter(<App />);
-
-    const pokemon = screen.getAllByText(/pikachu/i);
-    expect(pokemon.length).toBeLessThan(2);
-  });
-
   test('Verifica se a Pokédex tem os botões de filtro', () => {
     renderWithRouter(<App />);
 
-    let buttonType = screen.getByText(/all/i);
+    const buttonType = screen.getByText(/all/i);
     expect(buttonType).toBeInTheDocument();
 
-    const types = pokemonsType();
-    types.forEach((type) => {
-      buttonType = screen.getAllByRole('button', { name: type });
-      expect(buttonType[0]).toBeInTheDocument();
-      expect(buttonType.length).toBeLessThan(2);
+    const buttonsType = screen.getAllByTestId('pokemon-type-button');
+    const pokemonType = pokemonsType();
+
+    const nextPokemonButton = screen.getByRole('button', { name: /Próximo pokémon/i });
+
+    buttonsType.forEach((button, index) => {
+      expect(button).toHaveTextContent(pokemonType[index].type);
+      userEvent.click(button);
+      const { filteredPokemons } = pokemonType[index];
+      if (filteredPokemons.length > 1) {
+        pokemonType[index].filteredPokemons.forEach((pokemon) => {
+          const showedPokemon = screen.getByText(pokemon.name);
+          expect(showedPokemon).toBeInTheDocument();
+          userEvent.click(nextPokemonButton);
+        });
+      }
     });
   });
 
-  test('Verificar se se a Pokédex contém um botão para resetar o filtro', () => {
+  test('Verificar se a Pokédex contém um botão para resetar o filtro', () => {
     renderWithRouter(<App />);
 
     const allButton = screen.getByRole('button', { name: /all/i });

b2d80958c8b193c6d857c62db06d4044922e5bf5
unamednada <dias.gustavo.dias@gmail.com>
2021-10-11 11:06:13 -0300

    Create first test for pokemondetails. issue with type of id

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import PokemonDetails from '../components/PokemonDetails';
+import pokemons from '../data';
+
+describe('Tests the PokemonDetails component', () => {
+  const detailPokemon = pokemons[0];
+  const { id } = detailPokemon;
+  const renderWithDetails = () => {
+    const isFavoriteObj = { [`${id}`]: false };
+    console.log(isFavoriteObj);
+    return renderWithRouter(<PokemonDetails
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isFavoriteObj }
+      match={ { params: { id } } }
+    />);
+  };
+  it('should render detailed info about the Pokemon', () => {
+    renderWithDetails();
+  });
+});

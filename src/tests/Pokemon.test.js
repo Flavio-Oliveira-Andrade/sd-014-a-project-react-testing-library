b1194d504780fc2841548c0c40440e841dc476f2
vanessabastos <vanessarsb83@gmail.com>
2021-10-11 15:07:08 -0300

    refatorando requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,54 +4,64 @@ import { screen, render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../App';
 
-describe('Teste o componente `<Pokemon.js />`', () => {
-  test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+const MoreDetails = 'More details';
+
+describe('6. Teste o componente `<Pokemon.js />`', () => {
+  test('Teste se é renderizado um card com as'
+  + 'informações de determinado pokémon.', () => {
     render(
       <Router>
         <App />
       </Router>,
     );
+
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
     expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+
     expect(screen.getByTestId('pokemon-type')).toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Electric');
+
+    expect(screen.getByTestId(('pokemon-weight')
+      .valueOf('Average weight: 6.0 kg'))).toBeInTheDocument();
   });
 
-  test('A imagem do Pokémon deve ser exibida.'
-  + 'Ela deve conter um atributo `src` com a URL da imagem'
-  + 'e um atributo `alt` com o texto `<name> sprite`,'
-  + 'onde `<name>` é o nome do pokémon;', () => {
+  test('- A imagem do Pokémon deve ser exibida. Ela deve'
+    + 'conter um atributo `src` com a URL da imagem e um atributo'
+    + '`alt` com o texto `<name> sprite`, onde `<name>` é o nome do pokémon;', () => {
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    expect(screen.getByAltText('Pikachu sprite'))
+      .toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  test('Teste se o card do Pokémon indicado na Pokédex contém'
+    + 'um link de navegação para exibir detalhes deste Pokémon.'
+    + 'O link deve possuir a URL `/pokemons/<id>`, onde `<id>` é'
+    + ' o id do Pokémon exibido;', () => {
     render(
       <Router>
         <App />
       </Router>,
     );
 
-    expect(screen.getByAltText('Pikachu sprite')).toBeInTheDocument();
-  });
-
-  test('Teste se o card do Pokémon indicado na Pokédex contém um link'
-  + 'de navegação para exibir detalhes deste Pokémon.'
-  + 'O link deve possuir a URL `/pokemons/<id>`,'
-  + 'onde `<id>` é o id do Pokémon exibido;', () => {
-    render(
-      <Router>
-        <App />
-      </Router>,
-    );
-
-    expect(screen.getByRole('link',
-      { name: 'More details' })).toBeInTheDocument();
+    expect(screen.getByRole('link', {
+      name: 'More details',
+    })).toHaveAttribute('href', '/pokemons/25');
   });
 
   test('Teste se ao clicar no link de navegação do Pokémon,'
-  + 'é feito o redirecionamento da aplicação para a'
-  + 'página de detalhes de Pokémon.', () => {
+    + 'é feito o redirecionamento da aplicação para a página de'
+    + ' detalhes de Pokémon. ', () => {
     render(
       <Router>
         <App />
       </Router>,
     );
 
-    userEvent.click(screen.getByRole('link', { name: 'More details' }));
+    userEvent.click(screen.getByRole('link', { name: MoreDetails }));
     expect(screen.getByText('Pikachu Details')).toBeInTheDocument();
   });
 
@@ -65,5 +75,7 @@ describe('Teste o componente `<Pokemon.js />`', () => {
     userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
     expect(screen.getByAltText('Pikachu is marked as favorite'))
       .toHaveAttribute('src', '/star-icon.svg');
+
+    expect(screen.getByAltText('Pikachu is marked as favorite')).toBeInTheDocument();
   });
 });

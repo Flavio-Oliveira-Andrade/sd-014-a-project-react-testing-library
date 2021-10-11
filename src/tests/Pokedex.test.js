d10077f2ede2710db923f991aac20bd2fcbf29ab
unamednada <dias.gustavo.dias@gmail.com>
2021-10-10 23:12:42 -0300

    Create first test for pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+describe('Tests the Pokedex component', () => {
+  it('should render a heading with the appropriate text', () => {
+    const isFavoriteObj = {};
+    pokemons.forEach(({ id }) => {
+      isFavoriteObj[id] = false;
+    });
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isFavoriteObj }
+    />);
+
+    const pokedexHeading = screen.getByRole('heading', {
+      name: /encountered pokémons/i,
+      level: 2,
+    });
+    expect(pokedexHeading).toBeInTheDocument();
+  });
+});

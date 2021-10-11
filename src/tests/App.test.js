8b739958ab0c95700126803ed9b3eb7cac415233
unamednada <dias.gustavo.dias@gmail.com>
2021-10-10 21:08:28 -0300

    Finish test app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Tests the App component', () => {
@@ -22,5 +22,37 @@ describe('Tests the App component', () => {
       name: /favorite pokémons/i,
     });
     expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+
+  it('redirects to the correct page when corresponding link is clicked', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', {
+      name: /home/i,
+    });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+
+    const aboutLink = screen.getByRole('link', {
+      name: /about/i,
+    });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+
+    const favoritePokemonsLink = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    expect(favoritePokemonsLink).toBeInTheDocument();
+    userEvent.click(favoritePokemonsLink);
+    expect(history.location.pathname).toBe('/favorites');
+
+    history.push('/foo');
+    const proofNotFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(proofNotFound).toBeInTheDocument();
   });
 });

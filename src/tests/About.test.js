d963dffb33010da90c762824054577b11205c38a
unamednada <dias.gustavo.dias@gmail.com>
2021-10-10 21:26:06 -0300

    Create test for heading and paragraphs in about.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -13,5 +13,15 @@ describe('Tests the About component', () => {
       level: 2,
     });
     expect(aboutTitle).toBeInTheDocument();
+
+    const info1Text = /This application simulates a Pokédex,/i;
+
+    const aboutInfo1 = screen.getByText(info1Text);
+    expect(aboutInfo1).toBeInTheDocument();
+
+    const info2Text = /One can filter Pokémons by type,/i;
+
+    const aboutInfo2 = screen.getByText(info2Text);
+    expect(aboutInfo2).toBeInTheDocument();
   });
 });

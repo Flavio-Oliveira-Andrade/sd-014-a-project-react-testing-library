34b620d4cbefd0605f13439a8706d1ed86844350
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-11 10:24:51 -0300

    Corrige erros apontados no stryker - Conclui requisito 6 (ou nao)

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,13 +9,13 @@ describe('Pokemon.js test', () => {
   test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
     renderWithRouter(<App />);
     const pkmnName = screen.getByTestId('pokemon-name');
-    expect(pkmnName).toBeInTheDocument();
+    expect(pkmnName).toHaveTextContent('Pikachu');
     const pkmnType = screen.getByTestId('pokemon-type');
-    expect(pkmnType).toBeInTheDocument();
+    expect(pkmnType).toHaveTextContent('Electric');
     const pkmnWeight = screen.getByTestId('pokemon-weight');
-    expect(pkmnWeight).toBeInTheDocument();
-    const pkmnImg = screen.getByRole('img');
-    expect(pkmnImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(pkmnWeight).toHaveTextContent('Average weight: 6.0 kg');
+    const pkmnImg = screen.getByRole('img', { name: /pikachu sprite/i }).src;
+    expect(pkmnImg).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
   test('Teste se o card do pkmn contém um link que possui a URL /pokemons/<id>', () => {
     const { history } = renderWithRouter(<App />);
@@ -27,8 +27,8 @@ describe('Pokemon.js test', () => {
   });
   test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
     renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite />);
-    const starIcon = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
-    expect(starIcon).toBeInTheDocument();
-    expect(starIcon).toHaveAttribute('src', '/star-icon.svg');
+    const favorite = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(favorite.src).toBe('http://localhost/star-icon.svg');
+    expect(favorite.alt).toBe(`${pokemons[0].name} is marked as favorite`);
   });
 });

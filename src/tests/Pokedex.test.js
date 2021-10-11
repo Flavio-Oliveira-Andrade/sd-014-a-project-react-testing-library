031e973d87b50abcbe8ff0c71ce3623e024221f5
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-11 01:24:22 -0300

    Resolve requisito 5 parcialmente

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Pokedex.js test', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const h2Text = screen.getByRole('heading', {
+      level: 2,
+      name: /encountered pokémons/i,
+    });
+    expect(h2Text).toBeInTheDocument();
+  });
+  test('Teste se é exibido o próximo pkmn da lista quando o botão é clicado', () => {
+    renderWithRouter(<App />);
+    const nextPkmnButton = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPkmnButton).toBeInTheDocument();
+
+    pokemons.forEach(({ name }) => {
+      const nextPkmn = screen.getByTestId('pokemon-name');
+      expect(nextPkmn).toBeInTheDocument();
+      expect(nextPkmn).toHaveTextContent(name);
+      fireEvent.click(nextPkmnButton);
+    });
+  });
+  test('Teste se é mostrado apenas um Pokémon por vez.', () => {
+    renderWithRouter(<App />);
+    const pkmnDisplay = screen.getAllByTestId('pokemon-name');
+    expect(pkmnDisplay.length).toBe(1);
+  });
+  test('Teste se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(<App />);
+  });
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+  });
+});

69078044492e7fa43c5dcec693de204aeae1738e
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-10 22:42:13 -0300

    Cria arquivo renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+
+describe('App.js test'), () => {
+  test('Testa se o topo da aplicação contém um conjunto fixo de links', () => {
+      
+  });
+};

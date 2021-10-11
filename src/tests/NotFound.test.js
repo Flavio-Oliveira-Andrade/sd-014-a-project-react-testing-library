8547650e8a2149c2d10e9ac401ae3a3cef0c3892
Yan Brasileiro <yan.brasileiro@gmail.com>
2021-10-11 12:47:26 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um heading e uma Imagem', () => {
+    render(<NotFound />);
+    const notFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    const img = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+
+    expect(notFound).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

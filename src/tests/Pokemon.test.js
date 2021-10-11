f9eb0f040c1f5e10743f59b125ea3a156896ca4e
dandara-dias <dandaradias.contato@gmail.com>
2021-10-11 01:28:59 -0300

    Requisito 6, completo no Stryker

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
 
 describe('Testa se é renderizado um card com as informações de determinado pokémon',
   () => {
@@ -32,3 +34,26 @@ describe('Testa se é renderizado um card com as informações de determinado po
       expect(pokemonAlt).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
     });
   });
+
+describe('Testa o componente <Pokemon />', () => {
+  test('O card do Pokémon indicado na Pokédex contém um link de navegação para exibir '
+  + 'detalhes deste Pokémon. O link deve possuir a URL "/pokemons/<id>", onde "<id>" é '
+  + 'o id do Pokémon exibido', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByText('More details');
+
+    expect(detailsLink.href).toContain('pokemons/25');
+  });
+});
+
+describe('Testa se existe um ícone de estrela nos Pokémons favoritados', () => {
+  test('O ícone deve ser uma imagem com o atributo "src" contendo o caminho '
+  + '"/star-icon.svg"', () => {
+    renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite />);
+
+    const favoriteIcon = screen.getByAltText(/is marked as favorite/);
+
+    expect(favoriteIcon).toHaveAttribute('src', '/star-icon.svg');
+  });
+});

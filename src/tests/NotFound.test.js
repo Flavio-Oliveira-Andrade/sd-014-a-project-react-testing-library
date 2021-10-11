9386d5d6f4b45c1ade22d36816fcf2865d4d6804
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-11 00:54:25 -0300

    Resolve requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound.js test', () => {
+  test('Teste se página contém um h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const headingNotFound = screen.getByText(/Page requested not found/i);
+    expect(headingNotFound).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem requisitada', () => {
+    renderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByRole('img', { name: /not found/i });
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

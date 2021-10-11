3eb950ee02687d3858ab71568ae20d101a1a5e10
WillianAmorim <willian.amorim1@outlook.com>
2021-10-11 11:27:48 -0300

    feito requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -6,13 +6,38 @@ describe('Testando componente About', () => {
   test('Teste se a página contém as informações sobre a Pokédex.', () => {
     render(<About />);
 
-    expect(screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémons'))
-      .toBeInTheDocument();
-    expect(screen.getByText('One can filter Pokémons by type, and see more details for each one of them'))
-      .toBeInTheDocument();
+    expect(screen.getByText(
+      (/This application simulates a Pokédex/i),
+    )).toBeInTheDocument();
   });
 
   test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
     render(<About />);
+
+    const textH2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+
+    expect(textH2).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    render(<About />);
+
+    expect(screen.getByText(
+      (/This application simulates a Pokédex/i),
+    )).toBeInTheDocument();
+    expect(screen.getByText(
+      (/One can filter Pokémons by type/i),
+    ))
+      .toBeInTheDocument();
+  });
+
+  test('Teste se contém uma imagem no arquivo', () => {
+    render(<About />);
+
+    const image = screen.getByRole('img');
+    expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
---
+++ src/tests/App.test.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import { App } from '../App';
+
+// // describe('Testando o componente App', () => {
+// //   test('Teste se o topo da aplicação contém links de navegação', () => {
+// //     render(<App />);
+
+// //     expect(screen.getByText('Home')).toBeInTheDocument();
+// //     expect(screen.getByText('About')).toBeInTheDocument();
+// //     expect(screen.getByText('Favorite Pokémons')).toBeInTheDocument();
+// //   });
+// // });

44e7d25f7b75c073af5aa5827559c9145a743152
gsdu-arte <gabi15duarte@hotmail.com>
2021-10-11 10:49:46 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './helper/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('FavoritePokemons component test', () => {
+  test('should display the message "No favorite pokemon found" '
+  + 'if theres no favorite pokemon', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText(/no favorite pokemon/i);
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('should render all favorite pokemons', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+
+    const charmander = screen.getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+
+    const mew = screen.getByText('Mew');
+    expect(mew).toBeInTheDocument();
+  });
+});

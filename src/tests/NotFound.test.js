5e5e9048a29e48bf41c53754bd8de006995ccb58
Maurício Leite <mauricioleite1@gmail.com>
2021-10-10 22:24:59 -0300

    Requisito 04 - correção

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -5,14 +5,16 @@ import { render, screen } from '@testing-library/react';
 import NotFound from '../components/NotFound';
 
 describe('Teste do componente Not found', () => {
-  it('Exibe um heading h2 com texto "Page requested not found"', () => {
+  beforeEach(() => {
     const customHistory = createMemoryHistory();
     render(
       <Router history={ customHistory }>
         <NotFound />
       </Router>,
     );
+  });
 
+  it('Exibe um heading h2 com texto "Page requested not found"', () => {
     expect(
       screen.getByRole('heading', {
         level: 2,
@@ -22,16 +24,10 @@ describe('Teste do componente Not found', () => {
   });
 
   it('Exibe uma imagem de Pikachu chorando', () => {
-    const customHistory = createMemoryHistory();
-    render(
-      <Router history={ customHistory }>
-        <NotFound />
-      </Router>,
-    );
-
     const img = screen
       .getByAltText('Pikachu crying because the page requested was not found');
 
     expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });

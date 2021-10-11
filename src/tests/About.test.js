74d95b7674d5f97f17f984fe1c726834adfb8768
unamednada <dias.gustavo.dias@gmail.com>
2021-10-10 21:15:11 -0300

    Create first test for about.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import About from '../components/About';
+
+describe('Tests the About component', () => {
+  it('renders info about the pokedex', () => {
+    renderWithRouter(<About />);
+
+    const aboutTitle = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+});

1b543e3fca5b9b762f249869d93b2bbd01a945d9
jmporto <jmporto@outlook.com>
2021-10-11 00:45:19 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+// test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../services/renderWithRouter';
+import pokemons from '../data';
+
+describe('Testando FavoritePokemons', () => {
+  test('Teste se renderiza a mensagem "No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorites = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorites).toBeInTheDocument();
+  });
+
+  test('Teste se os pokemons favoritados rendeziram', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    expect(screen.getByText('Caterpie')).toBeInTheDocument();
+    expect(screen.getByText('Ekans')).toBeInTheDocument();
+  });
+});

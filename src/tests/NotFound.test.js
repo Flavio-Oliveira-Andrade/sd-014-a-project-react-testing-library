3859b6cb4a0ac2cd44b252671708e222b6274636
unamednada <dias.gustavo.dias@gmail.com>
2021-10-10 22:54:09 -0300

    Test notfound redundant plus image

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Tests the NotFound component', () => {
+  it('should render a heading with the not found message', () => {
+    renderWithRouter(<NotFound />);
+
+    const message = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(message).toBeInTheDocument();
+  });
+
+  it('should render an image of Pikachu', () => {
+    renderWithRouter(<NotFound />);
+
+    const sadPikachu = screen.getByRole('img', {
+      name: /Pikachu crying because the page requested was not found/i,
+    });
+    expect(sadPikachu).toBeInTheDocument();
+    expect(sadPikachu.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

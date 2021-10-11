8d09659b0db9e6d448a60fc02a9209b172adafb9
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-11 00:05:34 -0300

    Resolve requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('1', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import About from '../components/About';
+
+describe('About.js test', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const aboutPokedexBrief = screen.getByText(/This application simulates a Pokédex/i);
+    expect(aboutPokedexBrief).toBeInTheDocument();
+  });
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const headingPokedex = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(headingPokedex).toBeInTheDocument();
+  });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const firstParagraph = screen.getByText(/This application simulates a Pokédex/i);
+    const secondParagraph = screen.getByText(/One can filter Pokémons by type/i);
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+  test('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const imgPokedex = screen.getByRole('img');
+    expect(imgPokedex).toBeInTheDocument();
+    expect(imgPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

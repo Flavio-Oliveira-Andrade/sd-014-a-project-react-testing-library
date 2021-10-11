d73c2a8fb10f1a321880b47392e67000b19d67b8
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2021-10-11 15:39:56 -0300

    req 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 04 Not Found', () => {
+  it('Deve conter um heading h2', () => {
+    renderWithRouter(<NotFound />);
+    const headh2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    });
+    expect(headh2).toBeInTheDocument();
+  });
+
+  it('Deve aparecer uma imagem', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
+
+// requisito feito com a ajuda do repositorio do colega Rafael Frasson
+// link: https://github.com/tryber/sd-014-a-project-react-testing-library/pull/110/commits/7632fa5ed693e67c01e5a5417f459242dbc70ad8
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import App from '../App';
+
+// describe('Requisito 5, Pokedex', () => {
+//   it('deve conter um heading h2', () => {
+//     renderWithRouter(<App />);
+//     const headingh2 = screen.getByRole('heading', {
+//       level: 2,
+//       name: 'Encountered pokémons',
+//     });
+//     expect(headingh2).toBeInTheDocument();
+//   });
+
+//   it()
+
+// });

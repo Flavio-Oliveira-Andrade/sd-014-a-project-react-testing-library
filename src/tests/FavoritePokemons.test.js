4ea11b698d2f9a03fe2d2ae85b59a722b0a216db
unamednada <dias.gustavo.dias@gmail.com>
2021-10-10 21:41:34 -0300

    Create test for favorite pokemon not found

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Tests the FavoritePokemons component', () => {
+  it('should render a message if no favorite pokemon added', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const MESSAGE = /no favorite pokemon found/i;
+    const message = screen.getByText(MESSAGE);
+    expect(message).toBeInTheDocument();
+  });
+});

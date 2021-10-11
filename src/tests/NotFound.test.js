8a7ad5da6bdff8bf12de7cbc87def1f5150d71f7
Guilherme Roos <guilherme.roosr@gmail.com>
2021-10-11 11:52:42 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  renderWithRouter(<NotFound />);
+  test('este se página contém um heading h2 com o texto Page requested not found', () => {
+    const h2 = screen.getByRole('heading', {
+      name: /Page requested not foun/i,
+      level: 2,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem', () => {
+    const notFoundImg = screen.getByAltText(/pikachu crying/i);
+    expect(notFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

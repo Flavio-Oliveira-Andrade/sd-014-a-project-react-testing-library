7a991d56b46c2970240eb27d66a3eca8fcfa9d68
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-10-12 19:37:22 -0300

    testa o componente About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../rendreWithRouter';
+import About from '../components/About';
+
+const srcImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+describe('Testa o componente About.js', () => {
+  it('testa se a pagina contém as informações sobre a pokedex', () => {
+    renderWithRouter(<About />);
+
+    const infoAboutPokedex = screen.getByText(/This application simulates a Pokédex/i);
+    expect(infoAboutPokedex).toBeInTheDocument();
+  });
+
+  it('testa se a pagina contém um heading h2 com o texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const heading = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const pokedexImage = screen.getByAltText('Pokédex');
+    expect(pokedexImage.src).toContain(srcImg);
+    expect(pokedexImage).toHaveAttribute('src', srcImg);
+  });
+});
+
+/** referencia para usar o teste na imagem: https://stackoverflow.com/questions/60509527/
+ * jestreact-native-testing-library-how-to-test-an-image-src */

9a049e0e16ff9cd3ee849e011512fe7f95fbe02d
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-10-12 20:22:52 -0300

    Testa o componente NotFound.j

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../rendreWithRouter';
+
+describe('Teste o componente NotFound.js', () => {
+  it('testa se contém um heading h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('testa se a pagina mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

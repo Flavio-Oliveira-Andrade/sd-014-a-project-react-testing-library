0dad9bfa2ad1cb68cb89c8c67e1854cc7cb044fc
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-10-12 18:24:49 -0300

    Testa o component App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../rendreWithRouter';
+import App from '../App';
+
+describe('Testa o componente App.js', () => {
+  it('o primeiro link deve possuir o texto "Home"', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  it('o segundo link deve possuir o texto "About"', () => {
+    renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  it('o terceiro link deve possuir o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const linkFavoritePokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavoritePokemons).toBeInTheDocument();
+  });
+});

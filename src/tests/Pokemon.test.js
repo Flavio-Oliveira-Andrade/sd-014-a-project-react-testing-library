ff1bc5e0a114d0a59bb586a64d6fc40ae0c926f2
vanessabastos <vanessarsb83@gmail.com>
2021-10-13 19:59:47 -0300

    refatorando requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -3,17 +3,40 @@ import userEvent from '@testing-library/user-event';
 import { screen, render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../App';
+import Pokemon from '../components/Pokemon';
+import renderWinthRouter from './novoTeste/ renderWithRouter';
 
 const MoreDetails = 'More details';
+const mockPokemon = {
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'Kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Kanto Viridian Forest',
+      map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+    },
+    {
+      location: 'Kanto Power Plant',
+      map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    },
+  ],
+  summary: `This intelligent Pokémon roasts hard berries with electricity
+  to make them tender anough to eat.`,
+};
 
 describe('6. Teste o componente `<Pokemon.js />`', () => {
   test('Teste se é renderizado um card com as'
   + 'informações de determinado pokémon.', () => {
-    render(
-      <Router>
-        <App />
-      </Router>,
-    );
+    renderWinthRouter(<Pokemon pokemon={ mockPokemon } isFavorite />);
+    const AVERAGE_WEIGHT = `Average weight: ${mockPokemon
+      .averageWeight.value} ${mockPokemon.averageWeight.measurementUnit}`;
 
     expect(screen.getByText('Pikachu')).toBeInTheDocument();
     expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
@@ -21,8 +44,8 @@ describe('6. Teste o componente `<Pokemon.js />`', () => {
     expect(screen.getByTestId('pokemon-type')).toBeInTheDocument();
     expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Electric');
 
-    expect(screen.getByTestId(('pokemon-weight')
-      .valueOf('Average weight: 6.0 kg'))).toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-weight')).toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-weight')).toHaveTextContent(AVERAGE_WEIGHT);
   });
 
   test('- A imagem do Pokémon deve ser exibida. Ela deve'
@@ -48,7 +71,7 @@ describe('6. Teste o componente `<Pokemon.js />`', () => {
     );
 
     expect(screen.getByRole('link', {
-      name: 'More details',
+      name: MoreDetails,
     })).toHaveAttribute('href', '/pokemons/25');
   });
 
@@ -63,6 +86,13 @@ describe('6. Teste o componente `<Pokemon.js />`', () => {
 
     userEvent.click(screen.getByRole('link', { name: MoreDetails }));
     expect(screen.getByText('Pikachu Details')).toBeInTheDocument();
+  });
+
+  test('Teste também se a URL exibida no navegador muda para `/pokemon/<id>`,'
+  + 'onde `<id>` é o id do Pokémon cujos detalhes se deseja ver;', () => {
+    renderWinthRouter(<Pokemon pokemon={ mockPokemon } isFavorite />);
+
+    expect(screen.getByRole('link', { name: MoreDetails })).toBeInTheDocument();
   });
 
   test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {

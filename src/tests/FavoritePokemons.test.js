2a4f7b02c9f3622fc7c447cfefb7fff9fd39d688
wendeldemelo <demelowendel@gmail.com>
2021-10-12 22:25:28 -0400

    requisito 3 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Verifica o componente <FavoritePokemons.js />', () => {
+  it('Verifica se é exibido na tela a mensagem No favorite pokemon found, '
+   + 'se a pessoa não tiver pokémons favoritos.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const headingText = screen.getByRole('heading', {
+      name: 'Favorite pokémons',
+      level: 2,
+    });
+    expect(headingText).toBeInTheDocument();
+    const notFoundText = screen.getByText('No favorite pokemon found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Verifica se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByText('More details'));
+    userEvent.click(screen.getByText('Pokémon favoritado?'));
+    history.push('/favorites');
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Electric');
+    expect(screen.getByTestId('pokemon-weight'))
+      .toHaveTextContent('Average weight: 6.0 kg');
+    expect(screen.getByText('More details')).toBeInTheDocument();
+  });
+});

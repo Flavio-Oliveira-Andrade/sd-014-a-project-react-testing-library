9e98a6b40f9d4483e5e85edaa104b9af0844af6d
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-10-14 16:06:13 -0300

    requisito 1 resolvido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,24 @@
-//
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../render/renderWithRouter';
+import App from '../App';
+
+describe('Testa se aplicação é renderizada para o componente App', () => {
+  test('Se o link possui o texto Home', () => {
+    renderWithRouter(<App />);
+    const linkTextHome = screen.getByText('Home');
+    expect(linkTextHome).toBeInTheDocument();
+  });
+
+  test('Se o link possui o texto About', () => {
+    renderWithRouter(<App />);
+    const linkTextAbout = screen.getByText('About');
+    expect(linkTextAbout).toBeInTheDocument();
+  });
+
+  test('Se o link possui o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const linkTextFavoritePokemons = screen.getByText('Favorite Pokémons');
+    expect(linkTextFavoritePokemons).toBeInTheDocument();
+  });
+});

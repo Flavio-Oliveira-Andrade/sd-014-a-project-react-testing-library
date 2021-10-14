80c00a24531b4e4d4dfec49f5ab2e6285a818d13
juli0c3sar <jcmuchiutti@gmail.com>
2021-10-13 22:15:01 -0300

    requisito 5 completo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,61 +4,63 @@ import userEvent from '@testing-library/user-event';
 import RenderWithRouter from '../util/RenderWithRouter';
 import App from '../App';
 
-test('h2 com o texto "encountered pokemons"', () => {
-  RenderWithRouter(<App />);
+describe('', () => {
+  test('h2 com o texto "encountered pokemons"', () => {
+    RenderWithRouter(<App />);
 
-  const pokEncontrados = screen.getByRole('heading', {
-    name: 'Encountered pokémons',
-    level: 2,
+    const pokEncontrados = screen.getByRole('heading', {
+      name: 'Encountered pokémons',
+      level: 2,
+    });
+    expect(pokEncontrados).toBeInTheDocument();
   });
-  expect(pokEncontrados).toBeInTheDocument();
-});
 
-test('testando proximo pokemon', () => {
-  RenderWithRouter(<App />);
-  const buttonProximoPokemon = screen.getByTestId('next-pokemon');
-  userEvent.click(buttonProximoPokemon);
+  test('testando proximo pokemon', () => {
+    RenderWithRouter(<App />);
+    const buttonProximoPokemon = screen.getByTestId('next-pokemon');
+    userEvent.click(buttonProximoPokemon);
 
-  const verificandoPokemon = screen.getByTestId('pokemon-name').innerHTML;
-  expect(verificandoPokemon).toBe('Charmander');
-  expect(buttonProximoPokemon).toBeInTheDocument();
-});
-
-test('renderizando um pokemon por vez', () => {
-  RenderWithRouter(<App />);
-  const pokemonRef = screen.getAllByTestId('pokemon-name');
-  expect(pokemonRef).toHaveLength(1);
-});
-
-test('teste se aparece os pokemons com seu respectivo tipo', () => {
-  RenderWithRouter(<App />);
-  const buttonTypebutton = screen.getAllByTestId('pokemon-type-button');
-  expect(buttonTypebutton[0].innerHTML).toBe('Electric');
-
-  userEvent.click(buttonTypebutton[1]);
-  expect(buttonTypebutton[1].innerHTML).toBe('Fire');
-
-  userEvent.click(buttonTypebutton[2]);
-  expect(buttonTypebutton[2].innerHTML).toBe('Bug');
-
-  userEvent.click(buttonTypebutton[3]);
-  expect(buttonTypebutton[3].innerHTML).toBe('Poison');
-
-  userEvent.click(buttonTypebutton[4]);
-  expect(buttonTypebutton[4].innerHTML).toBe('Psychic');
-
-  userEvent.click(buttonTypebutton[5]);
-  expect(buttonTypebutton[5].innerHTML).toBe('Normal');
-
-  userEvent.click(buttonTypebutton[6]);
-  expect(buttonTypebutton[6].innerHTML).toBe('Dragon');
-});
-
-test('testando se existe o botão "All"', () => {
-  RenderWithRouter(<App />);
-  const buttonAll = screen.getByRole('button', {
-    name: /all/i,
+    const verificandoPokemon = screen.getByTestId('pokemon-name').innerHTML;
+    expect(verificandoPokemon).toBe('Charmander');
+    expect(buttonProximoPokemon).toBeInTheDocument();
   });
-  userEvent.click(buttonAll);
-  expect(buttonAll.innerHTML).toBe('All');
+
+  test('renderizando um pokemon por vez', () => {
+    RenderWithRouter(<App />);
+    const pokemonRef = screen.getAllByTestId('pokemon-name');
+    expect(pokemonRef).toHaveLength(1);
+  });
+
+  test('teste se aparece os pokemons com seu respectivo tipo', () => {
+    RenderWithRouter(<App />);
+    const buttonTypebutton = screen.getAllByTestId('pokemon-type-button');
+    expect(buttonTypebutton[0].innerHTML).toBe('Electric');
+
+    userEvent.click(buttonTypebutton[1]);
+    expect(buttonTypebutton[1].innerHTML).toBe('Fire');
+
+    userEvent.click(buttonTypebutton[2]);
+    expect(buttonTypebutton[2].innerHTML).toBe('Bug');
+
+    userEvent.click(buttonTypebutton[3]);
+    expect(buttonTypebutton[3].innerHTML).toBe('Poison');
+
+    userEvent.click(buttonTypebutton[4]);
+    expect(buttonTypebutton[4].innerHTML).toBe('Psychic');
+
+    userEvent.click(buttonTypebutton[5]);
+    expect(buttonTypebutton[5].innerHTML).toBe('Normal');
+
+    userEvent.click(buttonTypebutton[6]);
+    expect(buttonTypebutton[6].innerHTML).toBe('Dragon');
+  });
+
+  test('testando se existe o botão "All"', () => {
+    RenderWithRouter(<App />);
+    const buttonAll = screen.getByRole('button', {
+      name: /all/i,
+    });
+    userEvent.click(buttonAll);
+    expect(buttonAll.innerHTML).toBe('All');
+  });
 });

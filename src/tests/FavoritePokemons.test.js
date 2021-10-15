2f87bb5f5556c6c9c01cc1a475b19097ba28793c
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-10-15 18:37:08 -0300

    requisito 3 resolvido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../render/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa se aplicação é renderizada para o componente FavoritePokemons', () => {
+  test(`Se é exibido na tela a mensagem "No favorite pokemon found",
+  se a pessoa não tiver pokémons favoritos.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textNoFavoritePokemonFound = screen.getByText('No favorite pokemon found');
+    expect(textNoFavoritePokemonFound).toBeInTheDocument();
+  });
+});

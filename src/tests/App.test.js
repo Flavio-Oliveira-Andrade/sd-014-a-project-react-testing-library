6b5ff7c0a988e1d53648959d71cc14b7c45581ca
Lucas G Silveira <lucgs88@gmail.com>
2021-10-17 16:33:53 -0300

    item 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Requisito 1', () => {
+  it('O primeiro link possui o texto \'Home\'', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto \'About\'', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText('About')).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto \'Favorite Pokémons\'', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  it('Ao clicar no link Home, a URL é redirecionada para ”/”', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('Home'));
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Ao clicar no link About, a URL é redirecionada para ”/about”', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('About'));
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Ao clicar no link Favorite, URL é redirecionada para ”/favorites”', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('Favorite Pokémons'));
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Ao passar uma rota desconnhecida, a URL é redirecionada para ”/Not Found”', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/ilanmon');
+    expect(screen.getByText('Page requested not found')).toBeInTheDocument();
+  });
+});

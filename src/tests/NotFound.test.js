fa20929d95b97b472ef45898b37dc0c8edc391ec
Victor Veloso <victorveloso13@gmail.com>
2021-10-19 20:41:03 -0300

    Requisito 4 - NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testa o componente NotFound.js', () => {
+  test('Testa se página contém um heading h2 com o texto Page requested not found',
+    () => {
+      render(<NotFound />);
+      const notFoundHeading = screen.getByRole('heading', {
+        level: 2,
+        name: /Page requested not found/i,
+      });
+      expect(notFoundHeading).toBeInTheDocument();
+    });
+
+  test('Testa se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const notFoundImg = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(notFoundImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

f6782c0f4a87fc4bd12845c17890c577ed073a85
Victor Veloso <victorveloso13@gmail.com>
2021-10-19 19:19:10 -0300

    Requisito 3 - FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('Testa o componente FavoritePokemons.js', () => {
+  test('Testa se é exibido na tela a mensagem "No favorite pokemon found",'
+   + 'se a pessoa não tiver pokémons favoritos',
+  () => {
+    render(<FavoritePokemons />);
+    const favoriteNotFound = screen.getByText(/No favorite pokemon found/i);
+    expect(favoriteNotFound).toBeInTheDocument();
+  });
+});

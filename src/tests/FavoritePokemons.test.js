3e16606336ba3bf136ad9f71e0b3935d623e7a64
DanFilipe2000 <anielilipe90@gmail.com>
2021-10-19 12:54:57 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import renderPath from './utilities/renderPath';
+
+describe('Testa o funcionamento do componente FavoritePokemons', () => {
+  test('Testa se é exibida na tela a mensagem de "No favorite pokemon found"', () => {
+    renderPath('/favorites');
+
+    const favoritesNotFound = screen.getByText('No favorite pokemon found');
+
+    expect(favoritesNotFound).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido na tela o pokemon quando é favoritado', () => {
+    renderPath('/pokemons/25');
+
+    const buttonFavorite = screen.getByText('Pokémon favoritado?');
+
+    userEvent.click(buttonFavorite);
+
+    const linkToFavorites = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    userEvent.click(linkToFavorites);
+
+    const pokemons = screen.getAllByRole('img');
+
+    expect(pokemons[0]).toBeInTheDocument();
+  });
+});

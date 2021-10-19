690b418590a0d7f7b705e4939df9118fc18383b1
DanFilipe2000 <anielilipe90@gmail.com>
2021-10-19 13:12:28 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+
+import renderPath from './utilities/renderPath';
+
+describe('Teste se o componente NotFound funciona corretamente', () => {
+  test('Testa se a página contém o título NotFound pokedex', () => {
+    renderPath('/notfound');
+
+    const notFoundTitle = screen.getByRole('heading', {
+      level: 2,
+    });
+
+    expect(notFoundTitle.innerHTML).toMatch('Page requested not found');
+  });
+
+  test('Testa se a página contém uma imagem', () => {
+    renderPath('/notfound');
+
+    const image = screen.getAllByRole('img');
+
+    expect(image[1].src).toMatch('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

7c7f37ec8d9a8dc7b2d0dd9b9270048e3fdf6740
PeDD1 <pedrohr0909@gmail.com>
2021-10-23 14:20:04 -0300

    Component App tests are done w/ lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utilitary/renderWithRouter';
+import App from '../App';
+
+describe('Tests component App redirects', () => {
+  beforeEach(() => { renderWithRouter(<App />); });
+
+  it('should render Home text', () => {
+    expect(screen.getByText(/Home/)).toBeInTheDocument();
+  });
+
+  it('should render About text', () => {
+    expect(screen.getByText(/About/)).toBeInTheDocument();
+  });
+
+  it('should render Favorite Pokémon text', () => {
+    expect(screen.getByText(/Favorite Pokémon/)).toBeInTheDocument();
+  });
+});

814bf256c625a95eaa24a20277588bab566e626f
Isaddora Bayma <isadora.bayma@gmail.com>
2021-10-23 18:35:07 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Verifica o componente <NotFound />', () => {
+  test('Mensagem de erro aparece na pagina Not Found', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundMsg = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(notFoundMsg).toBeInTheDocument();
+  });
+
+  test('Imagem aparece na pagina Not Found', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundImg = screen.getByAltText(/Pikachu crying/i);
+    expect(notFoundImg.src).toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

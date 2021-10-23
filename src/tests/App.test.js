273df30e814170dca65053441f5e5040cea611b0
Isaddora Bayma <isadora.bayma@gmail.com>
2021-10-23 01:37:08 -0300

    app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Verifica o <App />', () => {
+  test('Primeiro link deve ser "Home"', () => {
+    renderWithRouter(<App />);
+
+    const link1 = screen.getByRole('link', { name: 'Home' });
+
+    expect(link1).toBeInTheDocument();
+  });
+
+  test('Segundo link deve ser "About"', () => {
+    renderWithRouter(<App />);
+
+    const link2 = screen.getByRole('link', { name: 'About' });
+
+    expect(link2).toBeInTheDocument();
+  });
+
+  test('Terceiro link deve ser "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const link3 = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(link3).toBeInTheDocument();
+  });
+});

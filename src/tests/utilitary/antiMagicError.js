3def5f0668d1e7113b3cae37508c5303ed177627
PeDD1 <pedrohr0909@gmail.com>
2021-10-23 14:04:25 -0300

    Anti Magic lint error file setted

---
+++ src/tests/utilitary/antiMagicError.js
@@ -0,0 +1,11 @@
+export const HOME_PAGE = 'Home';
+export const FAVORITE_PAGE = 'Favorite Pokémons';
+export const DETAILS_PAGE = 'More details';
+export const POKE_TYPES = ['Eletric', 'Fire', 'Bug',
+  'Poison', 'Psychic', 'Normal', 'Dragon'];
+export const FAVORITE_POKE = 'Pokémon favoritado?';
+export const NAME_INFO = 'pokemon-name';
+export const TYPE_INFO = 'pokemon-type';
+export const WEIGHT_INFO = 'pokemon-weight';
+export const NEXT_POKE = 'Próximo pokémon';
+export const TYPE_BUTTON = 'pokemon-type-button';

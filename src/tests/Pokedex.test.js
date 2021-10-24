657c7e08d1e1b22abe6e7ac5d825bd058bfdeafc
PeDD1 <pedrohr0909@gmail.com>
2021-10-24 17:28:51 -0300

    Testing Pokedex Component w/ success

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,63 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './utilitary/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+import { NAME_INFO, TYPE_BUTTON, POKE_TYPES } from './utilitary/antiMagicError';
+
+describe('Tests Pokédex component', () => {
+  beforeEach(() => { renderWithRouter(<App />); });
+  it('should render Encountered Pokémons <h2>', () => {
+    expect(screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+      level: 2,
+    })).toBeInTheDocument();
+  });
+
+  it('should redirects to the next pokemon', () => {
+    const nextPokemonBtn = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    const nameId = screen.getByTestId(NAME_INFO);
+    expect(nextPokemonBtn).toBeInTheDocument();
+    pokemons.map((pokemon, index) => {
+      expect(nameId).toHaveTextContent(pokemon.name);
+      fireEvent.click(nextPokemonBtn);
+      return expect(nameId).not.toHaveTextContent(pokemons[index].name);
+    });
+    pokemons.forEach((_, index) => {
+      if (index < pokemons.length - 1) fireEvent.click(nextPokemonBtn);
+    });
+    expect(nameId).toHaveTextContent(pokemons[pokemons.length - 1].name);
+    fireEvent.click(nextPokemonBtn);
+    expect(nameId).toHaveTextContent(pokemons[0].name);
+  });
+
+  it('should render only 1 pokémon', () => {
+    expect((screen.getAllByTestId(NAME_INFO).length)).toBe(1);
+  });
+
+  it('should have functional filter buttons', () => {
+    expect(screen.getAllByTestId(TYPE_BUTTON).length).toBe(POKE_TYPES.length);
+    screen.getAllByTestId(TYPE_BUTTON).map((buttonType, index) => expect(buttonType)
+      .toHaveTextContent(POKE_TYPES[index]));
+    expect(screen.getByRole('button', {
+      name: /All/i,
+    })).toBeInTheDocument();
+  });
+
+  it('should have a functional button to clean filters', () => {
+    const allPokeBtn = screen.getByRole('button', {
+      name: /All/i,
+    });
+    expect(allPokeBtn).toBeInTheDocument();
+    fireEvent.click(allPokeBtn);
+    pokemons.forEach((_, index) => {
+      expect(screen.getByTestId(NAME_INFO)).toHaveTextContent(pokemons[index].name);
+      fireEvent.click(screen.getByRole('button', {
+        name: /Próximo pokémon/i,
+      }));
+      expect(allPokeBtn).toBeInTheDocument();
+    });
+  });
+});
--- src/tests/utilitary/antiMagicError.js
+++ src/tests/utilitary/antiMagicError.js
@@ -1,7 +1,7 @@
 export const HOME_PAGE = 'Home';
 export const FAVORITE_PAGE = 'Favorite Pokémons';
 export const DETAILS_PAGE = 'More details';
-export const POKE_TYPES = ['Eletric', 'Fire', 'Bug',
+export const POKE_TYPES = ['Electric', 'Fire', 'Bug',
   'Poison', 'Psychic', 'Normal', 'Dragon'];
 export const FAVORITE_POKE = 'Pokémon favoritado?';
 export const FAVORITES_TOTAL = 3;

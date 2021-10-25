38f7ff708e428a8f3d462ba34a3580b8de14905f
julianoprogrammierer <julianoprogrammierer@gmail.com>
2021-10-25 13:12:48 -0300

    'Teste o componente <NotFound.js />'

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testa o componente <NotFound.js />', () => {
+  test('Teste se página contém um heading h2'
+  + ' com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const message = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(message).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra o gif do pikachu.', () => {
+    render(<NotFound />);
+    const img = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const gif = screen.getByAltText(/pikachu crying/i);
+    expect(gif.src).toStrictEqual(img);
+  });
+});

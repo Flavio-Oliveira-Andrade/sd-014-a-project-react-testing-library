106c370dafb29d2c5814099a5f0a38f438ef317c
edsonmac <edson-mac@hotmail.com>
2021-10-25 02:22:43 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorites = screen.getByText('No favorite pokemon found');
+    expect(noFavorites).toBeInTheDocument();
+  });
+});

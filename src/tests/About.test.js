427568b6bc412ec76b66a2feeafeff13830b484a
edsonmac <edson-mac@hotmail.com>
2021-10-25 02:07:49 -0300

    requisito 2 About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Teste o componente <About.js />.', () => {
+  beforeEach(() => renderWithRouter(<About />));
+  test('se a página contém um heading h2 com o texto About Pokédex', () => {
+    const heading = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const firstParagraphFrase = /This application simulates a Pokédex, a digital/i;
+    const firstParagraph = screen.getByText(firstParagraphFrase);
+    const secondParagraphFrase = /One can filter Pokémons by type, and see more details/i;
+    const secondParagraph = screen.getByText(secondParagraphFrase);
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  test('Se a imagem da Pokédex foi renderizada na página', () => {
+    const imgLink = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imgTest = screen.getByRole('img');
+    expect(imgTest).toHaveAttribute('src', imgLink);
+  });
+});

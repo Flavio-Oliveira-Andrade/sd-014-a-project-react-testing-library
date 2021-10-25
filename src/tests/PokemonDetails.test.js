fc13d6d8d9fc1b67372986fb93a5e6d9b3cfcebf
César Meira <cesarmeirasouza@gmail.com>
2021-10-25 00:06:25 -0300

    Lint

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -23,10 +23,10 @@ describe('Tests if the detailed Pokémon information is shown on the screen', ()
     const links = screen.getAllByRole('link');
     links.forEach((link) => {
       expect(link).not.toHaveTextContent(/More Details/i);
-    })
+    });
   });
   it('should contain a h2 with the text Summary', () => {
-    const h2 = screen.getByRole('heading', {level: 2, name: /Summary/i })
+    const h2 = screen.getByRole('heading', { level: 2, name: /Summary/i });
     expect(h2).toBeInTheDocument();
   });
   it('should contain a paragraph with a summary of the specific Pokémon', () => {
@@ -36,32 +36,35 @@ describe('Tests if the detailed Pokémon information is shown on the screen', ()
 });
 
 describe('Tests if there is a section with maps containing the pokémon locations', () => {
+  const KANTO_LOCATIONS = 3;
+  const TOTAL_IMAGES = 4;
   it('should contain a h2 with the text Game Locations of (pokemon name here)', () => {
-    const h2 = screen.getByRole('heading', {level: 2, name: /Game Locations of Charmander/i })
+    const h2 = screen.getByRole('heading', { level: 2, name: /Locations of Charma/i });
     expect(h2).toBeInTheDocument();
   });
   it('shows all the Pokémon locations', () => {
     const locations = screen.getAllByText(/Kanto/i);
-    expect(locations).toHaveLength(3);
+    expect(locations).toHaveLength(KANTO_LOCATIONS);
     const otherLocation = screen.getByText(/Alola/i);
     expect(otherLocation).toBeInTheDocument();
   });
   it('displays the name and image of each location', () => {
     const locations = screen.getAllByText(/Kanto/i);
-    expect(locations).toHaveLength(3);
+    expect(locations).toHaveLength(KANTO_LOCATIONS);
     const otherLocation = screen.getByText(/Alola/i);
     expect(otherLocation).toBeInTheDocument();
 
     const images = screen.getAllByAltText(/charmander location/i);
-    expect(images).toHaveLength(4);
+    expect(images).toHaveLength(TOTAL_IMAGES);
   });
   it('should have a src attribute with location URL on the location image', () => {
+    const URL = 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png';
     const images = screen.getAllByAltText(/charmander location/i);
-    expect(images[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png');
+    expect(images[0]).toHaveAttribute('src', URL);
   });
   it('should have an alt attribute with (pokemon name here) location', () => {
     const images = screen.getAllByAltText(/charmander location/i);
-    expect(images).toHaveLength(4);
+    expect(images).toHaveLength(TOTAL_IMAGES);
   });
 });
 
@@ -72,7 +75,7 @@ describe('Tests if the user can favorite a Pokémon on details page', () => {
     const star = screen.getByAltText(/favorite/i);
     expect(star).toBeInTheDocument();
   });
-  it('should add and remove the pokemon from favorite list, with alternate clicks', () => {
+  it('should add and remove the poke from favorite list, with alternate clicks', () => {
     const checkbox = screen.getByRole('checkbox');
     const star = screen.getByAltText(/favorite/i);
     expect(star).toBeInTheDocument();

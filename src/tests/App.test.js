ba03d64dd2b9e28462579d8f3c5a836f3818947a
YanParoni <paroniyan@gmail.com>
2021-10-26 20:20:15 -0300

    lint fix

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithReactRouter from './renderWithReactRouter';
+
+describe('Componente App', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithReactRouter(<App />);
+
+    const urlHome = screen.getByText(/Home/i);
+    const urlAbout = screen.getByText(/About/i);
+    const urlFavorite = screen.getByText(/Favorite Pokémons/i);
+
+    expect(urlHome).toBeInTheDocument();
+    expect(urlAbout).toBeInTheDocument();
+    expect(urlFavorite).toBeInTheDocument();
+  });
+
+  it('Teste se ao clicar no link Home redireciona para a url "/"', () => {
+    const { history } = renderWithReactRouter(<App />);
+    const urlHome = screen.getByText(/Home/i);
+    fireEvent.click(urlHome);
+    const pathHome = history.location.pathname;
+    expect(pathHome).toBe('/');
+  });
+
+  it('Teste se ao clicar no link About redireciona para a url "/about"', () => {
+    const { history } = renderWithReactRouter(<App />);
+    const urlAbout = screen.getByText(/About/i);
+    fireEvent.click(urlAbout);
+    const pathAbout = history.location.pathname;
+    expect(pathAbout).toBe('/about');
+  });
+
+  it('Teste clicar no link Favorite Pokémons redireciona para a url "/favorites"',
+    () => {
+      const { history } = renderWithReactRouter(<App />);
+      const urlFavorites = screen.getByText(/Favorite Pokémons/i);
+      fireEvent.click(urlFavorites);
+      const pathFavorites = history.location.pathname;
+      expect(pathFavorites).toBe('/favorites');
+    });
+
+  it('Teste se for passado um URL desconhecido redireciona para a página Not Found',
+    () => {
+      const { history } = renderWithReactRouter(<App />);
+      history.push('/unknown');
+      const notFound = screen.getByText('Page requested not found');
+      expect(notFound).toBeInTheDocument();
+    });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithReactRouter from './renderWithReactRouter';
+
+describe('Componente Not Found', () => {
+  it('Teste se página contém um heading h2 com o texto: Page requested not found', () => {
+    renderWithReactRouter(<NotFound />);
+    const h2 = screen.getByRole('heading',
+      {
+        name: /Page requested not found/,
+        level: 2,
+      });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    renderWithReactRouter(<NotFound />);
+    const img = screen.getByAltText(/pikachu crying/i);
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
